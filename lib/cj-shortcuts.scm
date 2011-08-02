7f2c6ecd81e92d0ec5bde5aaca3b2e6da11a653d
Christian Jaeger <chrjae@gmail.com>
2011-08-02 12:09:55 -0400

    create cj-shortcuts

---
+++ lib/cj-shortcuts.scm
@@ -0,0 +1,10 @@
+;;; Copyright 2011 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+(define-macro* (L args . body)
+  `(lambda ,args ,@body))
