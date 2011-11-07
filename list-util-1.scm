c4e84c59495d3f8d3683f17ff335e220fd69064d
Christian Jaeger <chrjae@gmail.com>
2011-11-06 19:51:57 -0500

    create list-util-1.scm

---
+++ list-util-1.scm
@@ -0,0 +1,20 @@
+;;; Copyright 2010, 2011 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+(require (lib.srfi-1))
+
+
+;; This is separate from list-util (and list-util-2) to enable use in
+;; mod.scm (bootstrapping)
+
+(define (map/tail func l tail)
+  (fold-right (lambda (val tail)
+		(cons (func val)
+		      tail))
+	      tail
+	      l))
