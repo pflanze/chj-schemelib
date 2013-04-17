550612dc11f0587a0446fc703cfd32d70ae4bbe6
Christian Jaeger <chrjae@gmail.com>
2013-04-17 14:33:54 +0100

    add |symbol-replace-_-with/|

--- cj-ffi.scm
+++ cj-ffi.scm
@@ -5,3 +5,23 @@
 	     `(define ,name (##c-code ,(string-append "___RESULT= ___FIX("
 						      (symbol->string name)
 						      ");"))))))
+
+;; helper function to replace _ in identifiers:
+(define (symbol-replace-_-with/ c)
+  (assert* char? c
+	   (lambda (c)
+	     (compose* string->symbol
+		       list->string
+		       (cut map (lambda_
+				 (if (eq? _ '#\_)
+				     c
+				     _))
+			    <>)
+		       string->list
+		       symbol->string))))
+
+(TEST
+ > ((symbol-replace-_-with/ #\c) 'M_-bar)
+ Mc-bar
+ )
+
