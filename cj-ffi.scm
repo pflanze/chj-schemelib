621ebbb74e0d4eb348a26f1eea8ea46cc80d67fe
Christian Jaeger <chrjae@gmail.com>
2013-04-21 02:28:51 +0100

    symbol-replace-_-with/: accept strings and symbols

--- cj-ffi.scm
+++ cj-ffi.scm
@@ -8,21 +8,36 @@
 
 ;; helper function to replace _ in identifiers:
 (define (symbol-replace-_-with/ c)
-  (assert* char? c
-	   (lambda (c)
-	     (compose* string->symbol
-		       list->string
-		       (cut map (lambda_
-				 (if (eq? _ '#\_)
-				     c
-				     _))
-			    <>)
-		       string->list
-		       symbol->string))))
+  (let ((cont (lambda (chars)
+		(compose* string->symbol
+			  list->string
+			  (cut fold-right
+			       (lambda (c res)
+				 (if (eq? c '#\_)
+				     (append chars res)
+				     (cons c res)))
+			       '()
+			       <>)
+			  string->list
+			  symbol->string)))
+	(c* (source-code c)))
+    (mcase c
+	   (char?
+	    (cont (list c*)))
+	   (string?
+	    (cont (string->list c*)))
+	   (symbol?
+	    (cont (string->list (symbol->string c*)))))))
 
 (TEST
  > ((symbol-replace-_-with/ #\c) 'M_-bar)
  Mc-bar
+ > ((symbol-replace-_-with/ "abc") 'M_-bar)
+ Mabc-bar
+ > ((symbol-replace-_-with/ 'xyz) 'M_-bar)
+ Mxyz-bar
+ > ((symbol-replace-_-with/ "") 'M_-bar)
+ M-bar
  )
 
 (define-macro* (define-macro-symbol-replace-_-with nam cvar)
