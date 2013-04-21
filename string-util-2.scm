641913b1661c006f9c6cba3431376c5ece3d0bcd
Christian Jaeger <chrjae@gmail.com>
2013-04-21 05:06:10 +0100

    add |string-trim-right| and |chomp|

--- string-util-2.scm
+++ string-util-2.scm
@@ -10,3 +10,72 @@
  "scm"
  )
 
+
+;; slow way. just  .
+;; drop-while but from the end.
+(define (list-trim-right lis pred)
+  ((compose* reverse
+	     (cut drop-while pred <>)
+	     reverse) lis))
+
+
+(define string-trim-right
+  (compose* list->string
+	    (cut list-trim-right <> char-whitespace?)
+	    string->list))
+
+(TEST
+ > (string-trim-right "")
+ ""
+ > (string-trim-right " ")
+ ""
+ > (string-trim-right "foo\n")
+ "foo"
+ > (string-trim-right "foo\n ")
+ "foo"
+ )
+
+
+(define (string-ref* str i)
+  (if (negative? i)
+      (let ((len (string-length str)))
+	(string-ref str (+ len i)))
+      (string-ref str i)))
+(TEST
+ > (string-ref* "abc" 0)
+ #\a
+ > (string-ref* "abc" 1)
+ #\b
+ > (string-ref* "abc" -1)
+ #\c
+ > (string-ref* "abc" -2)
+ #\b
+ > (with-exception-handler range-exception? (thunk (string-ref* "abc" -4)))
+ #t
+ )
+
+(define (string-empty? str)
+  (zero? (string-length str)))
+
+;; A string chom that works like Perl's
+
+(define (chomp str)
+  (if (string-empty? str)
+      str
+      (if (char=? (string-ref* str -1) #\newline) ;; Perl even ignores \r heh
+	  (substring str 0 (dec (string-length str)))
+	  str)))
+
+(TEST
+ > (chomp "")
+ ""
+ > (chomp " ")
+ " "
+ > (chomp "a\n")
+ "a"
+ > (chomp "a\n\n")
+ "a\n"
+ > (chomp "a\r")
+ "a\r"
+ )
+
