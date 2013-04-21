f9af0159694b07f2bae17636411e7a204c9d6d4d
Christian Jaeger <chrjae@gmail.com>
2013-04-21 10:49:58 +0100

    add |pathspec.xcontent| (and |port.content|)
    not much to share with backtick, interestingly

--- cj-io-util.scm
+++ cj-io-util.scm
@@ -3,6 +3,16 @@
   (newline))
 
 
+(define (port.content p)
+  (read-line p #f))
+
+(define (pathspec.xcontent pathspec)
+  (let* ((p (open-input-file pathspec))
+	 (output (port.content p)))
+    (close-port p)
+    output))
+
+
 (define (backtick cmd . args)
   (let* ((p (open-process (list path: cmd
 				arguments: args
