3c4b01522932f1731d5a2cf15d7f5c196208b01e
Christian Jaeger <chrjae@gmail.com>
2013-04-22 20:05:30 +0100

    make it handle optional ":" based prefixes

--- dot-oo.scm
+++ dot-oo.scm
@@ -10,18 +10,44 @@
 			  " for value:")
 	   obj)))
 
-(define (dot-oo:split-name sym)
-  (let ((parts (string-split (symbol->string* sym) #\.)))
-    (mcase (possibly-sourcify parts sym)
-	   (`(`a `b)
-	    (values (string->symbol a)
-		    (string->symbol
-		     ;; really prepend the dot? I'm still confused, torn.
-		     (string-append "." b)))))))
+;; (XX lost the string-split that allowed to give me limits on the
+;; number of result values. Working around using strings-join.)
+
+(define (dot-oo:split-typename.methodname str)
+  (let ((parts (string-split (source-code str) #\.)))
+    (mcase (possibly-sourcify parts str)
+	   (`(`typename . `rest)
+	    (values typename
+		    ;; really prepend the dot? I'm still confused, torn.
+		    (string-append "." (strings-join rest ".")))))))
+
+(define (dot-oo:split-prefix:typename.methodname str)
+  (define (cont prefix remainder)
+    (letv ((typename methodname) (dot-oo:split-typename.methodname remainder))
+	  (values prefix
+		  typename
+		  methodname)))
+  (let* ((parts (string-split (source-code str) #\:)))
+    (if (= (length parts) 1)
+	(cont "" str)
+	(cont (string-append (car parts) ":")
+	      (possibly-sourcify
+	       (strings-join (cdr parts) ":") str)))))
 
 (TEST
- > (values->vector (dot-oo:split-name 'foo.bar))
- #(foo .bar)
+ > (values->vector (dot-oo:split-typename.methodname "foo.bar"))
+ #("foo" ".bar")
+ > (values->vector (dot-oo:split-typename.methodname "foo.bar.baz"))
+ #("foo" ".bar.baz")
+ > (values->vector (dot-oo:split-typename.methodname "fix:foo.bar.baz"))
+ #("fix:foo" ".bar.baz")
+ > (values->vector (dot-oo:split-prefix:typename.methodname
+		    "fix:foo.bar.baz:boo"))
+ #("fix:" "foo" ".bar.baz:boo")
+ > (values->vector (dot-oo:split-prefix:typename.methodname
+		    "foo.bar.baz"))
+ #("" "foo" ".bar.baz")
+ ;; XX I'm not testing "foo.bar.baz:boo".. what should it do then?...
  )
 
 (define (dot-oo:if-type-then-run-else-run type? then else)
@@ -32,16 +58,22 @@
 
 (define define.-template
   (lambda (name expr)
-    (letv ((typename genericname) (dot-oo:split-name name))
-	  `(begin
-	     (define ,name ,expr)
-	     (define-if-not-defined ,genericname
-	       (dot-oo:no-method-found-for-generic ',genericname))
-	     (set! ,genericname
-		   (dot-oo:if-type-then-run-else-run
-		    ,(source.symbol-append typename '?)
-		    ,name
-		    ,genericname))))))
+    (let ((namestr (possibly-sourcify (symbol->string (source-code name))
+				      name)))
+      (letv ((prefixstr typenamestr genericnamestr)
+	     (dot-oo:split-prefix:typename.methodname namestr))
+	    (let ((genericname (string->symbol (string-append prefixstr
+							      genericnamestr)))
+		  (typename (string->symbol typenamestr)))
+	      `(begin
+		 (define ,name ,expr)
+		 (define-if-not-defined ,genericname
+		   (dot-oo:no-method-found-for-generic ',genericname))
+		 (set! ,genericname
+		       (dot-oo:if-type-then-run-else-run
+			,(source.symbol-append typename '?)
+			,name
+			,genericname))))))))
 
 (define-macro* (define. first . rest)
   (mcase first
