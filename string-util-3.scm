57cd1fbcf201598263ad755b70f52d281d930791
Christian Jaeger <chrjae@gmail.com>
2013-05-01 13:53:06 +0100

    |string-contains-char?|

--- string-util-3.scm
+++ string-util-3.scm
@@ -2,11 +2,22 @@
 ;; Hm actually perhaps it doesn't.
 ;; Well or maybe it does. |for| as macro, then .for for the underlying?
 
-(define-macro* (for var seq . body)
-  `(.for seq
-	 (lambda (,var return)
-	   ,@body)))
+;; (define-macro* (for var seq . body)
+;;   `(.for seq
+;; 	 (lambda (,var return)
+;; 	   ,@body)))
 
 
 (define (string-contains-char? str pred)
-  (let ((len))))
+  (let ((len (string-length str)))
+    (let lp ((i 0))
+      (and (< i len)
+	   (or (pred (string-ref str i))
+	       (lp (inc i)))))))
+
+(TEST
+ > (string-contains-char? "Hello" char-newline?)
+ #f
+ > (string-contains-char? "Hello\n" char-newline?)
+ #t
+ )
