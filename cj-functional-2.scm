2a60039ac577da2e5e7e56ea2e42480b27e642e6
Christian Jaeger <chrjae@gmail.com>
2013-05-04 23:44:10 +0100

    fix dependency ordering
    because of present .o files not noticed, right?
    need mcase
    but mcase needs compose in cj-functional

---
+++ cj-functional-2.scm
@@ -0,0 +1,19 @@
+;; The "Clojure-macro". What was it named? XXX
+
+(define (chain-expand start exprs)
+  (let next ((exprs exprs)
+	     (res start))
+    (if (null? exprs)
+	res
+	(next (cdr exprs)
+	      (mcase (car exprs)
+		     (`(`call . `rest)
+		      `(,call ,res ,@rest)))))))
+
+(TEST
+ > (chain-expand 'input '((foo-set 1) (bar-set 2)))
+ (bar-set (foo-set input 1) 2))
+
+(define-macro* (chain start . exprs)
+  (chain-expand start exprs))
+
