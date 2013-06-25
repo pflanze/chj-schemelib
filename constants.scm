8f40da588ea6c61046d95c3721fabdbd26df9ce7
Christian Jaeger <chrjae@gmail.com>
2013-06-25 17:38:02 +0100

    create

---
+++ constants.scm
@@ -0,0 +1,29 @@
+;;; Copyright 2013 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+(require (lib.define-macro-star)
+	 (lib.cj-phasing))
+
+(compile-time
+ (define *CONSTs* (make-table)))
+
+(define-macro* (CONST e)
+  (let* ((sym (string->symbol (object->string (cj-desourcify e)))))
+    (if (not (table-ref *CONSTs* sym #f))
+	;; heh location info from another place, then
+	(table-set! *CONSTs* sym e))
+    sym))
+
+(define-macro* (CONSTANTS)
+  (let ((e `(begin
+	      ,@(map (lambda (sym.e)
+		       `(define ,(car sym.e) ,(cdr sym.e)))
+		     (table->list *CONSTs*)))))
+    (set! *CONSTs* (make-table))
+    e))
+
