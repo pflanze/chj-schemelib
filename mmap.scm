1263f6ff1222e23020a7a19c3c4c4dcf7ed440ce
Christian Jaeger <chrjae@gmail.com>
2013-07-01 14:25:58 +0100

    copyright

--- mmap.scm
+++ mmap.scm
@@ -1,3 +1,10 @@
+;;; Copyright 2013 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
 
 (require cj-functional
 	 cj-ffi)
