a8b2aefcf49302af509dde580223a1b4e97a40b4
Christian Jaeger <chrjae@gmail.com>
2013-07-07 14:34:52 +0100

    more copy from chjmodule

---
+++ posix/interrupts.scm
@@ -0,0 +1,283 @@
+
+;; cj Fri, 27 Jan 2006 12:25:15 +0100
+;; based on code sent by Marc Feeley on 4 Jan 2006
+
+;; this is unix centric, and expects BSD like signal behaviour.
+;; I develop on Debian.
+;; (Maybe some time, this could go into a |posix| module?)
+
+;; todo: this assumes that the order in which raised interrupts are
+;; delivered to scheme code is the same as the one in which the C
+;; signal handler is called. Is this a safe assumption?
+
+;; todo: make this thread safe
+
+;; create a retrieve-current setting infrastructure, which then can be
+;; given instead of procedure?
+;; or, do it with parameters instead ?  how?
+
+;; cj Thu, 28 Dec 2006 01:35:31 +0100
+;; NOTE: this is using 'signal', not 'sigaction'. But as long as
+;; nobody's interested in realtime signals (and/or special signals
+;; like async etc.), that probably doesn't make a difference (since
+;; non-realtime signals can be lost).
+
+;; -- compilation infrastructure
+
+;;simplified COPY from gperl-lowlevel.scm
+(##define-macro (define-constant-from-c name)
+  `(define ,name
+     ((c-lambda ()
+		int
+		,(string-append "___result="
+				(symbol->string name)
+				";")))))
+;;/COPY
+
+(define-compiletime (make-gen-code check-code return-code)
+  (lambda (name/args c-name argtypes returntype error-message #!optional error-message2)
+    (let ((name (car name/args))
+	  (args (cdr name/args)))
+      `(define ,name/args
+	 (let ((res ((c-lambda ,argtypes ,returntype ,c-name) ,@args)))
+	   (if ,check-code
+	       (error (string-append
+		       ,(string-append (symbol->string name) ": ")
+		       ,(if error-message2
+			    `(if (= res SIGQUEUE_ERROR2)
+				 ,error-message2
+				 ,error-message))))
+	       ,return-code))))))
+
+(##define-macro (define-c/int_or_error . args)
+  (apply (make-gen-code '(= res SIGQUEUE_ERROR) 'res) args))
+
+(##define-macro (define-c/status . args)
+  (apply (make-gen-code '(not (= res 0)) #!void) args))
+
+
+;; ----- sigqueue interface ---------------
+
+(c-declare "#include \"interrupts_Cpart.c\"")
+
+;(c-define-type sigqueue (pointer (struct "sigqueue") sigqueue* "sigqueue_release"))
+(c-define-type sigqueue (pointer (struct "sigqueue") sigqueue "sigqueue_release"))
+; aber nein, default handling of gambit is with the stars.
+;(c-define-type sigqueue (pointer (struct "sigqueue") sigqueue* "sigqueue_release"))
+; but why. how would I deal with a structure without pointer? todo ask.
+;; hey and even stuff like latin1-string do not have stars.
+
+
+(define-constant-from-c SIGQUEUE_SUCCESS)
+(define-constant-from-c SIGQUEUE_ERROR)
+(define-constant-from-c SIGQUEUE_ERROR2)
+
+(define make-sigqueue (c-lambda () sigqueue "make_sigqueue"))
+
+(define sigqueue-usage (c-lambda (sigqueue) int "sigqueue_usage"))
+
+(define (sigqueue-full? q)
+  (= ((c-lambda (sigqueue) int "sigqueue_isfull") q)
+     1))
+(define (sigqueue-empty? q)
+  (= ((c-lambda (sigqueue) int "sigqueue_isempty") q)
+     1))
+
+(define-c/status (sigqueue-add! q signo) "sigqueue_add" (sigqueue int) int "queue is full")
+
+(define-c/int_or_error (sigqueue-ref-signo q) "sigqueue_ref_signo" (sigqueue) int "queue is empty")
+
+(define-c/status (sigqueue-remove! q) "sigqueue_remove" (sigqueue) int "queue is empty")
+
+(define-c/status (sig-lock!) "sig_lock" () int "already locked" "sigprocmask gave error");;todo show OS error
+(define-c/status (sig-unlock!) "sig_unlock" () int "not locked" "sigprocmask gave error");;todo show OS error
+
+(define (sig-locked?)
+  ((c-lambda () bool "___result= is_locked;")))
+
+;; scheme addons:
+
+(define (sigqueue-take! q)
+  (let ((res (sigqueue-ref-signo q)))
+    (sigqueue-remove! q)
+    res))
+
+(define (sigqueue-startpos q)
+  ((c-lambda (sigqueue) int "___result= ___arg1->startpos;") q))
+(define (sigqueue-endpos q)
+  ((c-lambda (sigqueue) int "___result= ___arg1->endpos;") q))
+(define (sigqueue-positions q)
+  (values (sigqueue-startpos q)
+	  (sigqueue-endpos q)))
+
+(define (sigqueue-overflow q)
+  ((c-lambda (sigqueue) bool "___result= ___arg1->overflow;") q))
+(define (sigqueue-overflow-reset! q)
+  ((c-lambda (sigqueue) void "___arg1->overflow=false;") q))
+
+
+;; ---- the list of possible signals and their names:  -------------
+
+(insert-result-of
+ (begin
+   ;; Get all signal names. This assumes that the shell command "kill -l
+   ;; <number>" will return the signal name without the SIG prefix.
+   (define (signal-number->name num)
+     (let* ((port (open-process (list path: "kill"
+				      arguments: (list "-l" (number->string num)))))
+	    (line (read-line port))
+	    (res (close-port port)))
+       ;;(warn "got kill res code-or-so =" res) is always #!void.  still ask how todo this.
+       (string-append "SIG" line)))
+   (let ((alis (map-iota (lambda (num)
+			   (cons num (string->symbol (signal-number->name num))))
+			 64 ;; the number of valid signal numbers
+			 1 ;; the lowest signal number
+			 )))
+     `(begin
+	(define %signal-number->name% ',alis)
+	,@ (map (lambda (p)
+		  `(define ,(cdr p) ,(car p)))
+		alis)))))
+
+(define-if-not-defined SIGIO SIGPOLL)
+
+(define (signal-number->name num); returns symbol. [should we take err+success continuations?...]
+  (alist-ref %signal-number->name% num
+	     (lambda ()
+	       (error "signal-number->name: unknown signal number:" num))))
+
+
+;; ------- interfacing scheme with handler setup functions: ------
+
+(define sig-errstr (c-lambda () latin1-string "sig_errstr"))
+
+(insert-result-of
+ (cons 'begin
+       (map (lambda (CNAME)
+	      `(define (,(string->symbol CNAME)
+			  num)
+		 (or (= ((c-lambda (int) int ,CNAME) num) 0)
+		     (error (string-append ,(string-append CNAME ": system error: ")
+					   (sig-errstr))))))
+	    '("install_signal" "uninstall_signal"))))
+
+
+(define-if-not-defined handlers '())
+(define (handlers-set! v)
+  (set! handlers v))
+
+(define (interrupt-install-handler! signal-number handler) ;; signal-number may also be a list of numbers
+  (if (and (##fixnum? signal-number)
+	   (> signal-number 0))
+      ;; check for SIGINT and then set current-user-interrupt-handler instead?
+      (if (= signal-number SIGINT)
+	  (current-user-interrupt-handler handler)
+	  (begin
+	    (alist-set! handlers signal-number handler handlers-set!)
+	    (install_signal signal-number)))
+      (if (list? signal-number)
+	  (for-each interrupt-install-handler! signal-number)
+	  (error "interrupt-install-handler!: signal-number argument is not an ordinal number:" signal-number))))
+
+(define (interrupt-remove-handler! signal-number) ;; signal-number may also be a list of numbers
+  (if (and (##fixnum? signal-number)
+	   (> signal-number 0))
+      (if (= signal-number SIGINT)
+	  (begin
+	    (current-user-interrupt-handler ##default-user-interrupt-handler ;; ok?
+					    ;; and: do we want it to reinstall the default gambit one? or rather remove it really..?
+					    ;; (todo: should we route signal 2 through this interrupts module as well?)
+					    ))
+	  (begin
+	    (uninstall_signal signal-number)
+	    (alist-remove! handlers signal-number
+			   (lambda ()
+			     (error "interrupt-remove-handler!: no handler installed for signal:" signal-number))
+			   handlers-set!)))
+      (if (list? signal-number)
+	  (for-each interrupt-remove-handler! signal-number)
+	  (error "interrupt-remove-handler!: signal-number argument is not an ordinal number:" signal-number))))
+
+
+(define-if-not-defined global_queue #f)
+
+;;çç neue funktionen. noch mehr?
+
+(define (call-with-locks thunk)
+  (dynamic-wind
+      sig-lock!
+      thunk
+      sig-unlock!))
+
+(define (signal-take!)
+  (call-with-locks (lambda ()
+		     (sigqueue-take! global_queue))))
+
+
+(define (interrupt-dispatch)
+  ;; called by scheme runtime as result of the C
+  ;; ___EXT(___raise_interrupt) (___INTR_7); call and the
+  ;; (##interrupt-vector-set! 7 interrupt-dispatch) mapping.
+  (let ((signum (call-with-locks
+		 (lambda ()
+		   (if (sigqueue-overflow global_queue)
+		       (begin
+			 (warn "Warning: signal queue has been overflown!")
+			 (sigqueue-overflow-reset! global_queue)))
+		   (sigqueue-take! global_queue)))))
+    (alist-ref handlers signum
+	       (lambda ()
+		 (warn "no scheme signal handler installed anymore for signal:" signum)
+		 (exit 123);;;?todo
+		 )
+	       (lambda (handler)
+		 (handler)))))
+
+
+(define (init)
+  (let ((res ((c-lambda () int "init"))))
+    (cond ((= res SIGQUEUE_SUCCESS)
+	   ;; 'extract' the queue:
+	   ;;(if (not global_queue) ehr no, forget it. newly initialized in C, we need it here to. The only alternative would be the other direction, give from scheme to C. But we only loose not-yet-delivered signals, so no really big deal anyway.
+	   (set! global_queue ((c-lambda () sigqueue "___result_voidstar= global_queue;")))
+	   (##interrupt-vector-set! 7 interrupt-dispatch))
+	  ((= res SIGQUEUE_ERROR)
+	   (error "interrupts init: could not allocate memory for sigqueue"))
+	  (else
+	   (error "interrupts init: ?? unknown error")))))
+
+(init);yes on each load init it. since C part needs it.right?.
+
+; --------
+
+; test:
+(insert-result-of
+ (cond (#f
+	'(begin
+	   (interrupt-install-handler! SIGINT (lambda () (warn "sigint called")))
+	   (interrupt-install-handler! SIGUSR1 (lambda () (warn "sigusr1 called")))
+	   (interrupt-install-handler! SIGUSR2 (lambda () (warn "sigusr2 called")))
+
+
+	   (define my-pid (##os-getpid))
+
+	   (display "executing kill -USR1\n")
+
+	   (shell-command (string-append "kill -USR1 " (number->string my-pid)))
+
+	   (display "executing kill -USR2\n")
+
+	   (shell-command (string-append "kill -USR2 " (number->string my-pid)))
+
+	   (display "done\n")))
+       (#f
+	'(begin
+	   (define (test port)
+	     (interrupt-install-handler! SIGINT (lambda () (display "i" port)))
+	     (interrupt-install-handler! SIGUSR1 (lambda () (display "1" port)))
+	     (interrupt-install-handler! SIGUSR2 (lambda () (display "2" port)))
+	     (interrupt-install-handler! SIGRTMIN (lambda () (error "got RT signal")))
+	     )))
+       (else
+	'(begin))))
---
+++ posix/interrupts_Cpart.c
@@ -0,0 +1,257 @@
+// cj Fri, 27 Jan 2006 19:18:46 +0100
+
+// A fixed-size queue which doesn't allocate memory.
+// an interrupt safe queue (well, no thread locking is done yet - but it doesn't allocate memory or change global data in an unsafe way, I hope.)
+
+
+struct sigqueue_entry {
+    int signo;
+};
+
+
+#define bool int
+#define true 1
+#define false 0
+
+/* the following constant actually has to be one greater than the
+   maximally possible load, to make it possible to differentiate
+   between fullness and emptyness */
+
+#define SIGQUEUE_LENGTH 10000  /* big enough so as to not overflow on signal storms */
+/* 1000 has not been enough when being tested with this:
+   perl -we '$pid= 23892; for(1..1e6) {kill "USR1",$pid; kill "USR2",$pid; kill "USR1",$pid; kill "USR2",$pid; kill "USR2",$pid; }'
+   10'000 has been ok (the end size of the output file has been 4812800, so about 3.8% of the signals have been lost; but unix doesn't guarantee delivery of the full number of URS1/2 signals.)
+*/
+   
+
+struct sigqueue {
+    int startpos;
+    int endpos;
+    bool overflow;
+    struct sigqueue_entry data[SIGQUEUE_LENGTH];
+};
+
+//static struct sigqueue *q;
+
+//static int
+//init_queue () {
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define SIGQUEUE_ERROR -1
+#define SIGQUEUE_SUCCESS 0
+typedef int int_or_error;
+typedef int status;
+
+
+static struct sigqueue *
+make_sigqueue () {
+    struct sigqueue* q= malloc(sizeof(struct sigqueue));
+    if (q) {
+	q->startpos=0;
+	q->endpos=0;
+	q->overflow=false;
+    }
+    return q;
+}
+
+static int
+sigqueue_usage (struct sigqueue *q) {
+    if (q->startpos <= q->endpos) {
+	return q->endpos - q->startpos;
+    } else {
+	return q->endpos + SIGQUEUE_LENGTH - q->startpos;
+    }
+}
+
+static bool
+sigqueue_isfull (struct sigqueue *q) {
+    return (sigqueue_usage(q) == (SIGQUEUE_LENGTH-1));
+}
+static bool
+sigqueue_isempty (struct sigqueue *q) {
+    return (q->startpos == q->endpos);
+}
+
+static status
+sigqueue_add (struct sigqueue *q, int signo) {
+    if (sigqueue_isfull(q)) {
+	q->overflow=true;
+	return SIGQUEUE_ERROR;
+    } else {
+	q->data[q->endpos].signo=signo;
+	q->endpos++;
+	if (q->endpos == SIGQUEUE_LENGTH) {
+	    q->endpos=0;
+	}
+	return SIGQUEUE_SUCCESS;
+    }
+}
+
+static int_or_error
+sigqueue_ref_signo (struct sigqueue * q) {
+    if (sigqueue_isempty(q)) {
+	return SIGQUEUE_ERROR;
+    } else {
+	return q->data[q->startpos].signo;
+    }
+}
+
+static status
+sigqueue_remove (struct sigqueue * q) {
+    if (sigqueue_isempty(q)) {
+	return SIGQUEUE_ERROR;
+    } else {
+	q->startpos++;
+	if (q->startpos == SIGQUEUE_LENGTH) {
+	    q->startpos=0;
+	}
+	return SIGQUEUE_SUCCESS;
+    }
+}
+
+
+//static void
+//sigqueue_release (struct sigqueue*q) {
+static ___SCMOBJ
+sigqueue_release (void*q) {
+    //fprintf(stderr,"releasing %p\n",q);
+    free(q);
+    return ___FIX(___NO_ERR);
+}
+
+
+/* --- interfacing scheme with handler setup functions: -------
+ */
+
+#include <signal.h>
+
+static struct sigqueue * global_queue;
+static sigset_t set_of_all_handlers;
+#include <errno.h>
+static int sig_errno=0;
+
+static void
+handler (int sig)
+{
+    sigset_t saved_set;
+    // since I'm too lazy to change all the signal() calls to be sure to block all others, I do this:
+    // [btw: do we have to add SIGINT to set_of_all_handlers? no , not until we install_signal on it and then it's been taken care of. ok?]
+    if (0== sigprocmask(SIG_BLOCK, &set_of_all_handlers, &saved_set)) {
+	sigqueue_add(global_queue, sig);
+	if (0== sigprocmask(SIG_SETMASK, &saved_set, 0)) {
+	    /* and, regardless of result of sigqueue_add: (since
+	       scheme will still have to look at it) */
+	    ___EXT(___raise_interrupt) (___INTR_7);
+	} else {
+	    fprintf(stderr,"WARNING from signal handler: got error2\n");
+	}
+    } else {
+	fprintf(stderr,"WARNING from signal handler: got error1\n");
+    }
+}
+
+static status
+install_signal (int sig)
+{
+    if (signal (sig, handler) != SIG_ERR) {
+	if (sigaddset(&set_of_all_handlers, sig)>=0) {
+	    return SIGQUEUE_SUCCESS;
+	} else {
+	    sig_errno=errno;
+	    return SIGQUEUE_ERROR;
+	}
+    } else {
+	sig_errno=errno;
+	return SIGQUEUE_ERROR;
+    }
+}
+
+static status
+uninstall_signal (int sig)
+{
+    if (signal (sig, SIG_DFL) != SIG_ERR) {
+	if (sigdelset(&set_of_all_handlers, sig)>=0) {
+	    return SIGQUEUE_SUCCESS;
+	} else {
+	    sig_errno=errno;
+	    return SIGQUEUE_ERROR;
+	}
+    } else {
+	sig_errno=errno;
+	return SIGQUEUE_ERROR;
+    }
+}
+
+
+/* --- process-wide locking by masking interrupts: -------
+
+ Masking signals is the only safe way to prevent signals from changing
+ the queue while scheme is reading it. (I think semaphores would risk
+ deadlocks)
+ 
+ (And, since we're not using multiple scheme pthreads, no semapthores
+ for locking out other threads is necessary.)
+
+ int sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
+*/
+
+#include <signal.h>
+
+static sigset_t saved_set;
+static bool is_locked=false;
+
+#define SIGQUEUE_ERROR2 -2
+    
+static status
+sig_lock() {
+    if (is_locked) {
+	return SIGQUEUE_ERROR;
+    } else {
+	//sigset_t newset;
+	// mask which signals? all? no, segv and such must still be delivered. well:
+	// all the signal handlers which have been set by us.
+	if (0== sigprocmask(SIG_BLOCK, &set_of_all_handlers, &saved_set)) {
+	    is_locked=true;
+	    return SIGQUEUE_SUCCESS;
+	} else {
+	    sig_errno=errno;
+	    return SIGQUEUE_ERROR2;
+	}
+    }
+}
+
+static status
+sig_unlock() {
+    if (is_locked) {
+	if (0== sigprocmask(SIG_SETMASK, &saved_set, 0)) {
+	    is_locked=false;
+	    return SIGQUEUE_SUCCESS;
+	} else {
+	    sig_errno=errno;
+	    return SIGQUEUE_ERROR2;
+	}
+    } else {
+	return SIGQUEUE_ERROR;
+    }
+}
+
+#include <string.h>
+
+static char*
+sig_errstr() {
+    return strerror(sig_errno);
+}
+
+/* ---- init: ------------ */
+
+static status
+init() {
+    sigemptyset(&set_of_all_handlers);
+    //global_queue= make_sigqueue() || return SIGQUEUE_ERROR; we are not in perl :/
+    if (!(global_queue= make_sigqueue()))
+	return SIGQUEUE_ERROR;
+    return SIGQUEUE_SUCCESS;
+}
