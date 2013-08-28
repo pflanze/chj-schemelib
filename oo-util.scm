815e24185937658b0ed0a51905d986a0ad2b0e63
Christian Jaeger <chrjae@gmail.com>
2013-08-29 00:13:19 +0100

    add |true?| and |true| (hm)

--- oo-util.scm
+++ oo-util.scm
@@ -1,5 +1,10 @@
 (define false? not) ;; so as to be able to use "false." as OO prefix
 (define (anything? x) #t)
+;; maybe also, since at it:
+(define (true? x) (eq? x #t)) ;; dangerous to mistake?
+(define (true x)
+  ;; any kind of true; identity
+  (not (not x)))
 
 
 (define. number.string number->string)
