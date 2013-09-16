3459f56a0d2c66e54a7210cb573c42077f22eb47
Christian Jaeger <chrjae@gmail.com>
2013-09-16 20:14:16 +0100

    rename cross-product -> cartesian-product

--- list-util-2.scm
+++ list-util-2.scm
@@ -120,7 +120,9 @@
  2
  )
 
-(define (cross2 a orig-b)
+;; 'cross product'
+
+(define (cartesian-product-2 a orig-b)
   (let rec ((a a)
 	    (b orig-b))
     (cond ((null? a)
@@ -135,27 +137,27 @@
 		      (cdr b)))))))
 
 (TEST
- > (cross2 '(E F) (cross2 '(C D) '((A) (B))))
+ > (cartesian-product-2 '(E F) (cartesian-product-2 '(C D) '((A) (B))))
  ((E C A) (E C B) (E D A) (E D B) (F C A) (F C B) (F D A) (F D B))
  )
 
-(define (cross-product . lists)
+(define (cartesian-product . lists)
   (cond ((null? lists)
 	 (error "?XXX"))
 	((null? (cdr lists))
 	 (map list (car lists)))
 	(else
-	 (cross2 (car lists)
-		 (apply cross-product (cdr lists))))))
+	 (cartesian-product-2 (car lists)
+			      (apply cartesian-product (cdr lists))))))
 
 (TEST
- > (cross-product '(A B) '(C D) '(E F))
+ > (cartesian-product '(A B) '(C D) '(E F))
  ((A C E) (A C F) (A D E) (A D F) (B C E) (B C F) (B D E) (B D F))
- > (cross-product '(A B) '(C D) '(E))
+ > (cartesian-product '(A B) '(C D) '(E))
  ((A C E) (A D E) (B C E) (B D E))
- > (cross-product '(A B) '(C D))
+ > (cartesian-product '(A B) '(C D))
  ((A C) (A D) (B C) (B D))
- > (cross-product '(A B))
+ > (cartesian-product '(A B))
  ((A) (B))
  )
 
