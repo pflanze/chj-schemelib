cf3a77bdf76b6700602f06f28199543715a2ee20
Christian Jaeger <chrjae@gmail.com>
2013-09-25 19:08:37 +0100

    re-add |-d?| and |-f?|
    Well, last time it was |-d| and |-f| IIRC.

--- cj-path.scm
+++ cj-path.scm
@@ -5,8 +5,6 @@
 
 
 ;; XX should this be in an IO library, not path.
-(define (file-directory? path)
-  (eq? (file-info-type (file-info path)) 'directory))
 
 (define (if-file-info path chase? then/1 else/0)
   (let ((prevhandler (current-exception-handler)))
@@ -23,3 +21,18 @@
 (define (maybe-file-info path #!optional (chase? #t))
   (if-file-info path chase? identity false/0))
 
+(define (file-directory? path)
+  (eq? (file-info-type (file-info path)) 'directory))
+;; well, better, no error when not existing:
+(define (-d? path #!optional (chase? #t))
+  (if-file-info path
+		chase?
+		(lambda (info)
+		  (eq? (file-info-type info) 'directory))
+		false/0))
+(define (-f? path #!optional (chase? #t))
+  (if-file-info path
+		chase?
+		(lambda (info)
+		  (eq? (file-info-type info) 'regular))
+		false/0))
