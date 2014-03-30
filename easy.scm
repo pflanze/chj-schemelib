34b7d5f3a81ebf69c3f8af79002e6e4d618840c6
Christian Jaeger <ch@christianjaeger.ch>
2014-03-30 17:22:16 +0100

    add 'easy language'

---
+++ easy.scm
@@ -0,0 +1,23 @@
+;; short identifiers
+
+(define-macro* (& . args)
+  ;; `(thunk ,@args)
+  `(lambda () ,@args))
+
+(define-macro* (defstruct . args)
+  `(define-struct. ,@args))
+
+(define-macro* (def first . rest)
+  (if (pair? (source-code first))
+      `(define-typed ,first ,@rest)
+      `(define ,first ,@rest)))
+
+(define-macro* (def. . args)
+  `(define. ,@args))
+
+(define-macro* (defenum name . args)
+  `(define-enum ,name ,@args))
+
+(define-macro* (defmacro . args)
+  `(define-macro* ,@args))
+
