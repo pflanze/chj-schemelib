49e96124d0c5cc8da674bc398e3947f6ca61e7ff
Christian Jaeger <ch@christianjaeger.ch>
2014-05-27 01:26:19 +0100

    add |filename-or-.-..-string?| and |filename-string?

--- cj-path.scm
+++ cj-path.scm
@@ -3,6 +3,44 @@
 (define (path-append a b)
   (string-append a "/" b))
 
+(define (filename-or-.-..-string? v)
+  (and (string? v)
+       ;; max length is (a) filesystem dependent, and (b) encoding
+       ;; dependent. More than 255 characters shouldn't be there,
+       ;; though, 'usually'.
+       (<= 1 (string-length v) 255)
+       (not (string-contains? v "/"))))
+
+(define (filename-string? v)
+  (and (filename-or-.-..-string? v)
+       (not (string=? v "."))
+       (not (string=? v ".."))))
+
+
+(TEST
+ > (filename-string? ".")
+ #f
+ > (filename-string? "..")
+ #f
+ > (filename-or-.-..-string? ".")
+ #t
+ > (filename-or-.-..-string? "..")
+ #t
+ > (filename-or-.-..-string? "/")
+ #f
+ > (filename-or-.-..-string? "a..")
+ #t
+ > (filename-or-.-..-string? "..a")
+ #t
+ > (filename-string? "..a")
+ #t
+ > (filename-string? "a")
+ #t
+ > (filename-string? "./a")
+ #f
+ > (filename-string? "")
+ #f)
+
 
 ;; XX should this be in an IO library, not path.
 
