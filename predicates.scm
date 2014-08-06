73aabe801b0ed6e21e5ff0bae243ca501fd3976b
Christian Jaeger <ch@christianjaeger.ch>
2014-08-06 22:00:12 +0100

    add 'improper to proper' map, |string-of|, |improper-list-of|
    and improper-every and char-one-of

--- predicates.scm
+++ predicates.scm
@@ -16,3 +16,51 @@
 
 ;; btw should probably move predicates stuff from cj-functional here
 
+;; XXX lib, together with nonempty-string? from "lib/cj-posixpath"
+
+;; improper->proper-map
+
+(define (improper*-map/tail fn v tail)
+  (improper-fold-right (lambda (a r)
+			 (cons (fn a) r))
+		       tail
+		       v))
+
+(define improper*-map (cut improper*-map/tail <> <> '()))
+
+(TEST
+ > (improper*-map true? '("" . ""))
+ (#f #f))
+
+
+(define (string-of pred)
+  (lambda (v)
+    (and (string? v)
+	 (string-every pred v))))
+
+(TEST
+ > (map (string-of char-alphanumeric?) '(foo "" " " "foo" "foo bar" "foo:" "Foo_"))
+ (#f #t #f #t #f #f #t))
+
+
+(define (improper-every pred v)
+  (cond ((pair? v)
+	 (and (pred (car v))
+	      (improper-every pred (cdr v))))
+	((null? v)
+	 #t)
+	(else
+	 (pred v))))
+
+(define (improper-list-of pred)
+  (cut improper-every pred <>))
+
+(TEST
+ > (map (improper-list-of (string-of char-alphanumeric?))
+	'("foo" ("a" "b") ("a" . "b") ("a" . b) ("a" ("b"))))
+ (#t #t #t #f #f))
+
+
+(define char-one-of char-one-of?/)
+
+
