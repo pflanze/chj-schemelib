833749665ef441b0edb23a85961fa764518c6253
Christian Jaeger <ch@christianjaeger.ch>
2014-08-15 16:05:25 +0100

    de-gensymify gensyms for error messages

--- cj-typed.scm
+++ cj-typed.scm
@@ -17,35 +17,37 @@
 ;;;
 
 
-(define (cj-typed#type-check-error exprstr predstr w v)
+(define (cj-typed#type-check-error maybe-exprstr predstr w v)
   ;; v = value
   ;; w = result of predicate
-  (cond ((eq? w #f)
-	 (error (string-append exprstr
-			       " does not match "
-			       predstr
-			       ":")
-		v))
-	;; HACK: hand-coded predicate for |failure?| to avoid circular
-	;; dependency between fail.scm and cj-typed.scm
-	((and (vector? w)
-	      (> (vector-length w) 1)
-	      (eq? (vector-ref w 0) 'failure))
-	 ;; although the joke is that we need to rely on circular
-	 ;; dynamic linking for |failure.string| anyway... (but at
-	 ;; least reducing to just this one will allow type-check to
-	 ;; work without test.scm loaded).
-	 (error (string-append "does not match "
-			       predstr
-			       " "
-			       (failure.string w)
-			       ":")
-		v))
-	(else
-	 (error "predicate "
-		predstr
-		" returned invalid non-boolean value:"
-		w))))
+
+  (let ((err (lambda strs
+	       (error (apply string-append
+			     (if maybe-exprstr
+				 (string-append maybe-exprstr " "))
+			     "does not match "
+			     predstr
+			     strs)
+		      v))))
+    (cond ((eq? w #f)
+	   (err ":"))
+	  ;; HACK: hand-coded predicate for |failure?| to avoid circular
+	  ;; dependency between fail.scm and cj-typed.scm
+	  ((and (vector? w)
+		(> (vector-length w) 1)
+		(eq? (vector-ref w 0) 'failure))
+	   ;; although the joke is that we need to rely on circular
+	   ;; dynamic linking for |failure.string| anyway... (but at
+	   ;; least reducing to just this one will allow type-check to
+	   ;; work without test.scm loaded).
+	   (err " "
+		(failure.string w)
+		":"))
+	  (else
+	   (error "predicate "
+		  predstr
+		  " returned invalid non-boolean value:"
+		  w)))))
 
 
 (define-macro* (type-check predicate expr . body)
@@ -56,7 +58,17 @@
 	     (##if (##eq? ,W #t)
 		   (##let () ,@body)
 		   (cj-typed#type-check-error
-		    ,(scm:object->string (cj-desourcify expr))
+		    ,(let ((expr* (cj-desourcify expr)))
+		       ;; avoid putting gensyms into exception messages,
+		       ;; to make code using this testable.
+		       (if (cj-gensym? expr*)
+			   (cond ((cj-gensym-maybe-name expr*)
+				  => (lambda (name)
+				       (string-append "gensym '"
+						      (scm:object->string name))))
+				 (else
+				  #f))
+			   (scm:object->string expr*)))
 		    ,(scm:object->string (cj-desourcify predicate))
 		    ,W
 		    ,V)))))
