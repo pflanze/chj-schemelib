59cde780153c031dd67ff0f3c3dfa70e9af5a834
Christian Jaeger <ch@christianjaeger.ch>
2014-08-27 21:49:23 +0100

    add copyright statement

--- cj-io-util.scm
+++ cj-io-util.scm
@@ -1,3 +1,11 @@
+;;; Copyright 2013-2014 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
 (define (read-lines #!optional (p (current-input-port)) (tail '()))
   (let rec ()
     (let ((line (read-line p)))
