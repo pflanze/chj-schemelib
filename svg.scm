5d672470133333a9ebab80e143f424d3004776c9
Christian Jaeger <ch@christianjaeger.ch>
2014-09-08 00:44:34 +0100

    update

--- svg.scm
+++ svg.scm
@@ -6,6 +6,8 @@
 ;;;    (at your option) any later version.
 
 
+(require 2d-shape)
+
 (def. (exact.svg-string x)
   ;; integers are rational, too [in Scheme]
   ;; ah duh, even inexact numbers are rationals.
@@ -13,6 +15,7 @@
    (if (integer? x)
        x
        (exact->inexact x))))
+
 ;; > (exact? "f")
 ;; *** ERROR IN (console)@15.1 -- (Argument 1) NUMBER expected
 ;;wll. how does that type system work of mine  ?
@@ -35,11 +38,11 @@
      ;; mi,ma = min and max
      (let* ((fit (let* ((range (.- ma mi))
 			(stretch (../ size range)))
-		   (let-point
+		   (let-2d-point
 		    ((x0 x1) mi)
 		    (lambda (p)
-		      (let-point ((p0 p1) p)
-				 (..* (.- p mi) stretch)))))))
+		      (let-2d-point ((p0 p1) p)
+				    (..* (.- p mi) stretch)))))))
        `(svg
 	 (@
 	  (height ,svg-height)
@@ -47,38 +50,37 @@
 	 ,( ;;stream-map
 	   map
 	   (lambda (shape)
-	     (cond ((point? shape)
-		    (let ((p (fit shape)))
-		      `(circle (@ (cx ,(.svg-string (.x p)))
-				  (cy ,(.svg-string (.y p)))
-				  (r 1.5)
-				  (stroke "blue")
-				  (stroke-width 0)
-				  (fill "blue")))))
-		   ((Square? shape)
-		    `(path
-		      (@ (d ,(let* ((ps (.points shape))
-				    (p0 (car ps)))
-			       (cons (_svg-point "M" (fit p0))
-				     (fold-right (lambda (p r)
-						   (cons (_svg-point "L" (fit p))
-							 r))
-						 (_svg-point "L" (fit p0))
-						 (cdr ps)))))
-			 (fill "none")
-			 (stroke "black")
-			 (stroke-width "1"))))
-		   (else (error "unknown shape:" shape))))
+	     (xcond ((2d-point? shape)
+		     (let ((p (fit shape)))
+		       `(circle (@ (cx ,(.svg-string (.x p)))
+				   (cy ,(.svg-string (.y p)))
+				   (r 1.5)
+				   (stroke "blue")
+				   (stroke-width 0)
+				   (fill "blue")))))
+		    ((2d-square? shape)
+		     `(path
+		       (@ (d ,(let* ((ps (.points shape))
+				     (p0 (car ps)))
+				(cons (_svg-point "M" (fit p0))
+				      (fold-right (lambda (p r)
+						    (cons (_svg-point "L" (fit p))
+							  r))
+						  (_svg-point "L" (fit p0))
+						  (cdr ps)))))
+			  (fill "none")
+			  (stroke "black")
+			  (stroke-width "1"))))))
 	   shapes))))
 
 (def svg-path "out.svg")
 (def (viewsvg shapes)
-  ;; ah want regenerate stream(s) maybe? not cache? well. how to say har.
-  (let* ((p0 (.start (car (force shapes)))))
-    (let-pair ((mi ma) (stream-fold-left .min+maxs/prev
-					 (cons p0 p0)
-					 shapes))
-	      (sxml>>pretty-xml-file (svg (point svg-width
-						 svg-height)
-					  mi ma shapes)
-				     svg-path))))
+     ;; ah want regenerate stream(s) maybe? not cache? well. how to say har.
+     (let* ((p0 (.start (car (force shapes)))))
+       (let-pair ((mi ma) (stream-fold-left .min+maxs/prev
+					    (cons p0 p0)
+					    shapes))
+		 (sxml>>pretty-xml-file (svg (2d-point svg-width
+						       svg-height)
+					     mi ma shapes)
+					svg-path))))
