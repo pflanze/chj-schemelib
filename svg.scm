aa5a44afe353988d64faab6bce958835f557b2ff
Christian Jaeger <ch@christianjaeger.ch>
2014-09-12 06:02:47 +0100

    add xmlns, grrr

--- svg.scm
+++ svg.scm
@@ -114,7 +114,9 @@
 		     (lambda (p)
 		       (..* (.- p mi) stretch))))))
        `(svg
-	 (@ (height ,svg-height)
+	 (@ (xmlns "http://www.w3.org/2000/svg")
+	    (xmlns:xlink "http://www.w3.org/1999/xlink")
+	    (height ,svg-height)
 	    (width ,svg-width)
 	    ,(and background-color
 		  `(style ,(string-append
