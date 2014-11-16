6d5ebd813b5cdcb749e67e5323e2695a58f15792
Christian Jaeger <ch@christianjaeger.ch>
2014-11-16 04:49:52 +0100

    color: add 'paint' (paintoptions) type, rename 'colored' to 'painted'

--- color.scm
+++ color.scm
@@ -10,25 +10,57 @@
 	 rgb colorspaces)
 
 
+;; color =================================================================
+
 (class colorstring
        (struct #(string? value))
-       (method html-colorstring colorstring.value))
+       (method html-colorstring colorstring.value)
+
+       (method maybe-stroke-color identity)
+       (method maybe-fill-color identity)
+       (method maybe-stroke-width false/1))
 
 ;; change/parametrize when other solutions abound
 (def color? (either colorstring? rgb?))
 
-(defstruct colors
-  #(color? stroke)
-  #(color? fill))
+;; add methods to existing class (monkey patching)
+(class rgb
+       (method maybe-stroke-color identity)
+       (method maybe-fill-color identity)
+       (method maybe-stroke-width false/1))
 
-(defstruct colored
-  #((either color? colors?) color)
+;; paintoptions ==========================================================
+
+(class colors
+       (struct #(color? stroke)
+	       #(color? fill))
+       (method maybe-stroke-color colors.stroke)
+       (method maybe-fill-color colors.fill)
+       (method maybe-stroke-width false/1))
+
+;; with stroke width, and optional keyword parameters
+(class paint
+       (struct #!key
+	       #((maybe color?) stroke-color)
+	       #((maybe color?) fill-color)
+	       #((maybe positive-real?) stroke-width))
+       (method maybe-stroke-color paint.stroke-color)
+       (method maybe-fill-color paint.fill-color)
+       (method maybe-stroke-width paint.stroke-width))
+
+(def paintoptions?
+     (either color? colors? paint?))
+
+;; applying it ===========================================================
+
+(defstruct painted
+  #((improper-list-of paintoptions?) optionS)
   value)
 
 ;; delegates, now they're coming?..
-(def. (colored.start v)
-  (.start (colored.value v)))
+(def. (painted.start v)
+  (.start (painted.value v)))
 
-(def. (colored.min+maxs/prev a b)
-  (.min+maxs/prev (colored.value a) b))
+(def. (painted.min+maxs/prev a b)
+  (.min+maxs/prev (painted.value a) b))
 
