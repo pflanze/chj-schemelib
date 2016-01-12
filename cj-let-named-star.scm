86ded8e33633a020cdf16fdaaa4b14bdaa4c36d4
Christian Jaeger <ch@christianjaeger.ch>
2016-01-12 15:52:07 +0100

    cj-let-named-star: adapt
    Making it hygienically unsafe, as always, by way of missing full
    qualification now. Forever.

--- cj-let-named-star.scm
+++ cj-let-named-star.scm
@@ -1,60 +1,65 @@
-; Thu, 14 Jul 2005 00:07:30 +0200
 
-;;(save multiple occurences of ',vars-flat in the generated macro?)
-
-;;((btw lang layers?))
-
-(define-macro (let-named* ident bindings . body)
-  (let* ((vars-flat (map (lambda(v)
-			   (or (pair? v)
-			       (error "let-named*: pair expected in bindings:" v))
-			   (car v))
-			 bindings))
-	 (loop (gensym 'let-named*-loop-))
-	 (submacro `(##define-macro
-		     (,ident . args)
-		     ;; build an alist  varname -> syntax
-		     (let ((alis (let loop ((l '())
-					    (a args))
-				   (cond ((##pair? a)
-					  (or (##pair? (##cdr a))
-					      (gambit-interpreter-env#error "let-named* call: identifyer must be called with an even number of arguments: " ',ident args))
-					  (let ((key (##car a))
-						(val (##cadr a)))
-					    (or (##keyword? key)
-						(gambit-interpreter-env#error "let-named* call: expecting keyword in identifyer call: " ',ident key))
-					    (let ((varnam (##string->symbol
-							   (##keyword->string key))))
-					      (or (##member varnam ',vars-flat)
-						  (gambit-interpreter-env#error "let-named* call: no such variable:" ',ident varnam))
-					      (and (##assoc varnam l)
-						   (gambit-interpreter-env#error "let-named* call: same keyword given twice: " ',ident varnam))
-					      (loop (##cons (##cons varnam val) l)
-						    (##cddr a)))))
-					 (else l)))))
-		       (##cons ',loop
-			       (##map (lambda(v)
-					(cond ((##assoc v alis)
-					       => ##cdr)
-					      (else v))) ',vars-flat))))))
+(define-macro* (let-named* ident bindings . body)
+  (let* ((vars-flat
+	  (map (lambda(v)
+		 (assert* pair? v
+			  car))
+	       (source-code bindings)))
+	 
+	 (loop
+	  (gensym 'let-named*-loop-))
+	 
+	 (submacro
+	  `(##define-macro
+	    (,ident . args)
+	    ;; build an alist  varname -> syntax
+	    (let ((alis
+		   (let loop ((l '())
+			      (a args))
+		     (cond ((pair? a)
+			    (or (pair? (cdr a))
+				(error "let-named* call: identifyer must be called with an even number of arguments: "
+				       ',ident args))
+			    (let ((key (car a))
+				  (val (cadr a)))
+			      (or (keyword? key)
+				  (error "let-named* call: expecting keyword in identifyer call: "
+					 ',ident key))
+			      (let ((varnam (string->symbol
+					     (keyword->string key))))
+				(or (member varnam ',vars-flat)
+				    (error "let-named* call: no such variable:"
+					   ',ident varnam))
+				(and (assoc varnam l)
+				     (error "let-named* call: same keyword given twice: "
+					    ',ident varnam))
+				(loop (cons (cons varnam val) l)
+				      (cddr a)))))
+			   (else l)))))
+	      (cons ',loop
+		    (map (lambda(v)
+			   (cond ((assoc v alis)
+				  => cdr)
+				 (else v))) ',vars-flat))))))
     `(let ,loop ,bindings
 	  ,submacro
 	  ,@body)))
 
-;;(Note: non-namespacesafe identifyiers: quasiquote, and the usual syntax like cond let etc.)
 
-;simple test:
-; > (let-named* fun ((a 3)(b 4)) (println "a:" a ", b:" b) (if (> b 0) (fun b: (- b 1)) a))
-; a:3, b:4
-; a:3, b:3
-; a:3, b:2
-; a:3, b:1
-; a:3, b:0
-; 3
-; > (let-named* fun ((a 3)(b 4)) (println "a:" a ", b:" b) (if (> b 0) (fun b: (- b 1) a: (modulo b 2)) a))
-; a:3, b:4
-; a:0, b:3
-; a:1, b:2
-; a:0, b:1
-; a:1, b:0
-; 1
+(TEST
+ > (let* ((l '())
+	  (res (let-named* fun ((a 3)(b 4))
+			   (push! l (list a b))
+			   (if (> b 0)
+			       (fun b: (- b 1))
+			       a))))
+     (vector (reverse l) res))
+ #(((3 4) (3 3) (3 2) (3 1) (3 0)) 3)
+ > (let* ((l '())
+	  (res (let-named* fun ((a 3)(b 4))
+			   (push! l (list a b))
+			   (if (> b 0)
+			       (fun b: (- b 1) a: (modulo b 2))
+			       a))))
+     (vector (reverse l) res))
+ #(((3 4) (0 3) (1 2) (0 1) (1 0)) 1))
