bc0d2791efa08a2fb211c96c3bdf0e547140c5eb
Christian Jaeger <ch@christianjaeger.ch>
2016-02-05 12:31:52 +0100

    maybe: add if-maybe, maybe:if, maybe:cond
    but not sure what the best syntax would be. Really need some generic
    match.

--- maybe.scm
+++ maybe.scm
@@ -40,3 +40,50 @@
  > (just.value (.value (just (just 13))))
  13)
 
+
+
+(def (if-maybe #(maybe? v) then else)
+     (if (just? v)
+	 (then (just.value v))
+	 (else)))
+
+
+(defmacro (maybe:if t
+		    then
+		    #!optional
+		    else)
+  `(if-maybe ,t ,then (lambda () ,(or else `(void)))))
+
+(defmacro (maybe:cond t+then #!optional else)
+  (mcase t+then
+	 (`(`t => `then)
+	  `(if-maybe ,t
+		     ,then
+		     (lambda ()
+		       ,(if else
+			    (mcase else
+				   (`(else `else)
+				    else))
+			    `(void)))))))
+
+(TEST
+ > (def (psqrt x)
+	(if (positive? x)
+	    (just (sqrt x))
+	    (nothing)))
+ > (def (f x)
+	(maybe:if (psqrt x)
+		  inc
+		  'n))
+ > (def (g x)
+	(maybe:cond ((psqrt x) => inc)
+		    (else 'n)))
+ > (map (lambda (x)
+	  (list (f x)
+		(g x)))
+	(list 4 9 -4))
+ ((3 3)
+  (4 4)
+  (n n))
+ )
+
