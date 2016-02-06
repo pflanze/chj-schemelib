4da1bc79c70adfb97aa3ecfabad56868e1504f24
Christian Jaeger <ch@christianjaeger.ch>
2016-02-06 16:27:42 +0100

    memoize: accept optional table argument

--- memoize.scm
+++ memoize.scm
@@ -6,15 +6,14 @@
 ;;;    (at your option) any later version.
 
 
-(define (memoize f)
-  (let ((t (make-table))
-	(nothing (cons 1 2)))
+(define (memoize f #!optional (table (make-table)))
+  (let ((nothing (cons 1 2)))
     (lambda vs
-      (let ((v? (table-ref t vs nothing)))
+      (let ((v? (table-ref table vs nothing)))
 	(if (eq? v? nothing)
 	    (let ((v (apply f vs)))
 	      ;; XX: make multi-threading safe
-	      (table-set! t vs v)
+	      (table-set! table vs v)
 	      v)
 	    v?)))))
 
