50b571e24c20c9d3ecd8c39d7431e4fb4bd1d790
Christian Jaeger <ch@christianjaeger.ch>
2016-02-19 23:52:31 +0100

    create failing.scm

---
+++ failing.scm
@@ -0,0 +1,32 @@
+
+;; Module versions to toggle predicates between normal and
+;; exception-throwing modes
+
+(require easy)
+
+;; XX ugly. Because of the letrec like behaviour (assumption) in
+;; define-module body (re exported symbols, but really all of them
+;; right? because of my hacking around those Scheme/Gambit limits),
+;; need to pass in all original symbols explicitely. And bind them to
+;; different names.
+
+(defmodule (<failing> failing
+		      orig:equal?
+		      orig:source-equal?)
+  (export equal? source-equal?)
+  
+  (def equal? (failing orig:equal?))
+  (def source-equal? (failing orig:source-equal?)))
+
+
+(def (raise-failures f)
+     (lambda args
+       (or (apply f args)
+	   (error "function returned false"))))
+
+
+(def <failing-on> (<failing> raise-failures
+			     equal? source-equal?))
+(def <failing-off> (<failing> identity
+			      equal? source-equal?))
+
