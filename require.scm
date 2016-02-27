ae0f940708e795e202cf3599a6ce3738cd335e3b
Christian Jaeger <ch@christianjaeger.ch>
2016-02-27 17:48:45 +0100

    create tsort.scm and require.scm

---
+++ require.scm
@@ -0,0 +1,125 @@
+;; 'parse them', sort them, kr(ypton)
+
+(require easy
+	 test
+	 tsort
+	 Status)
+
+
+(def (require-decl.modulename v)
+     (mcase v
+	    (symbol?
+	     (source-code v))
+	    (pair?
+	     (source-code (car (source-code v))))))
+
+;; module-basename (vs. full module name in the future?)
+(def (path-string.modulename p)
+     (basename p ".scm"))
+
+
+(def modules-without-require-forms
+     '(cj-source
+       define-macro-star
+       dummy-module
+       vector-util-1
+
+       ;; lib/mod/ :
+       config-example
+       gambit
+       imperative-load-tree
+       lib
+       mod
+       monad
+       monadic-load-tree
+       remote
+       usersyntax))
+
+(def (path-string.topo-relation p)
+     (let ((form (call-with-input-file p read))
+	   (mname (string.symbol (path-string.modulename p))))
+       (mcase form
+	      (`(require . `rest)
+	       (topo-relation mname
+			      (map require-decl.modulename rest)))
+	      (else
+	       (if (memq mname modules-without-require-forms)
+		   (topo-relation mname '())
+		   (error "file does not have a require form as its first form:"
+			  p))))))
+
+(TEST
+ ;; well those are evil of course, will break upon module changes
+ > (path-string.topo-relation "require.scm")
+ #(topo-relation require (easy test tsort Status))
+ ;; > (path-string.topo-relation "tsort.scm")
+ ;; #(topo-relation tsort (easy test list-ref))
+ )
+
+(def (modulepaths-tsort paths)
+     (let* ((rs (map path-string.topo-relation paths))
+	    (z (map cons (map .name rs) paths)))
+       (map (C symbol-alist-ref z _) (topo.sort* rs))))
+
+
+(def (modulepaths-in-dir dir)
+     (parameterize
+      ((current-directory dir))
+      (map path-normalize
+	   (xcall-with-input-process
+	    (list path: "gls" arguments: '("*.scm"))
+      
+	    read-lines))))
+
+
+(def (lib)
+     (modulepaths-tsort (modulepaths-in-dir "lib")))
+(def (mydb)
+     (modulepaths-tsort (modulepaths-in-dir "mydb")))
+
+
+;; a single-dependency representation
+(class requires
+       (struct name dependency))
+
+(def (modulepaths-satisfying? paths) -> Status?
+     (call/cc
+      (lambda (report)
+	(let* ((all-rs (map path-string.topo-relation paths)))
+	  ;; nw how. wat's already loaded  walk through.
+	  (let lp ((rs all-rs)
+		   (loaded '()))
+	    (if (null? rs)
+		(Success)
+		(let-pair
+		 ((r rs*) rs)
+		 (for-each
+		  (lambda (d)
+		    (unless (memq d loaded)
+			    (report (Failure
+				     (requires (.name r) d)))))
+		  (.deps r))
+		 (lp rs*
+		     (cons (.name r) loaded)))))))))
+
+(TEST
+ > (modulepaths-satisfying? '("lib/Maybe.scm" "lib/easy.scm"))
+ #(Failure #(requires Maybe easy))
+ > (modulepaths-satisfying? '("lib/easy.scm" "lib/Maybe.scm"))
+ #(Failure #(requires easy define-macro-star))
+ > (modulepaths-satisfying? '("lib/define-macro-star.scm"))
+ #(Success)
+ )
+
+
+
+(def (load.scm-files)
+     (map (C string-append _ ".scm")
+	  (filter string?
+		  (flatten
+		   (call-with-input-file ".gambc/load.scm" read-all)))))
+
+(def (check-load.scm)
+     (modulepaths-satisfying?
+      (filter (C string-starts-with? _ "lib/") (load.scm-files))))
+
---
+++ tsort.scm
@@ -0,0 +1,118 @@
+(require easy
+	 test
+	 list-ref
+	 cj-seen
+	 )
+
+
+(class topo-relation
+       (struct #(symbol? name)
+	       #((list-of symbol?) deps)))
+
+;; topo:Maybe-ref, topo:ref
+(modimport/prefix topo: (<list-ref> symbol?
+				    topo-relation.name
+				    eq?))
+
+(def topo? (list-of topo-relation?))
+;; XX must .name be unique in each topo? ?
+
+(def (topo.sort rs) -> (list-of topo-relation?)
+     (letv
+      ((processed? processed!) (make-seen?+!))
+      (letv
+       ((seen? seen!) (make-seen?+!))
+       (let ((get (C topo:ref rs _))
+	     (out '())
+	     (all-rs rs))
+	 (let load ((rs rs))
+	   (for-each
+	    (lambda (r)
+	      (let-topo-relation
+	       ((name deps) r)
+	       (if (seen? name)
+		   (unless (processed? name)
+			   (error "cycle detected involving:" name))
+		   (begin
+		     (seen! name)
+		     (load (map get deps))
+		     (push! out r)
+		     (processed! name)))))
+	    rs))
+	 (reverse out)))))
+
+(def (topo.sort* rs) -> (list-of symbol?)
+     (map .name (topo.sort rs)))
+
+
+(TEST
+ > (def rs (list
+	    (topo-relation 'a '(c))
+	    (topo-relation 'b '())
+	    (topo-relation 'c '())
+	    (topo-relation 'd '())
+	    (topo-relation 'e '(a c))
+	    (topo-relation 'f '(b c d))
+	    (topo-relation 'g '(e))
+	    (topo-relation 'h '())
+	    (topo-relation 'i '(h))))
+ > (def sorted-ns (topo.sort* rs))
+ > sorted-ns
+ (c a b d e f g h i)
+ > (def (c . ns)
+	(let ((l (filter (lambda (n)
+			   (memq n ns))
+			 sorted-ns)))
+	  (if (equal? ns l)
+	      (if (equal? ns (reverse l))
+		  'eq
+		  'lt)
+	      'gt)))
+ > (c 'a 'e)
+ lt
+ > (c 'e 'a)
+ gt
+ > (c 'e 'h)
+ lt ;; eq well. does e need h or something? h needs nothing, e needs a
+    ;; c which don't need it. eq would be correct.
+ > (c 'e 'e)
+ gt ;; huh;  eq ;; ok?  
+ > (c 'a 'c)
+ gt ;; eq  well.
+ > (c 'e 'g)
+ lt
+ > (c 'c 'g)
+ lt ;; indirect dependency
+ )
+
+(TEST
+ > (cmp-sort '(a b) c)
+ (a b)
+ > (cmp-sort '(a c) c)
+ (c a)
+ > (cmp-sort '(b f) c)
+ (b f)
+ > (cmp-sort '(f b) c)
+ (b f)
+ > (cmp-sort '(f b a) c)
+ (a b f) ;; was (b f a) correct, too?
+
+ > (topo.sort* (cons* (topo-relation 'a '(c)) ;;sameas before
+		      (topo-relation 'b '(a))
+		      (cddr rs)))
+ (c a b d e f g h i)
+ > (topo.sort* (cons* (topo-relation 'a '(c)) ;;sameas before
+		      (topo-relation 'b '(e))
+		      (cddr rs)))
+ (c a e b d f g h i))
+
+
+(TEST
+ > (%try-error
+    (topo.sort* (list (topo-relation 'a '(b)) (topo-relation 'b '(a)))))
+ #(error "cycle detected involving:" a))
+
+
+;; XX add rule based tests
+
+
