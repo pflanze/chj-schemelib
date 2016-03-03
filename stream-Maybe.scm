2fb666618b3058fa620bb3d489eec427d6120d3f
Christian Jaeger <ch@christianjaeger.ch>
2016-03-03 18:56:12 +0100

    create stream-Maybe

---
+++ stream-Maybe.scm
@@ -0,0 +1,32 @@
+(require easy
+	 Maybe
+	 (list-util let-pair)
+	 (lazy FV)
+	 (cj-env-2 C)
+	 test)
+
+
+;; only 1-ary for now
+(def (stream-mapfilter/tail fn tail l)
+     (FV (l)
+	 (if (null? l)
+	     tail
+	     (let-pair ((a l*) l)
+		       (let ((rest (& (stream-mapfilter/tail fn tail l*))))
+			 (Maybe:cond ((fn a) =>
+				      (lambda (v)
+					(cons v
+					      (rest))))
+				     (else
+				      (rest))))))))
+
+
+;; only 1-ary for now
+(def (stream-mapfilter fn l)
+     (stream-mapfilter/tail fn '() l))
+
+
+(TEST
+ > (stream-mapfilter (lambda (v) (if (even? v) (Just v) (Nothing))) '(1 3 4 -2 -1))
+ (4 -2))
+
