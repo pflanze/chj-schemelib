11eb6c57348df42389ce57b3a2f778a33d3c5ee6
Christian Jaeger <ch@christianjaeger.ch>
2016-03-14 08:51:11 +0100

    add copyright/license statements

--- list-ref-test.scm
+++ list-ref-test.scm
@@ -1,3 +1,9 @@
+;;; Copyright 2016 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
 
 ;; Tests for list-ref.scm
 
--- list-ref.scm
+++ list-ref.scm
@@ -1,3 +1,11 @@
+;;; Copyright 2016 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
 ;; Unlike _-alist-ref doen't hard-code the pairings using pairs,
 ;; instead use parametrized accessors
 
