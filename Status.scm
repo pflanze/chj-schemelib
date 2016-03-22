acb51f5c0fd904dea19c47ea72b38035fc574828
Christian Jaeger <ch@christianjaeger.ch>
2016-03-22 20:18:46 +0100

    Status: renamings (removing macro again)
     Success   -> Success*
     Success/0 -> Success
     Success/1 -> Result
    
    BTW this change makes Status backwards compatible to how it was before
    the 'split'. (That wasn't the reason for these new names, though.)

--- Status.scm
+++ Status.scm
@@ -13,16 +13,19 @@
 ;; It could easily be done by using Maybe as meaning maybe an error,
 ;; but to avoid the possible confusion, define a new type.
 
+;; This also actually has 3 cases now, Success without result, and
+;; Result with a result (still representing Success*).
+
 (require easy
 	 more-oo
 	 test)
 
 ;; how do Perl6 or so call them?
 (class Status
-       (subclass Success
-		 (subclass Success/0
-			   (struct constructor-name: _Success/0))
-		 (subclass Success/1
+       (subclass Success*
+		 (subclass Success
+			   (struct constructor-name: _Success))
+		 (subclass Result
 			   (struct value)))
        (subclass Failure
 		 ;; could call this .reason, but perhaps should stay
@@ -30,52 +33,45 @@
 		 (struct value)))
 
 ;; optimization:
-(def __Success/0 (_Success/0))
-(def (Success/0)
-     __Success/0)
-
-(defmacro (Success . args)
-  (if (null? args)
-      `__Success/0 ;; `(Success/0) ?
-      (if (null? (cdr args))
-	  `(Success/1 ,(car args))
-	  (source-error stx "too many arguments"))))
+(def __Success (_Success))
+(def (Success)
+     __Success)
 
 
 (TEST
  > (eq? (Success) (Success))
  #t
  > (map (lambda (v)
-	  (map (C _ v) (list Status? Success? Failure?
+	  (map (C _ v) (list Status? Success*? Success? Failure?
 			     (lambda (v)
 			       (if (Failure? v)
 				   (Failure.value v)
-				   (if (Success/1? v)
-				       (.value v)
+				   (if (Result? v)
+				       (Result.value v)
 				       'n))))))
 	(list #f
 	      (values)
 	      (Success)
-	      (Success 21)
+	      (Result 21)
 	      (Failure 1)
 	      (Failure #f)
 	      (Failure (Success)) ;; hihi
 	      (Failure (Failure 13))))
- ((#f #f #f n)
-  (#f #f #f n)
-  (#t #t #f n)
-  (#t #t #f 21)
-  (#t #f #t 1)
-  (#t #f #t #f)
-  (#t #f #t #(Success/0))
-  (#t #f #t #(Failure 13)))
+ ((#f #f #f #f n)
+  (#f #f #f #f n)
+  (#t #t #t #f n)
+  (#t #t #f #f 21)
+  (#t #f #f #t 1)
+  (#t #f #f #t #f)
+  (#t #f #f #t #(Success))
+  (#t #f #f #t #(Failure 13)))
  > (Failure.value (.value (Failure (Failure 13))))
  13)
 
 
 
 (def-inline (if-Status #(Status? v) success failure)
-  (if (Success? v) ;; XXX Success/1 ? and pass value?
+  (if (Success? v) ;; XXX Result ? and pass value?
       (success)
       (failure (Failure.value v))))
 
@@ -113,29 +109,29 @@
  foo)
 
 
-(def (Status/0 pred-failure)
+(def (Status/Success pred-failure)
      (lambda (v)
-       (or (Success/0? v)
+       (or (Success? v)
 	   (and (Failure? v)
 		(pred-failure (Failure.value v))))))
 
-(def (Status/1 pred-success pred-failure)
+(def (Status/Result pred-result pred-failure)
      (lambda (v)
-       (or (and (Success/1? v)
-		(pred-success (Success/1.value v)))
+       (or (and (Result? v)
+		(pred-result (Result.value v)))
 	   (and (Failure? v)
 		(pred-failure (Failure.value v))))))
 
 (TEST
  > (def l
 	(list (Success)
-	      (Success 5)
-	      (Success 'x)
+	      (Result 5)
+	      (Result 'x)
 	      10
 	      (Failure 10)
 	      (Failure 'y)))
- > (map (Status/0 integer?) l)
+ > (map (Status/Success integer?) l)
  (#t #f #f  #f #t #f)
- > (map (Status/1 symbol? integer?) l)
+ > (map (Status/Result symbol? integer?) l)
  (#f #f #t  #f #t #f))
 
