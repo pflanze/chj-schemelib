435c0d9fe9011ef33889fafca3d5ae9ab99b0238
Christian Jaeger <ch@christianjaeger.ch>
2016-03-22 19:13:16 +0100

    add copyright, mention other file

--- Maybe.scm
+++ Maybe.scm
@@ -1,3 +1,13 @@
+;;; Copyright 2016 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+;; Also see Status.scm
+
 (require easy
 	 more-oo
 	 test)
--- Status.scm
+++ Status.scm
@@ -1,3 +1,11 @@
+;;; Copyright 2016 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
 ;; Very similar to Maybe: wrapper with two cases of which one holds a
 ;; value; but instead of carrying a value in the positive case, this
 ;; carries a value in the negative case.
