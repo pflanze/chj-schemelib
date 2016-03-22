831bc91b992a1dd2cda2b1e2857128218bf68c31
Christian Jaeger <ch@christianjaeger.ch>
2016-03-22 19:40:42 +0100

    Status: split Success into Success/0 and Success/1

--- Status.scm
+++ Status.scm
@@ -13,8 +13,6 @@
 ;; It could easily be done by using Maybe as meaning maybe an error,
 ;; but to avoid the possible confusion, define a new type.
 
-;; Also see Result which carries a value in either case. Hm.
-
 (require easy
 	 more-oo
 	 test)
@@ -22,16 +20,27 @@
 ;; how do Perl6 or so call them?
 (class Status
        (subclass Success
-		 (struct constructor-name: _Success))
+		 (subclass Success/0
+			   (struct constructor-name: _Success/0))
+		 (subclass Success/1
+			   (struct value)))
        (subclass Failure
 		 ;; could call this .reason, but perhaps should stay
 		 ;; compatible with Maybe ?
 		 (struct value)))
 
 ;; optimization:
-(def __Success (_Success))
-(def (Success)
-     __Success)
+(def __Success/0 (_Success/0))
+(def (Success/0)
+     __Success/0)
+
+(defmacro (Success . args)
+  (if (null? args)
+      `__Success/0 ;; `(Success/0) ?
+      (if (null? (cdr args))
+	  `(Success/1 ,(car args))
+	  (source-error stx "too many arguments"))))
+
 
 (TEST
  > (eq? (Success) (Success))
@@ -41,10 +50,13 @@
 			     (lambda (v)
 			       (if (Failure? v)
 				   (Failure.value v)
-				   'n)))))
+				   (if (Success/1? v)
+				       (.value v)
+				       'n))))))
 	(list #f
 	      (values)
 	      (Success)
+	      (Success 21)
 	      (Failure 1)
 	      (Failure #f)
 	      (Failure (Success)) ;; hihi
@@ -52,9 +64,10 @@
  ((#f #f #f n)
   (#f #f #f n)
   (#t #t #f n)
+  (#t #t #f 21)
   (#t #f #t 1)
   (#t #f #t #f)
-  (#t #f #t #(Success))
+  (#t #f #t #(Success/0))
   (#t #f #t #(Failure 13)))
  > (Failure.value (.value (Failure (Failure 13))))
  13)
@@ -62,7 +75,7 @@
 
 
 (def-inline (if-Status #(Status? v) success failure)
-  (if (Success? v)
+  (if (Success? v) ;; XXX Success/1 ? and pass value?
       (success)
       (failure (Failure.value v))))
 
@@ -100,14 +113,29 @@
  foo)
 
 
-(def (Status pred)
+(def (Status/0 pred-failure)
      (lambda (v)
-       (or (Success? v)
+       (or (Success/0? v)
 	   (and (Failure? v)
-		(pred (Failure.value v))))))
+		(pred-failure (Failure.value v))))))
+
+(def (Status/1 pred-success pred-failure)
+     (lambda (v)
+       (or (and (Success/1? v)
+		(pred-success (Success/1.value v)))
+	   (and (Failure? v)
+		(pred-failure (Failure.value v))))))
 
 (TEST
- > (def Status-integer? (Status integer?))
- > (map Status-integer? (list (Success) 10 (Failure 10)))
- (#t #f #t))
+ > (def l
+	(list (Success)
+	      (Success 5)
+	      (Success 'x)
+	      10
+	      (Failure 10)
+	      (Failure 'y)))
+ > (map (Status/0 integer?) l)
+ (#t #f #f  #f #t #f)
+ > (map (Status/1 symbol? integer?) l)
+ (#f #f #t  #f #t #f))
 
