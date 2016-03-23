920bf6d75d1544e966eb8dc441c9b66e6f573569
Christian Jaeger <ch@christianjaeger.ch>
2016-03-23 14:18:08 +0100

    oo-vector-lib: add copyright, and *huge* export statement
    with 334 entries

--- oo-vector-lib.scm
+++ oo-vector-lib.scm
@@ -1,6 +1,372 @@
+;;; Copyright 2014-2016 by Christian Jaeger <chrjae@gmail.com>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
 (require test
 	 code-map
 	 easy-1)
+
+(export sum
+
+	string->u8vector string.u8vector ;; XX remove this?
+	u8vector.string			 ;; dito?
+
+	;; XX why all the "-" versions instead of just the "." versions?
+
+	string.ref
+	string.set!
+	string.length
+	string.list
+	list.string
+	string.null?
+	;; string-inc!
+	;; string-set!
+	;; string.inc!
+	;; string-dec!
+	;; string.dec!
+	;; string.sum
+	string-map/iota
+	string.map/iota
+	string.chop-both-ends
+	string-for-each
+	string.for-each
+	string.map
+	strings-map
+	string-map
+	string-map*
+	string-fold-right
+	string.fold-right
+	string-fold
+	string.fold
+	strings-append
+	pair-with-car-string?
+	pair-with-car-string.append
+
+	;; the remaining ones have more variants
+	vector.ref
+	vector.set!
+	vector.length
+	vector.list
+	list.vector
+	vector.null?
+	vector-inc!
+	vector-set!
+	vector.inc!
+	vector-dec!
+	vector.dec!
+	vector.sum
+	vector-map/iota
+	vector.map/iota
+	vector.chop-both-ends
+	vector-for-each
+	vector.for-each
+	vector.map
+	vectors-map
+	vector-map
+	vector-map*
+	vector-fold-right
+	vector.fold-right
+	vector-fold
+	vector.fold
+	vectors-append
+	pair-with-car-vector?
+	pair-with-car-vector.append
+
+	f32vector.ref
+	f32vector.set!
+	f32vector.length
+	f32vector.list
+	list.f32vector
+	f32vector.null?
+	f32vector-inc!
+	f32vector-set!
+	f32vector.inc!
+	f32vector-dec!
+	f32vector.dec!
+	f32vector.sum
+	f32vector-map/iota
+	f32vector.map/iota
+	f32vector.chop-both-ends
+	f32vector-for-each
+	f32vector.for-each
+	f32vector.map
+	f32vectors-map
+	f32vector-map
+	f32vector-map*
+	f32vector-fold-right
+	f32vector.fold-right
+	f32vector-fold
+	f32vector.fold
+	f32vectors-append
+	pair-with-car-f32vector?
+	pair-with-car-f32vector.append
+
+	f64vector.ref
+	f64vector.set!
+	f64vector.length
+	f64vector.list
+	list.f64vector
+	f64vector.null?
+	f64vector-inc!
+	f64vector-set!
+	f64vector.inc!
+	f64vector-dec!
+	f64vector.dec!
+	f64vector.sum
+	f64vector-map/iota
+	f64vector.map/iota
+	f64vector.chop-both-ends
+	f64vector-for-each
+	f64vector.for-each
+	f64vector.map
+	f64vectors-map
+	f64vector-map
+	f64vector-map*
+	f64vector-fold-right
+	f64vector.fold-right
+	f64vector-fold
+	f64vector.fold
+	f64vectors-append
+	pair-with-car-f64vector?
+	pair-with-car-f64vector.append
+
+	u8vector.ref
+	u8vector.set!
+	u8vector.length
+	u8vector.list
+	list.u8vector
+	u8vector.null?
+	u8vector-inc!
+	u8vector-set!
+	u8vector.inc!
+	u8vector-dec!
+	u8vector.dec!
+	u8vector.sum
+	u8vector-map/iota
+	u8vector.map/iota
+	u8vector.chop-both-ends
+	u8vector-for-each
+	u8vector.for-each
+	u8vector.map
+	u8vectors-map
+	u8vector-map
+	u8vector-map*
+	u8vector-fold-right
+	u8vector.fold-right
+	u8vector-fold
+	u8vector.fold
+	u8vectors-append
+	pair-with-car-u8vector?
+	pair-with-car-u8vector.append
+
+	s8vector.ref
+	s8vector.set!
+	s8vector.length
+	s8vector.list
+	list.s8vector
+	s8vector.null?
+	s8vector-inc!
+	s8vector-set!
+	s8vector.inc!
+	s8vector-dec!
+	s8vector.dec!
+	s8vector.sum
+	s8vector-map/iota
+	s8vector.map/iota
+	s8vector.chop-both-ends
+	s8vector-for-each
+	s8vector.for-each
+	s8vector.map
+	s8vectors-map
+	s8vector-map
+	s8vector-map*
+	s8vector-fold-right
+	s8vector.fold-right
+	s8vector-fold
+	s8vector.fold
+	s8vectors-append
+	pair-with-car-s8vector?
+	pair-with-car-s8vector.append
+
+	u16vector.ref
+	u16vector.set!
+	u16vector.length
+	u16vector.list
+	list.u16vector
+	u16vector.null?
+	u16vector-inc!
+	u16vector-set!
+	u16vector.inc!
+	u16vector-dec!
+	u16vector.dec!
+	u16vector.sum
+	u16vector-map/iota
+	u16vector.map/iota
+	u16vector.chop-both-ends
+	u16vector-for-each
+	u16vector.for-each
+	u16vector.map
+	u16vectors-map
+	u16vector-map
+	u16vector-map*
+	u16vector-fold-right
+	u16vector.fold-right
+	u16vector-fold
+	u16vector.fold
+	u16vectors-append
+	pair-with-car-u16vector?
+	pair-with-car-u16vector.append
+
+	s16vector.ref
+	s16vector.set!
+	s16vector.length
+	s16vector.list
+	list.s16vector
+	s16vector.null?
+	s16vector-inc!
+	s16vector-set!
+	s16vector.inc!
+	s16vector-dec!
+	s16vector.dec!
+	s16vector.sum
+	s16vector-map/iota
+	s16vector.map/iota
+	s16vector.chop-both-ends
+	s16vector-for-each
+	s16vector.for-each
+	s16vector.map
+	s16vectors-map
+	s16vector-map
+	s16vector-map*
+	s16vector-fold-right
+	s16vector.fold-right
+	s16vector-fold
+	s16vector.fold
+	s16vectors-append
+	pair-with-car-s16vector?
+	pair-with-car-s16vector.append
+
+	u32vector.ref
+	u32vector.set!
+	u32vector.length
+	u32vector.list
+	list.u32vector
+	u32vector.null?
+	u32vector-inc!
+	u32vector-set!
+	u32vector.inc!
+	u32vector-dec!
+	u32vector.dec!
+	u32vector.sum
+	u32vector-map/iota
+	u32vector.map/iota
+	u32vector.chop-both-ends
+	u32vector-for-each
+	u32vector.for-each
+	u32vector.map
+	u32vectors-map
+	u32vector-map
+	u32vector-map*
+	u32vector-fold-right
+	u32vector.fold-right
+	u32vector-fold
+	u32vector.fold
+	u32vectors-append
+	pair-with-car-u32vector?
+	pair-with-car-u32vector.append
+
+	s32vector.ref
+	s32vector.set!
+	s32vector.length
+	s32vector.list
+	list.s32vector
+	s32vector.null?
+	s32vector-inc!
+	s32vector-set!
+	s32vector.inc!
+	s32vector-dec!
+	s32vector.dec!
+	s32vector.sum
+	s32vector-map/iota
+	s32vector.map/iota
+	s32vector.chop-both-ends
+	s32vector-for-each
+	s32vector.for-each
+	s32vector.map
+	s32vectors-map
+	s32vector-map
+	s32vector-map*
+	s32vector-fold-right
+	s32vector.fold-right
+	s32vector-fold
+	s32vector.fold
+	s32vectors-append
+	pair-with-car-s32vector?
+	pair-with-car-s32vector.append
+
+	u64vector.ref
+	u64vector.set!
+	u64vector.length
+	u64vector.list
+	list.u64vector
+	u64vector.null?
+	u64vector-inc!
+	u64vector-set!
+	u64vector.inc!
+	u64vector-dec!
+	u64vector.dec!
+	u64vector.sum
+	u64vector-map/iota
+	u64vector.map/iota
+	u64vector.chop-both-ends
+	u64vector-for-each
+	u64vector.for-each
+	u64vector.map
+	u64vectors-map
+	u64vector-map
+	u64vector-map*
+	u64vector-fold-right
+	u64vector.fold-right
+	u64vector-fold
+	u64vector.fold
+	u64vectors-append
+	pair-with-car-u64vector?
+	pair-with-car-u64vector.append
+
+	s64vector.ref
+	s64vector.set!
+	s64vector.length
+	s64vector.list
+	list.s64vector
+	s64vector.null?
+	s64vector-inc!
+	s64vector-set!
+	s64vector.inc!
+	s64vector-dec!
+	s64vector.dec!
+	s64vector.sum
+	s64vector-map/iota
+	s64vector.map/iota
+	s64vector.chop-both-ends
+	s64vector-for-each
+	s64vector.for-each
+	s64vector.map
+	s64vectors-map
+	s64vector-map
+	s64vector-map*
+	s64vector-fold-right
+	s64vector.fold-right
+	s64vector-fold
+	s64vector.fold
+	s64vectors-append
+	pair-with-car-s64vector?
+	pair-with-car-s64vector.append
+	)
+
 
 (def (sum nums)
      (fold + 0 nums))
