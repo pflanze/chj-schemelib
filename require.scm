56493a19cd7b5d649c09de95d22882c7bf19d2d1
Christian Jaeger <ch@christianjaeger.ch>
2016-04-09 19:42:02 +0200

    require: split off non-essential part into require-util

--- require.scm
+++ require.scm
@@ -1,11 +1,6 @@
 ;; 'parse them', sort them, kr(ypton)
 
-(require easy
-	 test
-	 (tree-util flatten)
-	 (cj-io-util read-lines)
-	 tsort
-	 Status)
+(require easy)
 
 
 (def (require-decl.modulename v)
@@ -58,91 +53,4 @@
 			    p)))))
 	form)))
 
-(TEST
- ;; well those are evil of course, will break upon module changes
- > (path-string.topo-relation "lib/require.scm")
- #(topo-relation require (easy test tree-util cj-io-util tsort Status))
- ;; > (path-string.topo-relation "tsort.scm")
- ;; #(topo-relation tsort (easy test alist))
- )
-
-(def (modulepaths-tsort paths)
-     (let* ((rs (map path-string.topo-relation paths))
-	    (z (map cons (map .name rs) paths)))
-       (map (C symbol-alist-ref z _) (topo.sort* rs))))
-
-
-(def (modulepaths-in-dir dir #!optional (tail '()))
-     (parameterize
-      ((current-directory dir))
-      (map/tail path-normalize
-		tail
-		(xcall-with-input-process
-		 (list path: "gls" arguments: '("*.scm"))
-      
-		 read-lines))))
-
-
-(def (modulepaths-in-dirs dirpaths #!optional (tail '()))
-     ;;(flatten (map modulepaths-in-dir dirpaths))
-     (fold-right modulepaths-in-dir tail dirpaths))
-
-(def (lib)
-     (modulepaths-tsort (modulepaths-in-dir "lib")))
-(def (mydb)
-     (modulepaths-tsort (modulepaths-in-dirs '("lib" "mydb"))))
-
-
-;; a single-dependency representation
-(class requires
-       (struct name dependency))
-
-(def (modulepaths-satisfying? paths) -> Status?
-     (call/cc
-      (lambda (report)
-	(let* ((all-rs (map path-string.topo-relation paths)))
-	  ;; nw how. wat's already loaded  walk through.
-	  (let lp ((rs all-rs)
-		   (loaded '()))
-	    (if (null? rs)
-		(Success)
-		(let-pair
-		 ((r rs*) rs)
-		 (for-each
-		  (lambda (d)
-		    (unless (memq d loaded)
-			    (report (Failure
-				     (requires (.name r) d)))))
-		  (.deps r))
-		 (lp rs*
-		     (cons (.name r) loaded)))))))))
-
-(TEST
- > (modulepaths-satisfying? '("lib/Maybe.scm" "lib/easy.scm"))
- #(Failure #(requires Maybe easy))
- > (modulepaths-satisfying? '("lib/easy-1.scm" "lib/Maybe.scm"))
- #(Failure #(requires easy-1 define-macro-star))
- > (modulepaths-satisfying? '("lib/cj-source.scm"
-			      "lib/define-macro-star.scm"))
- #(Success))
-
-
-
-(def (load.scm-files)
-     (map (C string-append _ ".scm")
-	  (filter (both string?
-			(complement (C string-ends-with? _ ".scm")))
-		  (flatten
-		   (call-with-input-file ".gambc/load.scm" read-all)))))
-
-(def (check-load.scm check-mydb-too?)
-     (let ((modulepaths
-	    (filter (let ((lib (C string-starts-with? _ "lib/")))
-		      (if check-mydb-too?
-			  (either lib
-				  (C string-starts-with? _ "mydb/"))
-			  lib))
-		    (load.scm-files))))
-       (vector (length modulepaths)
-	       (modulepaths-satisfying? modulepaths))))
-
+;; tests see in require-util.scm
