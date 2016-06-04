76101977ef307c9cc1ab214226b267e5c3ba1a19
Christian Jaeger <ch@christianjaeger.ch>
2016-06-04 20:18:04 +0100

    start show.scm (hacky)

---
+++ show.scm
@@ -0,0 +1,31 @@
+;;; Copyright 2016 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+(require dot-oo
+	 (predicates any)
+	 (cj-source-util-2 assert))
+
+(define. (any.show v)
+  v)
+
+;; structs:
+
+(define (struct->values v)
+  (let ((l (vector->list v)))
+    (assert (struct-tag? (car l)))
+    (cdr l)))
+
+;; XXX HACK for now, should generate for each struct according to its
+;; (default) constructor:
+
+(define. (struct.show v)
+   ;; The HACK is: assumption that the constructor carries the same
+   ;; name as the type, and that it takes positional arguments
+  (cons (struct-type-name v)
+	(map .show (struct->values v))))
+
