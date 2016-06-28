517e88179704bc7f51817acbb8f01eb132889b1b
Christian Jaeger <ch@christianjaeger.ch>
2016-06-28 17:16:08 +0100

    cj-functional-2: add =>> and =>>*

--- cj-functional-2.scm
+++ cj-functional-2.scm
@@ -4,7 +4,9 @@
 	 (cj-env-1 inc))
 
 (export (macro =>)
-	(macro =>*))
+	(macro =>*)
+	(macro =>>)
+	(macro =>>*))
 
 ;; The "Clojure-macros". Clojure calls them -> and ->> instead, but ->
 ;; is taken by cj-typed. Called this "chain" previously (but then how
@@ -36,4 +38,28 @@
 (TEST
  > ((=>* (inc)) 10)
  11)
+
+
+;; bah, copy-paste except for the last line
+(define (=>>-expand start exprs)
+  (let next ((exprs exprs)
+	     (res start))
+    (if (null? exprs)
+	res
+	(next (cdr exprs)
+	      (mcase (car exprs)
+		     (`(`call . `rest)
+		      `(,call ,@rest ,res)))))))
+
+
+;; dito
+(define-macro* (=>> start . exprs)
+  (=>>-expand start exprs))
+
+(define-macro* (=>>* . exprs)
+  (with-gensym
+   V
+   `(lambda (,V)
+      ,(=>>-expand V exprs))))
+
 
