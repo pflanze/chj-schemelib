3eddafe0dc3eeef418fc820f7ff07a3b77177acc
Christian Jaeger <ch@christianjaeger.ch>
2016-07-17 21:26:02 +0100

    split test-lib into test-lib-1 and test-lib

---
+++ test-lib-1.scm
@@ -0,0 +1,82 @@
+;;; Copyright 2010-2016 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+(require define-macro-star
+	 test)
+
+
+;; A library of helper functions for writing tests
+;; also see test-lib
+
+(export (macro time-cpu)
+	(macro %try-error)
+	(macro %error?)
+	(macro %try-syntax-error))
+
+;; test cpu usage:
+
+(define-macro* (time-cpu expr)
+  `(_time-cpu (lambda ()
+		,expr)))
+
+;; test for exceptions:
+
+(define (try-error-handler x-exception?
+			   x
+			   x-exception-message
+			   x-exception-parameters)
+  (lambda (e)
+    (cond ((x-exception? e)
+	   (apply vector
+		  x
+		  (x-exception-message e)
+		  (x-exception-parameters e)))
+	  (else
+	   (raise e)))))
+
+(define error-exception->structure
+  (try-error-handler error-exception?
+		     'error
+		     error-exception-message
+		     error-exception-parameters))
+
+(define (%try-error-f thunk)
+  (with-exception-catcher
+   error-exception->structure
+   thunk))
+
+(define-macro* (%try-error form)
+  `(%try-error-f (thunk ,form)))
+
+(define (%error?-f thunk)
+  (with-exception-catcher
+   (lambda (e)
+     #t)
+   (lambda ()
+     (vector 'not-an-error (thunk)))))
+
+(define-macro* (%error? form)
+  `(%error?-f (thunk ,form)))
+
+;; and for syntax exceptions:
+
+(define source-exception->structure
+  (try-error-handler source-error?
+		     'source-error
+		     source-error-message
+		     source-error-args ;; uh consistency?
+		     ))
+
+(define (%try-syntax-error-f thunk)
+  (with-exception-catcher
+   source-exception->structure
+   thunk))
+
+(define-macro* (%try-syntax-error form)
+  `(%try-syntax-error-f (thunk (eval ',form))))
+
--- test-lib.scm
+++ test-lib.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2010, 2011 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2010-2016 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
@@ -15,6 +15,38 @@
 	 (lazy-debug F))
 
 ;; A library of helper functions for writing tests
+;; also see test-lib-1
+
+(export (struct pseudorandomsource)
+	(struct range)
+	make-range/base ;; ?
+	pseudorandomsource->integer-stream
+	pseudorandomsource->real-stream
+	pseudorandomsource->char-stream
+	pseudorandomsource->a-z-stream
+	charstream:string ;; ?
+	charstream+lenstream->string-stream ;; ?
+	pseudorandomsource*->a-z-string-stream
+	symbol-stream
+	random-sort
+	test-lib:burn-time! ;; ?
+	test-lib:burn-time-numbers ;; ?
+	start-mutator ;; ?
+	random-integer..<
+	random-integer..
+	random-natural random-natural0
+	random-fraction
+	produce-stream ;; ?
+	random-natural0-above
+	random-natural0-above-stream
+	test-natural0-exponentials
+	test-natural0s
+	
+	#!optional
+	do-iter ;; ?
+	list->chunked-lists ;; XX move?
+	stream-rtake+rest ;; XX move?
+	)
 
 ;; pseudorandom sources
 
@@ -178,69 +210,6 @@
  #(#t
    (d a c e b))
  )
-
-
-;; test cpu usage:
-
-(define-macro* (time-cpu expr)
-  `(_time-cpu (lambda ()
-		,expr)))
-
-;; test for exceptions:
-
-(define (try-error-handler x-exception?
-			   x
-			   x-exception-message
-			   x-exception-parameters)
-  (lambda (e)
-    (cond ((x-exception? e)
-	   (apply vector
-		  x
-		  (x-exception-message e)
-		  (x-exception-parameters e)))
-	  (else
-	   (raise e)))))
-
-(define error-exception->structure
-  (try-error-handler error-exception?
-		     'error
-		     error-exception-message
-		     error-exception-parameters))
-
-(define (%try-error-f thunk)
-  (with-exception-catcher
-   error-exception->structure
-   thunk))
-
-(define-macro* (%try-error form)
-  `(%try-error-f (thunk ,form)))
-
-(define (%error?-f thunk)
-  (with-exception-catcher
-   (lambda (e)
-     #t)
-   (lambda ()
-     (vector 'not-an-error (thunk)))))
-
-(define-macro* (%error? form)
-  `(%error?-f (thunk ,form)))
-
-;; and for syntax exceptions:
-
-(define source-exception->structure
-  (try-error-handler source-error?
-		     'source-error
-		     source-error-message
-		     source-error-args ;; uh consistency?
-		     ))
-
-(define (%try-syntax-error-f thunk)
-  (with-exception-catcher
-   source-exception->structure
-   thunk))
-
-(define-macro* (%try-syntax-error form)
-  `(%try-syntax-error-f (thunk (eval ',form))))
 
 ;; utilities for writing concurrency tests:
 
