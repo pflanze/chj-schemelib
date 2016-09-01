b324a28969ecbd152f3d63f8875eeea9bdcb68e8
Christian Jaeger <ch@christianjaeger.ch>
2016-09-01 11:24:03 +0100

    create wbsymboltable.scm
    OK, actually do not necessarily have to subclass.

---
+++ wbsymboltable.scm
@@ -0,0 +1,96 @@
+;;; Copyright 2016 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 3 of the License, or
+;;;    (at your option) any later version. Also, as a special exception to the
+;;;    terms of the GPL you can link it with any code produced by Categorical
+;;;    Design Solutions Inc. from Quebec, Canada.
+
+
+;; do a 'naked' abstraction over wbtable, i.e. just wrappers for the
+;; functions (non-generics).
+
+(require easy
+	 cj-cmp
+	 wbtable
+	 test)
+
+(export wbsymboltable?
+	wbsymboltable-of
+	empty-wbsymboltable-of
+	list->wbsymboltable-of
+	list.wbsymboltable-of
+	
+	;; plus method and class re-exports from wbtable..
+	)
+
+
+(def wbsymboltable?
+     ;; hm can't use wbtable-of, wow. Since that does eq? not
+     ;; subtyping checking with the value? predicate.
+     (lambda (v)
+       (and (wbtable? v)
+	    (=> v
+		(.table-head)
+		(.key?)
+		(eq? symbol?)))))
+
+(def (wbsymboltable-of value?)
+     ;; XX Note that this one is (just strictly speaking?) also
+     ;; actually broken, since subtyping is missing; value? may very
+     ;; well return true for all of its values but not be eq? (well we
+     ;; knew that about the non-eq-but-equivalent case anyway, but the
+     ;; point is that the predicate doesn't even need to be type
+     ;; equivalent, but subtype matching is enough, for it to remain
+     ;; true.)
+     (wbtable-of symbol? value?))
+
+(def (empty-wbsymboltable-of value?)
+     (empty-wbtable-of symbol? symbol-cmp value?))
+
+
+(def (list->wbsymboltable-of value?)
+     (list->wbtable-of symbol? symbol-cmp value?))
+
+(def (list.wbsymboltable-of l value?)
+     ((list->wbtable-of symbol? symbol-cmp value?) l))
+
+
+(def. (wbsymboltable.show t)
+  `(list.wbsymboltable-of ,(.show (.list t))
+			  ,(.show (=> t
+				      (.table-head)
+				      (.value?)))))
+
+(TEST
+ > (def t (empty-wbsymboltable-of number?))
+ > (def t2 (.add t 'y 10))
+ > (def t3 (.add t2 'x 20.3))
+ > (map .length (list t t2 t3))
+ (0 1 2)
+ > (map wbsymboltable? (list t t2 t3 empty-wbtree))
+ (#t #t #t #f)
+ > (%try-error (list.wbsymboltable-of '((z "2/3") (a . "a") (b . "b")) number?))
+ #(error
+   "assertment failure: ($value? (cdr key.val))"
+   (number? (cdr '(z "2/3"))))
+ ;; OH well? Ugly or actually okay?
+ > (def u (list.wbsymboltable-of '((z . "2/3") (a . "a") (b . "b")) string?))
+ > (.show u)
+ (list.wbsymboltable-of (list (cons 'a "a")
+			      (cons 'b "b")
+			      (cons 'z "2/3"))
+			string?)
+ > (%try-error (.add u "q" "qq"))
+ #(error
+   "assertment failure: ($key? (car key.val))"
+   (symbol? (car '("q" . "qq")))) 
+ > (%try-error (.add u 'q 23))
+ #(error
+   "assertment failure: ($value? (cdr key.val))"
+   (string? (cdr '(q . 23)))) 
+ > (map (wbsymboltable-of string?) (list t t2 t3 empty-wbtree u))
+ (#f #f #f #f #t)
+ )
+
