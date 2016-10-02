e7a113f0ad810704d2988f8fe6f8c9df412267c2
Christian Jaeger <ch@christianjaeger.ch>
2016-10-02 16:38:49 +0100

    scheme-meta: add |sexpr-object?| and |sexpr?|
    ([Almost] pointless?)

--- scheme-meta.scm
+++ scheme-meta.scm
@@ -8,14 +8,38 @@
 
 ;; Meta-knowledge about Scheme
 
-(require)
+(require (cj-functional pair-of)
+	 ;;(vector-util vector-of)  circular dep?
+	 )
 
 (export self-quoting?
 	perhaps-quote
-
+	uvector?
+	svector?
+	fvector?
+	homogenous-vector?
+	sexpr-object?
+	sexpr?
 	#!optional
+	pair-of-sexpr?
+	vector-of-sexpr?
 	void?)
 
+;; COPY to avoid circular dep -----  XX move here instead?
+
+(define inc (##lambda (n) (+ n 1)))
+
+(define vector-every
+  (##lambda
+   (t? v)
+   (let ((len (vector-length v)))
+     (let lp ((i 0))
+       (if (< i len) (and (t? (vector-ref v i)) (lp (inc i))) #t)))))
+
+(define vector-of
+  (##lambda (t?) (lambda (v) (and (vector? v) (vector-every t? v)))))
+
+;; /COPY
 
 (define (void? v)
   ;; (eq? v (void))  or, since I don't have a void.show yet either:
@@ -45,4 +69,64 @@
       v
       (list 'quote v)))
 
+
+
+(define (uvector? v)
+  (or (u8vector? v)
+      (u16vector? v)
+      (u32vector? v)))
+
+(define (svector? v)
+  (or (s8vector? v)
+      (s16vector? v)
+      (s32vector? v)))
+
+(define (fvector? v)
+  (or (f32vector? v)
+      (f64vector? v)))
+
+(define (homogenous-vector? v)
+  (or (uvector? v)
+      (svector? v)
+      (fvector? v)))
+
+;; XX bit vectors ?
+
+
+(define (sexpr-object? v)
+  (or (symbol? v)
+      (self-quoting? v)
+      (null? v)
+      (pair? v)
+      (vector? v)
+       ;; XX really?
+      (homogenous-vector? v)))
+
+
+(define (sexpr? v)
+  (or (symbol? v)
+      (self-quoting? v)
+      (null? v)
+      (pair-of-sexpr? v)
+      (vector-of-sexpr? v)
+      ;; XX really?
+      (homogenous-vector? v)))
+
+(define pair-of-sexpr? (pair-of sexpr? sexpr?))
+(define vector-of-sexpr? (vector-of sexpr?))
+
+
+'(TEST
+ ;; hum, stupidly, is the only data structure not supported
+ ;; procedures, continuations, and other Gambit data structures
+ ;; (cj-struct ones would be considered s-expressions)? If Gambit used
+ ;; the cj-struct approach then there would be *none*? ! Wow
+ ;; beauty. (Also, interestingly, Gambit does use procedure values in
+ ;; its expanded source code, but ok, those are not sexprs anymore.)
+ > (sexpr? cons)
+ #f
+ ;; Notably also: (wow, that removes the need for |source-sexpr?|)
+ > (sexpr? (quote-source 4))
+ #t
+ )
 
