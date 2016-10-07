b358a62caccf8afce436296fed2ce0045ad8197c
Christian Jaeger <ch@christianjaeger.ch>
2016-10-07 15:17:16 +0100

    delimcc-simple: adapt (and split tests into separate file)

--- delimcc-simple.scm
+++ delimcc-simple.scm
@@ -1,3 +1,21 @@
+;; Adapted by Christian Jaeger from 
+;; http://okmij.org/ftp/continuations/implementations.html
+;; http://okmij.org/ftp/Scheme/delimcc-simple.scm
+
+(require define-macro-star)
+
+(export (macro shift)
+	(macro reset)
+	delimcc-simple:reset*
+	delimcc-simple:shift*
+
+	#!optional
+	;; but these change all the time? may not be exportable by value
+	delimcc-simple:go
+	delimcc-simple:pstack
+	)
+
+
 ; The implementation of ordinary shift/reset derived 
 ; by simplifying multi-prompt shift/reset in delimcc.scm
 ;
@@ -16,12 +34,12 @@
 
 ; This ought to be a call-with-unwinding-continuation, if an
 ; implementation supports such a thing.
-(define call/cc call-with-current-continuation)
+;(define call/cc call-with-current-continuation)
 
-(define go #f)
+(define delimcc-simple:go #f)
 
 ; pstack is a list of k: stack fragments
-(define pstack '())
+(define delimcc-simple:pstack '())
 
 ; Execute a thunk in the empty environment -- at the bottom of the stack --
 ; and pass the result, too encapsulated as a thunk, to the
@@ -46,12 +64,12 @@
 (let ((v
 	(call/cc
 	  (lambda (k)
-	    (set! go k)
+	    (set! delimcc-simple:go k)
 	    (k #f)))))
   (if v
     (let* ((r (v))
-	   (h (car pstack))
-	   (_ (set! pstack (cdr pstack))))
+	   (h (car delimcc-simple:pstack))
+	   (_ (set! delimcc-simple:pstack (cdr delimcc-simple:pstack))))
       (h r))	; does not return
     ))
 
@@ -63,143 +81,37 @@
 ;;   let () = p.mbox := fun () -> res in
 ;;   raise DelimCCE
 
-(define (reset* th)
+(define (delimcc-simple:reset* th)
   (call/cc
     (lambda (k)
-       (set! pstack (cons k pstack))
-       (go th))))			; does not return
+       (set! delimcc-simple:pstack (cons k delimcc-simple:pstack))
+       (delimcc-simple:go th))))			; does not return
 
-(define (shift* f)
+(define (delimcc-simple:shift* f)
   (call/cc
     (lambda (k)			; stack fragment
-      (go 
+      (delimcc-simple:go 
 	(lambda () 
 	  (f 
 	    (lambda (v)
 	      (call/cc (lambda (k1)
-			 (set! pstack (cons k1 pstack))
+			 (set! delimcc-simple:pstack (cons k1 delimcc-simple:pstack))
 			 (k v))))))))))
 
 ; ------------------------------- Syntactic sugar
 
-(define-syntax reset
-  (syntax-rules ()
-    ((_ ?e ?f ...) (reset* (lambda () ?e ?f ...)))))
+;; (define-syntax reset
+;;   (syntax-rules ()
+;;     ((_ ?e ?f ...) (reset* (lambda () ?e ?f ...)))))
 
-(define-syntax shift
-  (syntax-rules ()
-    ((_ ?k ?e ?f ...) (shift* (lambda (?k) ?e ?f ...)))))
-
-
-; ------------------------------- Tests
-
-(display (+ 10 (reset (+ 2 (shift k (+ 100 (k (k 3))))))))
-(newline)
-; --> 117
-
-(display (* 10 (reset (* 2 (shift g (* 5 (shift f (+ (f 1) 1))))))))
-(newline)
-; --> 60
-
-(display (let ((f (lambda (x) (shift k (k (k x))))))
-	   (+ 1 (reset (+ 10 (f 100))))))
-(newline)
-; --> 121
-
-; shift f1 tests that we implement shift rather than shift0
-(display (reset
-	   (let ((x (shift f 
-		      (shift f1 (f1 (cons 'a (f '())))))))
-	     (shift g x))))
-(newline)
-; ==> '(a)
-
-(define (p x) (if (eq? x p) '(p p) `(p ,x)))
-(reset (display (let ((x 'abcde)) (eq? x ((shift* shift*) x)))))
-(newline)
-
-(define traverse
-  (lambda (xs)
-    (letrec ((visit
-	       (lambda (xs)
-		 (if (null? xs)
-		   '()
-		   (visit (shift*
-			    (lambda (k)
-			      (cons (car xs) (k (cdr xs))))))))))
-      (reset*
-	(lambda ()
-	  (visit xs))))))
-
-(display "Ex by Olivier Danvy") (newline)
-(display (traverse '(1 2 3 4 5)))
-(newline)
+(define-macro* (reset e . fs)
+  `(delimcc-simple:reset* (##lambda () ,e ,@fs)))
 
 
-; Testing garbage-retention in Petite Chez Scheme
-; Using guardians
-; For explanations: http://www.scheme.com/csug/smgmt.html#g2352
-; This memory leak test is due to Chung-chieh Shan.
-; This test can be adjusted to run on any other system:
-; it should loop forever in constant memory. In fact, it was first
-; written in portable Scheme; guardians were added later.
+;; (define-syntax shift
+;;   (syntax-rules ()
+;;     ((_ ?k ?e ?f ...) (shift* (lambda (?k) ?e ?f ...)))))
 
-(define (test-gc)
-  (let ((g (make-guardian)))
-    (let loop ((junk-identity
-		 (let ((junk (list 'junk)))
-		   (cons junk (reset (shift f f)))))
-		(done 10))
-      (if (zero? done)
-	(begin
-	  (collect (collect-maximum-generation)) ; force all collection
-	  (display "checking if junk became inacessible:") (newline)
-	  (do ((junk-inaccessible (g) (g))) ((not junk-inaccessible))
-	    (display "collected junk of size: ")
-	    (display junk-inaccessible)
-	    (newline)))
-	(begin
-	  (g (car junk-identity)) ; register with the guardian
-	  (set-cdr! (car junk-identity)
-	    (list (cdr junk-identity)))
-	  (loop (cons (cdr (car junk-identity))
-		  (cdr junk-identity)) (- done 1)))))))
+(define-macro* (shift k e . fs)
+  `(delimcc-simple:shift* (##lambda (,k) ,e ,@fs)))
 
-
-;; > (test-gc)
-;; checking if junk became inacessible:
-;; collected junk of size: (junk #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure>)
-;; collected junk of size: (#<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure>)
-;; collected junk of size: (#<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure>)
-;; collected junk of size: (#<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure>)
-;; collected junk of size: (#<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure>)
-;; collected junk of size: (#<procedure> #<procedure> #<procedure> #<procedure> #<procedure> #<procedure>)
-;; collected junk of size: (#<procedure> #<procedure> #<procedure> #<procedure> #<procedure>)
-;; collected junk of size: (#<procedure> #<procedure> #<procedure> #<procedure>)
-;; collected junk of size: (#<procedure> #<procedure> #<procedure>)
-;; collected junk of size: (#<procedure> #<procedure>)
-;;
-
-; This listing shows that the junk is collected rather than retained.
-; In contrast, the original implementation of shift/reset in terms of
-; call/cc collects nothing: all junk is retained.
-
-
-; Another leak test
-(define (leak-test1-g identity-thunk)
-  (let ((g (make-guardian)))
-    (let loop ((id (lambda (x) x)) (done 10))
-      (if (zero? done)
-	(begin
-	  (collect (collect-maximum-generation)) ; force all collection
-	  (display "collected pieces of junk: ")
-	  (display
-	    (do ((junk-inaccessible (g) (g)) (c 0 (+ 1 c)))
-	        ((not junk-inaccessible) c)))
-	    (newline))
-	 (begin
-	   (g id) ; register with the guardian
-	   (loop (id (identity-thunk)) (- done 1)))))))
-
-; (leak-test1-g (lambda () (reset (shift f f))))
-; collected pieces of junk: 10
