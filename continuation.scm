eaa9c0d73c83df1de3c52af6244fb19328946857
Christian Jaeger <ch@christianjaeger.ch>
2016-10-10 01:28:39 +0100

    continuation: can't get it to do no winding?
    Oh: that winding means dynamic-wind (only), not parameters. ?

--- continuation.scm
+++ continuation.scm
@@ -1,6 +1,7 @@
 
 
-(require easy)
+(require easy
+	 test)
 
 (export continuation-return-no-winding
 	continuation-graft-no-winding)
@@ -15,6 +16,36 @@
 				    #(procedure? proc)
 				    . args)
      (apply ##continuation-graft-no-winding cont proc args))
+
+
+
+(TEST
+ > (defparameter tp #f)
+ > (def cnt 0)
+ > (def (t)
+	(continuation-capture
+	 (lambda (exit)
+	   (parameterize
+	    ((tp 111))
+	    (letv ((newtp newc)
+		   (continuation-capture
+		    (lambda (cont)
+		      (continuation-return-no-winding exit cont))))
+		  (warn "tp = "(tp))
+		  ;;(tp newtp)
+		  newc)))))
+ > (def c (t))
+ > (if (< cnt 3)
+       (begin
+	 (inc! cnt)
+	 (warn "calling c; tp,cnt=" (tp) cnt)
+	 (parameterize
+	  ((tp cnt))
+	  (continuation-return-no-winding c (values cnt c)))))
+ ;; can't get it to do no winding. why is this, SGH????
+ )
+
+
 
 
 ;; TODO: ##continuation-capture-aux
