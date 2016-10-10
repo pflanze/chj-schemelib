369113adfb1a55cda2baafb142e267fe8b3f602a
Christian Jaeger <ch@christianjaeger.ch>
2016-10-10 17:29:23 +0100

    predicates: add |argument-key|, change arguments-of to handle it

--- predicates.scm
+++ predicates.scm
@@ -8,7 +8,9 @@
 
 (require test
 	 predicates-1 ;; for re-exports only?
-	 cj-typed)
+	 cj-typed
+	 dot-oo
+	 symboltable-1)
 
 
 (export box-of
@@ -99,14 +101,111 @@
 
 
 
-;; with no support for rest / optional / keyword args:
+;; (def (argument-key #(symbol? key)
+;; 		   #(predicate? pred))
+;;      (lambda (k v else)
+;;        (if (eq? k key)
+;; 	   (pred v)
+;; 	   (else))))
+
+;; but then also need to check whether it *is* a keyword argument, so,
+;; use 'structs' after all. OH, can actually depend on them, wow. Ah,
+;; even dot-oo, so that we can get type checking (shouldn't
+;; define-typed-struct be an intermediate layer, though, for some
+;; cases?) So:
+
+(define-struct. argument-key
+  #(symbol? key)
+  #(predicate? pred))
+
+
+;; no support for rest / optional yet, only key using the above
+;; (Should I really instead just generate code?, a function with the
+;; given syntax, that returns #t, and if there are exceptions, well,
+;; catch them. That would support all features. But then all of this
+;; is useless anyway, right, since can't wrap functions, it's
+;; completely pointless until we get proper type objects and
+;; checking. And then it will be somewhat different anyway, but *will*
+;; need a proper implementation of the DSSSL stuff.)
+
 (define (arguments-of . preds)
-  (let ((arity (length preds)))
+  (let* ((normalpreds (filter (lambda (v)
+				(not (argument-key? v)))
+			      preds))
+	 (keypreds (filter argument-key? preds))
+	 (maybe-keypredstable (and (pair? keypreds)
+				   (list->symboltable
+				    (map (lambda (kp)
+					   (let-argument-key ((k p) kp)
+							     (cons k p)))
+					 keypreds)))))
+
     (lambda (args)
-      (let lp ((ps preds)
+
+      (define (keyword-process arguments)
+	(if (and (null? arguments)
+		 (not maybe-keypredstable))
+	    #t
+
+	    (if maybe-keypredstable
+		(let ((tbl (symboltable-copy maybe-keypredstable)))
+		  (let lp ((as arguments))
+		    (if (null? as)
+			;; check the missing keyword parameters, whether they
+			;; all accept a false, ah, or default value, oh. so
+			;; complex.
+			(symboltable:every?
+			 tbl
+			 (lambda (k pred)
+			   ;; predicate accepts false, but XX should check
+			   ;; that it doesn't have the default value;
+			   ;; arguments-of doesn't support those, thus safe
+			   ;; for now, but should extend.
+
+			   ;; oh, symboltable:every? of course still
+			   ;; iterates over all keys, since I didn't
+			   ;; delete them. (Should symboltable have a
+			   ;; delete! method that sets a special value
+			   ;; into the value slot to indicate
+			   ;; deletion?) (Stupid, I thought I had a
+			   ;; nice optimal way to do things but, not
+			   ;; so much.)
+			   (if pred
+			       (pred #f)
+			       ;; pred was deleted, hence satisfied already
+			       #t)))
+
+			(let-pair
+			 ((a as*) as)
+			 (and (keyword? a)
+			      (pair? as*)
+			      (let ((k (keyword.symbol a)))
+				(cond ((symboltable-ref tbl
+							k
+							#f)
+				       ;; ^ values in tbl are also set
+				       ;; to #f, which means,
+				       ;; duplicate keywords are
+				       ;; leading to #f for the whole
+				       ;; arguments-of, which seems
+				       ;; sensible since it's an error.
+				       => (lambda (pred)
+					    (let-pair
+					     ((b as**) as*)
+					     (and (pred b)
+						  (begin
+						    (symboltable-set! tbl
+								      k
+								      #f)
+						    (lp as**))))))
+				      (else #f))))))))
+		;; superfluous arguments
+		#f)))
+      
+      (let lp ((ps normalpreds)
 	       (as args))
 	(if (null? ps)
-	    (null? as)
+	    (keyword-process as)
 	    (if (pair? as)
 		;; now could save the length checks above!
 		(let-pair ((p? ps*) ps)
@@ -135,5 +234,44 @@
  > (p? '(1 ""))
  #f
  > (p? '(1 1))
- #f)
+ #f
+
+ ;; and keywords:
+ > (def p? (arguments-of symbol?
+			 (argument-key 'foo (maybe string?))
+			 (argument-key 'bar (maybe number?))))
+ > (p? '(f))
+ #t
+ > (p? '())
+ #f
+ > (p? '(f foo: #f))
+ #t
+ > (p? '(f foo: #t))
+ #f
+ > (p? '(f foo: "foo"))
+ #t
+ > (p? '(f foo: "foo" bar: 13))
+ #t
+ > (p? '(f foo: "foo" bar: "13"))
+ #f
+ > (def p? (arguments-of symbol?
+			 (argument-key 'foo (maybe string?))
+			 (argument-key 'bar number?)))
+ > (p? '(f))
+ #f
+ > (p? '())
+ #f
+ > (p? '(f foo: #f))
+ #f
+ > (p? '(f foo: #t))
+ #f
+ > (p? '(f foo: "foo"))
+ #f
+ > (p? '(f foo: "foo" bar: 13))
+ #t
+ > (p? '(f bar: 13))
+ #t
+ > (p? '(f foo: "foo" bar: "13"))
+ #f
+ )
 
