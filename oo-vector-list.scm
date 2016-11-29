dbc5d836a1cfe92803628a479be86f06dad3ef40
Christian Jaeger <ch@christianjaeger.ch>
2016-11-29 18:59:18 +0000

    oo-vector-list: add vector.map/iota-list, vectors.map/iota-list

--- oo-vector-list.scm
+++ oo-vector-list.scm
@@ -13,25 +13,34 @@
 	 test)
 
 
-(export (method vector.map-list)
-	(method vectors.map-list))
+(export (method vector.map-list
+		vector.map/iota-list
+		vectors.map-list
+		vectors.map/iota-list))
 
 
 (def dec (inline dec)) ;; didn't I have a macro for that? lost?
 
 
-(def. (vector.map-list vec fn #!optional (tail '()))
+(def. (vector.map-list vec fn #!optional (tail '()) with-i?)
   (let lp ((i (dec (vector-length vec)))
 	   (l tail))
     (if (>= i 0)
 	(lp (dec i)
-	    (cons (fn (vector-ref vec i)) l))
+	    (cons (let ((v (vector-ref vec i)))
+		    (if with-i?
+			(fn i v)
+			(fn v)))
+		  l))
 	l)))
+
+(def. (vector.map/iota-list vec fn #!optional (tail '()))
+  (vector.map-list vec fn tail #t))
 
 
 (def vectors? (list-of vector?))
 
-(def. (vectors.map-list vecs fn #!optional (tail '()))
+(def. (vectors.map-list vecs fn #!optional (tail '()) with-i?)
   (let ((len (vector-length (car vecs))))
     (for-each (lambda (v)
 		(assert (= (vector-length v) len)))
@@ -40,9 +49,16 @@
 	     (l tail))
       (if (>= i 0)
 	  (lp (dec i)
-	      (cons (apply fn (map (C vector-ref _ i) vecs))
+	      (cons (let ((vs (map (C vector-ref _ i) vecs)))
+		      (if with-i?
+			  (apply fn i vs)
+			  (apply fn vs)))
 		    l))
 	  l))))
+
+
+(def. (vectors.map/iota-list vecs fn #!optional (tail '()))
+  (vectors.map-list vecs fn tail #t))
 
 
 (TEST
@@ -51,5 +67,8 @@
  > (.map-list (vector 2 3 4) inc 'foo)
  (3 4 5 . foo)
  > (.map-list (list (vector 2 3 4) (vector 1 -1 9)) + 'foo)
- (3 2 13 . foo))
-
+ (3 2 13 . foo)
+ > (.map/iota-list (vector 2 3 4) + 'foo)
+ (2 4 6 . foo)
+ > (.map/iota-list (list (vector 2 3 4) (vector 1 -1 9)) + 'foo)
+ (3 3 15 . foo))
