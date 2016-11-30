74d335281be184a21e40e3c936406ff859fa859e
Christian Jaeger <ch@christianjaeger.ch>
2016-11-30 14:11:43 +0000

    create oo-vector.scm

---
+++ oo-vector.scm
@@ -0,0 +1,40 @@
+;;; Copyright 2016 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+;; Functions converting vectors to vectors.
+
+(require easy
+	 test)
+
+(export (method vector.filter/iota))
+
+
+(def inc (inline inc))
+
+(def. (vector.filter/iota v fn)
+  (let* ((len (vector-length v))
+	 (v* (make-vector len)))
+    (let lp ((i 0)
+	     (j 0))
+      (if (fx< i len)
+	  (let ((val (vector-ref v i)))
+	    (if (fn val i)
+		(begin
+		  (vector-set! v* j val)
+		  (lp (inc i) (inc j)))
+		(lp (inc i) j)))
+	  (begin
+	    (vector-shrink! v* j)
+	    v*)))))
+
+(TEST
+ > (.filter/iota (vector 2 -4 5 8) (lambda (v i) (even? v)))
+ #(2 -4 8)
+ > (.filter/iota (vector 2 -4 5 8) (lambda (v i) (even? i)))
+ #(2 5))
+
