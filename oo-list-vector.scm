4a02990d86d6f969c8c3b97a7f4c935980090a2b
Christian Jaeger <ch@christianjaeger.ch>
2016-12-03 03:27:28 +0000

    create oo-list-vector.scm

---
+++ oo-list-vector.scm
@@ -0,0 +1,53 @@
+;;; Copyright 2016 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+;; Functions converting lists to vectors.
+
+(require easy
+	 ;;(cj-functional list-of) ;; part of easy?
+	 test)
+
+(export list.every-second-vector)
+
+
+(def inc (inline inc))
+(def dec (inline dec))
+
+;; Would really profit from guaranteed-proper length-maintaining (and
+;; type maintaining?) lists. Well I've got them. Not using them?
+;; Advantage would be that oo dispatch could actually be used sanely,
+;; and saving the length trip.
+
+;; allow for lists of uneven length (so that we can get a vector from
+;; l then a vector from (cdr l) where l holds key value parings).
+(def. (list.every-second-vector l)
+  (if (null? l)
+      '#()
+      (let* ((len (length l))
+	     (len* (arithmetic-shift (inc len) -1))
+	     (v (make-vector len*)))
+	(let lp ((i 0)
+		 (l l))
+	  (let-pair ((a r) l)
+		    (vector-set! v i a)
+		    (let ((i* (inc i)))
+		      (if (< i* len*)
+			  (lp i* (cdr r))))))
+	v)))
+
+(TEST
+ > (list.every-second-vector '())
+ #()
+ > (list.every-second-vector '(a 1))
+ #(a)
+ > (list.every-second-vector '(a 1 b 2))
+ #(a b)
+ > (list.every-second-vector '(1 b 2))
+ #(1 2)
+ )
+
