cad9032a3ce63a6d4b77a6322eb6a7437a57dd72
Christian Jaeger <ch@christianjaeger.ch>
2017-07-21 18:36:17 +0100

    cj-c-errno_Cpart: use safe strerror_r

--- posix/cj-c-errno_Cpart.scm
+++ posix/cj-c-errno_Cpart.scm
@@ -10,11 +10,21 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
+
+static const size_t buflen= 1024;
+static char* buf = NULL;
 ")
 
 (define strerror
   (c-lambda (int)
 	    ISO-8859-1-string
-	    "strerror"))
-;;(^- todo: use strerror_r to be pthread-safe)
+	    "
+if (!buf) {
+    buf= malloc(buflen);
+    /* XX let it segfault if malloc fails? */
+}
+
+___result= strerror_r(___arg1, buf, buflen);
+"))
 
