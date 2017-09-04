bd99cdf4c36ce314de37187ac20d979b440cd286
Christian Jaeger <ch@christianjaeger.ch>
2017-09-04 17:13:43 +0100

    lazy-debug: fix F and S, derive from the same code, handle vectors, values

--- lazy-debug.scm
+++ lazy-debug.scm
@@ -7,32 +7,79 @@
 
 
 (require lazy
-	 debuggable-promise)
+	 debuggable-promise
+	 (vector-util vector-map)
+	 (srfi-11 values-map values->list))
 
 (export F
 	F1
 	S
 	#!optional
-	optim-cons)
+	alloc-optim-cons)
 
 
 (possibly-use-debuggable-promise)
 
-(define (optim-cons old a r)
+(define (alloc-optim-cons old a r)
   (if (and (eq? (car old) a)
-	   (eq? (car old) r))
+	   (eq? (cdr old) r))
       old
       (cons a r)))
 
+(TEST
+ > (let ((old (cons 1 2))) (eq? (alloc-optim-cons old 1 2) old))
+ #t
+ > (let ((old (cons 1 2))) (eq? (alloc-optim-cons old 2 1) old))
+ #f)
+
+
+;; and after allocating the pad already, but 'could' still be beneficial:
+(define (alloc-optim-vectorlikes length ref)
+  (lambda (old new)
+    (let ((l0 (length old))
+	  (l1 (length new)))
+      (if (and (= l0 l1)
+	       (let lp ((i 0))
+		 (if (< i l0)
+		     (and (eq? (ref old i)
+			       (ref new i))
+			  (lp (fx+ i 1)))
+		     #t)))
+	  old
+	  new))))
+
+(define alloc-optim-vector (alloc-optim-vectorlikes vector-length vector-ref))
+(define alloc-optim-values (alloc-optim-vectorlikes ##vector-length ##vector-ref))
+;; well could have used the latter for each case..
+
+
+(define (F/ promise-evaluated? force)
+  (named F (lambda (s)
+	     (let ((process (lambda (s)
+			      (cond ((pair? s)
+				     (alloc-optim-cons s
+						       (F (car s))
+						       (F (cdr s))))
+				    ((vector? s)
+				     (alloc-optim-vector s (vector-map F s)))
+				    ((values? s)
+				     (alloc-optim-values s (values-map F s)))
+				    (else
+				     s)))))
+	       (if (promise? s)
+		   (if (promise-evaluated? s)
+		       (process (force s))
+		       s)
+		   (process s))))))
+
 ;; make copy forcing everything
-(define (F s)
-  (let F ((s s))
-    (let ((s (force s)))
-      (if (pair? s)
-	  (optim-cons s
-		      (F (car s))
-		      (F (cdr s)))
-	  s))))
+(define F (F/ (lambda (v) #t)
+	      force))
+
+;; only copy what was already evaluated
+(define S (F/ promise-evaluated?
+	      evaluated-promise-value))
+
 
 ;; make copy forcing everything, but show <P> wherever there was a
 ;; promise (and one level at that for each). XX force1 is not
@@ -49,17 +96,55 @@
 	  (else
 	   s))))
 
-;; only copy what was already evaluated
-(define (S s)
-  (let F ((s s))
-    (if (promise? s)
-	(if (promise-evaluated? s)
-	    (let ((s (evaluated-promise-value s)))
-	      (if (pair? s)
-		  (optim-cons s
-			      (F (car s))
-			      (F (cdr s)))
-		  s))
-	    s)
-	s)))
+
+(TEST
+ > (F '(1 2))
+ (1 2)
+ > (F (delay 1))
+ 1
+ > (promise? (S (delay 1)))
+ #t
+ > (define s (delay (cons 1 (delay (cons 2 (delay (cons 3 '())))))))
+ > (promise? (S s))
+ #t
+ > (car (force s))
+ 1
+ > (promise? (S s))
+ #f
+ > (promise? s)
+ #t
+ > (promise? (cdr (S s)))
+ #t
+ > (promise? (cdr (F s)))
+ #f
+ > (promise? (cdr (S s)))
+ #f
+ > (S s)
+ (1 2 3)
+
+ ;; optim
+ > (eq? (S s) (S s))
+ #f ;; that's not what the optim does
+ > (let ((t (cons -1 s))) (eq? (S t) (S t)))
+ #f
+ > (define s (delay (cons 1 (delay (cons 2 (delay (cons 3 '())))))))
+ > (let ((t (cons -1 s))) (eq? (S t) (S t)))
+ #t
+
+ ;; values:
+ > (define s (delay (cons (delay (values 'a 1))
+			  (delay (cons (values 'b 2)
+				       (delay (cons (values 'c 3)
+						    (delay '()))))))))
+ > (define s* (F s))
+ > (length s*)
+ 3
+ > (values->list (car s*))
+ (a 1)
+ ;; and their optim:
+ > (define s (delay (cons 1 (delay (cons 2 (delay (cons 3 '())))))))
+ > (let ((t (cons -1 (values 2 s)))) (eq? (S t) (S t)))
+ #t
+ )
+
 
