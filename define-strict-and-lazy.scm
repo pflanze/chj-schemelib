3505aacb3fbb39e81219313334ae3e4938b36ca8
Christian Jaeger <ch@christianjaeger.ch>
2017-09-05 17:25:59 +0100

    define-strict-and-lazy: add alias: argument

--- define-strict-and-lazy.scm
+++ define-strict-and-lazy.scm
@@ -5,64 +5,75 @@
 (define-macro* (define-strict-and-lazy
 		 strict-name
 		 stream-name
-		 expr)
-  ;; replaces |DELAY| with delay or 'nothing', same for |FV| and |FORCE|
+		 #!key
+		 (aliases '()) ;; list of `(name strict-name stream-name)
+		 #!rest
+		 exprs)
+  ;; replaces |DELAY| with delay or 'nothing', same for |FV| and
+  ;; |FORCE|, and binds each name in aliases to strict-name or
+  ;; stream-name
   `(begin
      (define ,strict-name
-       (let ()
-	(##define-syntax DELAY
-			 (lambda (stx)
-			   (cj-sourcify-deep
-			    (match*
-			     stx
-			     ((DELAY expr)
-			      expr))
-			    stx)))
-	(##define-syntax FV
-			 (lambda (stx)
-			   (cj-sourcify-deep
-			    (match*
-			     stx
-			     ((FV vars . body)
-			      `(begin
-				 ,@body)))
-			    stx)))
-	(##define-syntax FORCE
-			 (lambda (stx)
-			   (cj-sourcify-deep
-			    (match*
-			     stx
-			     ((FORCE expr)
-			      expr))
-			    stx)))
-	,expr))
+       (let ,(map (lambda (alias-lis3)
+		    (let ((l (source-code alias-lis3)))
+		      `(,(car l) ,(cadr l))))
+		  (source-code aliases))
+	 (##define-syntax DELAY
+			  (lambda (stx)
+			    (cj-sourcify-deep
+			     (match*
+			      stx
+			      ((DELAY expr)
+			       expr))
+			     stx)))
+	 (##define-syntax FV
+			  (lambda (stx)
+			    (cj-sourcify-deep
+			     (match*
+			      stx
+			      ((FV vars . body)
+			       `(begin
+				  ,@body)))
+			     stx)))
+	 (##define-syntax FORCE
+			  (lambda (stx)
+			    (cj-sourcify-deep
+			     (match*
+			      stx
+			      ((FORCE expr)
+			       expr))
+			     stx)))
+	 ,@exprs))
      (define ,stream-name
-       (let ()
-	(##define-syntax DELAY
-			 (lambda (stx)
-			   (cj-sourcify-deep
-			    (match*
-			     stx
-			     ((DELAY expr)
-			      `(delay ,expr)))
-			    stx)))
-	(##define-syntax FV
-			 (lambda (stx)
-			   (cj-sourcify-deep
-			    (match*
-			     stx
-			     ((FV vars . body)
-			      `(let ,(map (lambda (v)
-					    `(,v (force ,v)))
-					  (source-code vars))
-				 ,@body)))
-			    stx)))
-	(##define-syntax FORCE
-			 (lambda (stx)
-			   (cj-sourcify-deep
-			    (match*
-			     stx
-			     ((FORCE expr)
-			      `(force ,expr)))
-			    stx)))
-	,expr))))
+       (let ,(map (lambda (alias-lis3)
+		    (let ((l (source-code alias-lis3)))
+		      `(,(car l) ,(caddr l))))
+		  (source-code aliases))
+	 (##define-syntax DELAY
+			  (lambda (stx)
+			    (cj-sourcify-deep
+			     (match*
+			      stx
+			      ((DELAY expr)
+			       `(delay ,expr)))
+			     stx)))
+	 (##define-syntax FV
+			  (lambda (stx)
+			    (cj-sourcify-deep
+			     (match*
+			      stx
+			      ((FV vars . body)
+			       `(let ,(map (lambda (v)
+					     `(,v (force ,v)))
+					   (source-code vars))
+				  ,@body)))
+			     stx)))
+	 (##define-syntax FORCE
+			  (lambda (stx)
+			    (cj-sourcify-deep
+			     (match*
+			      stx
+			      ((FORCE expr)
+			       `(force ,expr)))
+			     stx)))
+	 ,@exprs))))
