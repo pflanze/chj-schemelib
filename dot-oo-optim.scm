1b195694ddaf99dc88e9dc730c241c9cbe8d93d0
Christian Jaeger <ch@christianjaeger.ch>
2017-10-04 19:59:30 +0100

    dot-oo-optim: add macro |dot-oo-optim-later|; rename to genericname-symbol?

--- dot-oo-optim.scm
+++ dot-oo-optim.scm
@@ -10,44 +10,44 @@
 	 (simple-match-1 assert*)
 	 dot-oo)
 
-(export dot-oo-optim-for
+(export (macro dot-oo-optim-for)
+	(macro dot-oo-optim-later)
 
 	;; move to dot-oo and change it to not support foo:.bar any
 	;; more?
-	methodname-symbol?)
+	genericname-symbol?)
 
 
 ;; XX TODO: this assumes methodname is always starting with a
 ;; dot. Hence can't handle foo:.bar (give up on this?)
 
-(def (methodname-symbol? v)
+(def (genericname-symbol? v)
      (and (symbol? v)
 	  (let* ((str (symbol->string v))
 		 (len (string-length str)))
 	    (and (> len 1)
 		 (char=? (string-ref str 0) #\.)))))
 
-(def (dot-oo-optim-for-expand methodname num-args)
+(def (dot-oo-optim-for-expand genericname num-args)
      (assert*
-      methodname-symbol? methodname
-      (lambda (methodname)
+      genericname-symbol? genericname
+      (lambda (genericname)
 	(assert*
 	 natural? num-args
 	 (lambda (num-args)
 	   (let ((ARGS (map (lambda_ (gensym)) (iota num-args)))
-		 (generic-name (symbol-append methodname "/"
+		 (generic-name (symbol-append genericname "/"
 					      (number->string num-args))))
-	     `(define (,generic-name
-		       ,@ARGS)
+	     `(define (,generic-name ,@ARGS)
 		(cond ,@(map (lambda (entry)
 			       (let ((type-name (car entry)))
 				 `((,(symbol-append type-name "?") ,(car ARGS))
-				   (,(symbol-append type-name methodname)
+				   (,(symbol-append type-name genericname)
 				    ,@ARGS))))
 			     (dot-oo:show-method-table
 			      (eval ;; <-- XX forever evil unsafe
 			       (symbol-append "dot-oo-method-table#"
-					      methodname))))
+					      genericname))))
 		      (else
 		       (error ,(string-append (symbol->string generic-name)
 					      ": no method for:")
@@ -55,15 +55,42 @@
 
 
 
-(defmacro (dot-oo-optim-for methodname num-args)
-  (dot-oo-optim-for-expand methodname num-args))
+(defmacro (dot-oo-optim-for genericname num-args)
+  (dot-oo-optim-for-expand genericname num-args))
 
 
 ;; instead of having to write (.ref/2 s 2), allow (% .ref s 2)
-(defmacro (% methodname . args)
-  (assert* methodname-symbol? methodname
-	   (lambda (methodname)
+(defmacro (% genericname . args)
+  (assert* genericname-symbol? genericname
+	   (lambda (genericname)
 	     (let ((len (length args)))
-	       `(,(symbol-append methodname "/" (number->string len))
+	       `(,(symbol-append genericname "/" (number->string len))
 		 ,@args)))))
 
+;; and then since libraries may have to wait until their own method
+;; calls are optimized (late binding), we need a redefinable proxy:
+
+(defmacro (dot-oo-optim-later genericname num-args)
+  (assert*
+   genericname-symbol? genericname
+   (lambda (genericname)
+     (assert*
+      natural? num-args
+      (lambda (num-args)
+	(let ((ARGS (map (lambda_ (gensym)) (iota num-args)))
+	      (generic-name (symbol-append genericname "/"
+					   (number->string num-args)))
+	      ;; -- XX wow up to here it's all COPY-PASTE from
+	      ;; dot-oo-optim-for-expand; and the next few forms are
+	      ;; almost COPY-PASTE from dot-oo.scm:
+	      (method-table-name (generic-name-string.method-table-name
+				  (symbol->string genericname))))
+	  `(begin
+	     (define ,method-table-name
+	       (macro-symbol-value-or ,method-table-name
+				      dot-oo:new-method-table))
+	     (define-if-not-defined ,genericname
+	       (dot-oo:make-generic ',genericname ,method-table-name))
+	     ;;/ copy-paste
+	     (define (,generic-name ,@ARGS)
+	       (,genericname ,@ARGS)))))))))
