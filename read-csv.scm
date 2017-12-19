aabb954b2674d28eabb714dddb1cb1978c18a4d1
Christian Jaeger <ch@christianjaeger.ch>
2017-12-19 15:23:00 +0000

    move {read-csv,write-csv}.scm here

---
+++ read-csv.scm
@@ -0,0 +1,160 @@
+(require easy
+	 eol
+	 (predicates length-=)
+	 jclass)
+
+(export csv-file-stream
+	(jclass csv-reader)
+	(jinterface input-provider
+		    (jclass file-input-provider)))
+
+(def (_csv-port-stream port #!optional (tail '()))
+     (let lp ()
+       (delay
+	 (let ((line (read-line port)))
+	   (if (eof-object? line)
+	       (begin
+		 (close-port port)
+		 (assert (zero? (process-status port)))
+		 tail)
+	       (let ((vals (xone (call-with-input-string line read-all)))
+		     (rest (lp)))
+		 (cons vals rest)))))))
+
+
+(def (csv-file-stream path
+		      #!key
+		      (#(char? sep-char) #\tab)
+		      #(eol-name? eol)
+		      (tail '()))
+     (_csv-port-stream
+      (open-input-process
+       (list path: "./csv2sexpr"
+	     arguments: (list path
+			      "-"
+			      (string sep-char)
+			      (symbol.string eol))
+	     char-encoding: 'UTF-8))
+      tail))
+
+
+;;XX lib (and lostontie?)
+(def (send-file-strings inport outport)
+     ;; XX well.
+     (let ((str (read-line inport #f)))
+       (display str outport)))
+
+
+(def (csv-port-stream port
+		      #!key
+		      (#(char? sep-char) #\tab)
+		      #(eol-name? eol)
+		      (tail '()))
+     (let ((p (open-process
+	       (list path: "./csv2sexpr"
+		     arguments: (list "-"
+				      "-"
+				      (string sep-char)
+				      (symbol.string eol))
+		     ;; WOW, would really need different encoding for
+		     ;; input vs. output. If I wanted to process bytes
+		     ;; inbetween; breaking down so much. But yeah,
+		     ;; char-encoding when opening orig file is done,
+		     ;; then we have strings, we write them here as
+		     ;; UTF-8 and read them back as the same, so,
+		     ;; actually fine here. "send-file-strings kinda"
+		     ;; does the transcoding.
+		     char-encoding: 'UTF-8
+		     stdout-redirection: #t
+		     stdin-redirection: #t))))
+       (future
+	(let lp ()
+	  (send-file-strings port p)
+	  (close-output-port p)
+	  ;; XX btw TODO: check status, don't even do that in csv-file-stream!
+	  (close-port port)))
+       (_csv-port-stream p tail)))
+
+
+
+;; XX add more
+(def char-encodings '(UTF-8))
+
+(def (char-encoding? v)
+     (and (memq v char-encodings) #t))
+
+
+(jinterface input-provider
+
+	    (method (open) -> input-port?)
+
+	    (jclass (file-input-provider #(path-string? path-string)
+					 #(char-encoding? char-encoding))
+
+		    (def-method* (open s)
+		      (open-input-file (list path: path-string
+					     char-encoding: char-encoding)))
+
+		    ;; (def-method* (close s port)
+		    ;;   (close-port port)) unused
+		    ))
+
+
+;;XX lib
+(def (filter/snd vals keep?s)
+     (if (or (null? vals) (null? keep?s))
+	 '()
+	 (let-pair ((v vals) vals)
+		   (let-pair ((k? keep?s) keep?s)
+			     (let ((r (filter/snd vals keep?s)))
+			       (if k?
+				   (cons v r)
+				   r))))))
+
+(TEST
+ > (filter/snd '(a b c) '(#f #t #f))
+ (b)
+ > (filter/snd '(a b c) '(#f #t #t))
+ (b c)
+ > (filter/snd '(a b c) '(#f #t))
+ (b)
+ > (filter/snd '(a b c) '(#f))
+ ()
+ > (filter/snd '(a b c) '(#t))
+ (a)
+ ;; XX hmm what about these, really??:
+ > (filter/snd '(a b c) '(#f #t #t #f))
+ (b c)
+ > (filter/snd '(a b c) '(#f #t #t #t))
+ (b c))
+
+
+(jclass (csv-reader #(input-provider? input-provider)
+		    #!key
+		    #(char? sep-char)
+		    #(eol-name? eol)
+		    #((maybe natural0?) maybe-head-skip)
+		    #(boolean? skip-last?)
+		    #((maybe (list-of natural0?)) maybe-columns))
+
+	(def-method* (stream s)
+	  (let* ((s (csv-port-stream (.open input-provider)
+				     sep-char: sep-char
+				     eol: eol))
+		 (s (if maybe-head-skip
+			(stream-drop s maybe-head-skip)
+			s))
+		 (s (if skip-last?
+			(stream-skip-last s)
+			s))
+		 (s (if maybe-columns
+			(stream-map (lambda (row)
+				      (map (let* ((row* (list.vector row))
+						  (len (vector.length row*)))
+					     (lambda (i)
+					       (vector.ref row* i)))
+					   maybe-columns))
+				    s)
+			s)))
+	    s)))
+
---
+++ write-csv.scm
@@ -0,0 +1,141 @@
+(require easy
+	 U
+	 eol
+	 (char-util char-alphanumeric+?)
+	 test)
+
+;; https://en.wikipedia.org/wiki/Comma-separated_values
+;; https://www.ietf.org/rfc/rfc4180.txt
+
+
+(export stream->csv-file
+	#!optional
+	sep-chars
+	sep-char?
+	csv-map
+	csv-escape)
+
+(def sep-chars '#(#\; #\, #\:))
+
+(def (sep-char? v)
+     (and (char? v)
+	  (case v
+	    ((#\; #\, #\:) #t)
+	    (else #f))))
+
+(def (csv-escape* sep-char str)
+     (if (string-any
+	  ;; (lambda (c)
+	  ;;   (or (char=? c sep-char)
+	  ;; 	(char=? c #\")
+	  ;; 	(char=? c #\newline)
+	  ;; 	(char=? c #\return)))
+
+	  ;; but, maybe, probably, that was too lenient, need more
+	  ;; quotes to prevent errors:
+
+	  (lambda (c)
+	    (or (char=? c sep-char)
+		(not (char-alphanumeric+? c))))
+	  str)
+	 ;; XX more efficient would be to count the number of #\"
+	 ;; first (perhaps as part of the above), then.
+	 (list->string
+	  (cons #\"
+		(fold-right (lambda (c rest)
+			      (let ((r (cons c rest)))
+				(if (char=? c #\")
+				    (cons #\" r)
+				    r)))
+			    '(#\")
+			    (string->list str))))
+	 (if (string-empty? str)
+	     "\"\"" ;; make it different from #f which gives ""
+	     str)))
+
+(def (csv-map v) -> (maybe string?)
+     (cond ((string? v)
+	    v)
+	   ((number? v)
+	    ;; XX number formats?
+	    ;; XX and then treat just as a string, is this ok, correct?
+	    (number->string v))
+	   ((false? v)
+	    #f)
+	   (else
+	    (error "csv-map: don't know how to handle type of value:" v))))
+
+(def ((csv-escape sep-char) v)
+     (cond ((csv-map v) => (lambda_ (csv-escape* sep-char _)))
+	   (else "")))
+
+
+(TEST
+ > (U csv-escape #\, "foobarn")
+ "foobarn"
+ > (U csv-escape #\, 123)
+ "123"
+ > (U csv-escape #\, "123")
+ "123"
+ > (U csv-escape #\, "123,2")
+ "\"123,2\""
+ > (U csv-escape #\, "123.2")
+ "123.2"
+ > (U csv-escape #\. "123.2")
+ "\"123.2\""
+ > (U csv-escape #\. ".")
+ "\".\""
+ > (U csv-escape #\; ".")
+ "."
+ > (U csv-escape #\; "12,3")
+ "\"12,3\""
+ > (U csv-escape #\; "12,3\n")
+ "\"12,3\n\""
+ ;; hm should the \n be escaped (represented as a literal) really?
+ > (U csv-escape #\, "foo,barn")
+ "\"foo,barn\""
+ > (U csv-escape #\, "foo\nbarn")
+ "\"foo\nbarn\""
+ > (U csv-escape #\, "foo\r\nbarn")
+ "\"foo\r\nbarn\""
+ > (U csv-escape #\, "foo\r\n,barn")
+ "\"foo\r\n,barn\""
+ )
+
+
+(def (csv-row-writer #!key
+		     #(eol-name? eol)
+		     #(char? sep-char)
+		     #(output-port? port))
+     (let ((eol* (list (eol-name.string eol))))
+       (lambda (row)
+	 (print port: port
+		(list-join (map (csv-escape sep-char)
+				row)
+			   sep-char
+			   eol*)))))
+
+
+(def (stream->csv-file s
+		       #(path-string? path)
+		       #!key
+		       #(eol-name? eol)
+		       #(char? sep-char))
+     (let* ((p (open-output-file path))
+	    (w (csv-row-writer port: p
+			       eol: eol
+			       sep-char: sep-char)))
+       (let lp ((s s))
+	 (FV (s)
+	     (if (null? s)
+		 (close-port p)
+		 (let-pair ((row s) s)
+			   (w row)
+			   (lp s)))))))
+
+;; XX should I always just create a type (value bundle) right away for
+;; the options?! Are (openly placed) keywords even just a stupid idea?
+;; Even in Perl you rather bundle them up in hashes?
+
+;; (But then PG is very fond of them...)
+
