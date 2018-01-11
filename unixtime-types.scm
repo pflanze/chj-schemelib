0b3b8e40b1c43466a310c3470869be773b4e5bd7
Christian Jaeger <ch@christianjaeger.ch>
2018-01-11 17:10:57 +0000

    unixtime-types: move to jclass

--- unixtime-types.scm
+++ unixtime-types.scm
@@ -6,13 +6,13 @@
 ;;;    (at your option) any later version.
 
 
-(require test
-	 easy
-	 more-oo ;; contained in easy?
+(require easy
+	 jclass
 	 cj-functional ;; contained in easy?
 	 (string-util-2 number->padded-string)
 	 (vector-util vector.value.pos)
-	 english)
+	 english
+	 test)
 
 
 
@@ -47,99 +47,101 @@
 
 ;; kinda-highlevel lowlevel data implementation (used for FFI)
 
-(class localtime
-       (struct sec
-	       min
-	       hour
-	       mday
-	       month-1
-	       year-1900
-	       integer-wday
-	       yday
-	       integer-isdst
-	       ;; This is contained neither in what Perl's `localtime` returns nor
-	       ;; `struct tm` from time.h :
-	       integer-timezone)
+(jclass (localtime sec
+		   min
+		   hour
+		   mday
+		   month-1
+		   year-1900
+		   integer-wday
+		   yday
+		   integer-isdst
+		   ;; This is contained neither in what Perl's `localtime` returns nor
+		   ;; `struct tm` from time.h :
+		   integer-timezone)
 
-       (method month
-	       (compose inc localtime.month-1))
-       (method (year v)
-	       (+ (localtime.year-1900 v) 1900))
+	(def-method* (month v)
+	  (inc month-1))
 
-       (method year-string
-	       (compose number->string localtime.year))
-       (method mday-string
-	       (compose number->string localtime.mday))
+	(def-method* (year v)
+	  (+ year-1900 1900))
 
-       (method (hour-paddedstring v)
-	       (number->padded-string 2 (localtime.hour v)))
-       (method (min-paddedstring v)
-	       (number->padded-string 2 (localtime.min v)))
-       (method (sec-paddedstring v)
-	       (number->padded-string 2 (localtime.sec v)))
+	(def-method (year-string v)
+	  (number->string (localtime.year v)))
 
-       (method (tzoffset-string v)
-	       (let* ((tzoffset  (localtime.tzoffset v))
-		      ;; ^ is this really correct?
-		      (tzoffsetm (abs tzoffset))
-		      (tzoffset-hours (quotient tzoffsetm 3600))
-		      (tzoffset-minutes (quotient (modulo tzoffsetm 3600) 60)))
-		 (string-append (if (negative? tzoffset) "-" "+")
-				(number->padded-string 2 tzoffset-hours)
-				(number->padded-string 2 tzoffset-minutes))))
+	(def-method* (mday-string v)
+	  (number->string mday))
+
+	(def-method* (hour-paddedstring v)
+	  (number->padded-string 2 hour))
+
+	(def-method* (min-paddedstring v)
+	  (number->padded-string 2 min))
+
+	(def-method* (sec-paddedstring v)
+	  (number->padded-string 2 sec))
+
+	;; seconds
+	(def-method* (tzoffset tm) 
+	  (- (* integer-isdst 3600)
+	     integer-timezone))
+
+	(def-method* (tzoffset-string v)
+	  (let* ((tzoffset (localtime.tzoffset v))
+		 ;; ^ is this really correct?
+		 (tzoffsetm (abs tzoffset))
+		 (tzoffset-hours (quotient tzoffsetm 3600))
+		 (tzoffset-minutes (quotient (modulo tzoffsetm 3600) 60)))
+	    (string-append (if (negative? tzoffset) "-" "+")
+			   (number->padded-string 2 tzoffset-hours)
+			   (number->padded-string 2 tzoffset-minutes))))
 
 
-       ;; seconds
-       (method (tzoffset tm) 
-	       (- (* (localtime.integer-isdst tm) 3600)
-		  (localtime.integer-timezone tm)))
+	;; more date -R format
 
+	(def-method* (wday-shortstring v)
+	  (vector-ref rfc-2822:wdays integer-wday))
 
-       ;; more date -R format
+	(def-method* (wday-longstring v)
+	  (vector-ref english:weekdays integer-wday))
 
-       (method (wday-shortstring v)
-	       (vector-ref rfc-2822:wdays (.integer-wday v)))
+	(def-method* (month-shortstring v)
+	  (vector-ref rfc-2822:months month-1))
 
-       (method (wday-longstring v)
-	       (vector-ref english:weekdays (.integer-wday v)))
+	(def-method (time-string v #!optional (with-seconds? #t))
+	  (string-append
+	   (localtime.hour-paddedstring v)
+	   ":"
+	   (localtime.min-paddedstring v)
+	   (if with-seconds?
+	       (string-append ":"
+			      (localtime.sec-paddedstring v))
+	       "")))
 
-       (method (month-shortstring v)
-	       (vector-ref rfc-2822:months (.month-1 v)))
+	(def-method (rfc-2822-alike-string v maybe-zone-string
+					   #!optional (show-zone? #t))
+	  (string-append (localtime.wday-shortstring v)
+			 ", "
+			 (localtime.mday-string v)
+			 " "
+			 (localtime.month-shortstring v)
+			 " "
+			 (localtime.year-string v)
+			 " "
+			 (localtime.time-string v)
+			 (if show-zone? " " "")
+			 (if show-zone?
+			     (or maybe-zone-string
+				 (localtime.tzoffset-string v))
+			     "")))
 
-       (method (time-string v #!optional (with-seconds? #t))
-	       (string-append
-		(localtime.hour-paddedstring v)
-		":"
-		(localtime.min-paddedstring v)
-		(if with-seconds?
-		    (string-append ":"
-				   (localtime.sec-paddedstring v))
-		    "")))
+	(def-method (rfc-2822 v)
+	  (.rfc-2822-alike-string v #f))
 
-       (method (rfc-2822-alike-string v maybe-zone-string
-				      #!optional (show-zone? #t))
-	       (string-append (localtime.wday-shortstring v)
-			      ", "
-			      (localtime.mday-string v)
-			      " "
-			      (localtime.month-shortstring v)
-			      " "
-			      (localtime.year-string v)
-			      " "
-			      (localtime.time-string v)
-			      (if show-zone? " " "")
-			      (if show-zone?
-				  (or maybe-zone-string
-				      (localtime.tzoffset-string v))
-				  "")))
+	(def-method (gmtime-string v)
+	  (.rfc-2822-alike-string v "GMT"))
 
-       (method (rfc-2822 v)
-	       (.rfc-2822-alike-string v #f))
-
-       (method (gmtime-string v)
-	       (.rfc-2822-alike-string v "GMT"))
-
-       (method (localtime-string v)
-	       (.rfc-2822-alike-string v #f #f)))
+	(def-method (localtime-string v)
+	  (.rfc-2822-alike-string v #f #f)))
 
 
