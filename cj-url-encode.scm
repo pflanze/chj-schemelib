45174c39bb93fb115a266054d150fa344e20176d
Luke Brandon <luke@arcolaenergy.com>
2018-01-25 17:50:30 +0000

    cj-url-encode: speed up |url-encoding:unreserved?|

--- cj-url-encode.scm
+++ cj-url-encode.scm
@@ -21,19 +21,22 @@
 (define-typed (url-encoding:reserved? #(char? c))
   (and (memq c url-encoding:reserved-chars) #t))
 
-(define (url-encoding:unreserved? c)
-  (let* ((is (lambda (c2)
-	       (eq? c c2)))
-	 (n (char->integer c))
-	 (_in-range (cut <= <> n <>))
-	 (in-range (on char->integer _in-range)))
-    (or (in-range #\A #\Z)
-	(in-range #\a #\z)
-	(in-range #\0 #\9)
-	(is #\-)
-	(is #\_)
-	(is #\.)
-	(is #\~))))
+(define-typed (url-encoding:unreserved? #(char? c))
+  (declare (not safe) (fixnum))
+  (case c
+    ((#\- #\_ #\. #\~) #t)
+    (else
+     (let ((cn (char->integer c)))
+       (or (<= (insert-result-of (char->integer #\a))
+	       cn
+	       (insert-result-of (char->integer #\z)))
+	   (<= (insert-result-of (char->integer #\A))
+	       cn
+	       (insert-result-of (char->integer #\Z)))
+	   (<= (insert-result-of (char->integer #\0))
+	       cn
+	       (insert-result-of (char->integer #\9))))))))
+
 (TEST
  > (url-encoding:unreserved? #\c)
  #t
