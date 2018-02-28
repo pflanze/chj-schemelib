e240b8b55c68ce36f9bcbb7ddbf68e56511fd0cf
Christian Jaeger <ch@christianjaeger.ch>
2018-02-28 12:50:15 +0000

    predicates-1: add |list-of/length|

--- predicates-1.scm
+++ predicates-1.scm
@@ -58,6 +58,7 @@
 	length-is ;; see also list-of/length  -- rename to list-of-length ?
 	length=
 	list-of-length
+	list-of/length
 	lists?
 	0..1? ;; see also rgb:0..1?
 	in-signed-range?
@@ -359,6 +360,32 @@
  (#t #f #f #f #f #f)
  > (map (list-of-length 4) vals)
  (#f #f #f #f #f #f))
+
+(define (list-of/length pred n)
+  (lambda (v)
+    (let lp ((v v)
+	     (len 0))
+      (cond ((pair? v)
+	     (if (and (pred (car v))
+		      (< len n))
+		 (lp (cdr v) (inc len))
+		 #f))
+	    ((null? v)
+	     (= len n))
+	    (else
+	     ;; not a list
+	     #f)))))
+
+(TEST
+ > (def f (list-of/length number? 3))
+ > (f '(1 2))
+ #f
+ > (f '(1 2 3))
+ #t
+ > (f '(1 2 4 4))
+ #f
+ > (f '(1 2 a))
+ #f)
 
 
 (define lists? (list-of list?))
