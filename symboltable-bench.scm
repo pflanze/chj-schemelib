efab87fd4296e44c2267cfc7f2eab89fcaa5d2ba
Christian Jaeger <ch@christianjaeger.ch>
2018-04-23 09:15:10 +0100

    create symboltable-bench.scm

---
+++ symboltable-bench.scm
@@ -0,0 +1,58 @@
+(require easy
+	 symboltable)
+
+(include "lib/cj-standarddeclares.scm")
+
+(def (tag)
+     (=> 10
+	 random-hexstring
+	 .symbol
+	 list))
+
+(def tag?
+     (%all-of pair?
+	      (=>* car symbol?)))
+
+;; (def maket
+;;      (=>* iota
+;; 	  (.map (lambda (i)
+;; 		  (cons (tag) i)))
+;; 	  list->table))
+
+;; much HP48 feeling?:
+
+
+(def makealis
+     (=>* iota
+	  (.map (lambda (i)
+		  (cons (tag) i)))))
+
+(def maket
+     (=>* makealis
+	  list->table))
+
+
+(def (rt [table? t] [tag? k] [exact-natural? n])
+     (time (repeat n (table-ref t k))))
+
+
+(def makes
+     (=>* makealis
+	  (.map (lambda (kv)
+		  (cons (caar kv) kv)))
+	  list->symboltable))
+
+(def (rs [symboltable? t] [tag? k] [exact-natural? n])
+     (time (repeat n (symboltable-ref t (car k) #f))))
+
+
+(TEST
+ > (def t (maket 100))
+ > (exact-natural0? (rt t (=> (.list t) .first car) 4000000))
+ #t
+ > (def s (makes 100))
+ > ((pair-of tag? exact-natural0?)
+    (rs s (=> (.list s) .first) 4000000))
+ #t)
+
+
