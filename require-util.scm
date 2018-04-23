8a7ed6e78e2f328f04cb0caaa2dae1ac38c8e07b
Christian Jaeger <ch@christianjaeger.ch>
2018-04-24 00:13:26 +0100

    require-util: finally exclude include files

--- require-util.scm
+++ require-util.scm
@@ -104,7 +104,8 @@
 
 ;; path before normalization
 (def (require-util:modulepath-ignore? path)
-     (or (string-starts-with? path "doc/")
+     (or (require:include-path? path)
+	 (string-starts-with? path "doc/")
 	 (string-starts-with? path "Attic/")
 	 (string-starts-with? path "Trash/")
 	 (string-starts-with? path "trash/")
