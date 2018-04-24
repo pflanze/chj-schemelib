253fd9ff810bf4587a3e9ac2b45905ebb7102a3f
Christian Jaeger <ch@christianjaeger.ch>
2018-04-24 03:34:04 +0100

    create fixnum-test.scm

---
+++ fixnum-test.scm
@@ -0,0 +1,27 @@
+;;; Copyright 2018 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+(require fixnum
+	 (cj-gambit-sys max-fixnum)
+	 test)
+
+
+;; "should allow anything that works for vector access. Then could do
+;; unsafe. What about wrap-around?"
+
+(TEST
+ > (with-exception-catcher fixnum-overflow-exception?
+			   (& (fx+ max-fixnum 1)))
+ #t
+ > (with-exception-catcher fixnum-overflow-exception?
+			   (& (inc max-fixnum)))
+ #t
+ > (with-exception-catcher fixnum-overflow-exception?
+			   (& (dec min-fixnum)))
+ #t)
+
