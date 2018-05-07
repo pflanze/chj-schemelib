f50c40bb74e81e801255dded8e9fe365bb37c285
Christian Jaeger <ch@christianjaeger.ch>
2018-05-07 15:23:03 +0100

    add maybe-list.scm

---
+++ maybe-list.scm
@@ -0,0 +1,16 @@
+(require easy)
+
+(include "lib/cj-standarddeclares.scm")
+
+
+(def (list-maybe-ref lis [natural0? n])
+     (cond
+      ((null? lis) #f)
+      ((zero? n) (first lis))
+      (else (list-maybe-ref (rest lis) (- n 1)))))
+
+
+(def (maybe-fifth lis)
+     (list-maybe-ref lis 4))
+
+
