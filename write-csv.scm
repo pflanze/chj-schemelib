a885417b7cc7f86f112f79752d8c42d363b57a17
Christian Jaeger <ch@christianjaeger.ch>
2018-05-23 22:21:46 +0100

    add copyright/license

--- write-csv.scm
+++ write-csv.scm
@@ -1,3 +1,11 @@
+;;; Copyright 2016-2018 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
 (require easy
 	 (oo-util-lazy iseq?)
 	 U
