0ec99d9ef74e2ac1d491e881f6bc2c7af0d9f634
Christian Jaeger <ch@christianjaeger.ch>
2018-06-08 21:51:00 +0100

    string-case-bench: compare with memcmp
    Wow. Well, in very mixed cases or moderately ones if I speed up the
    code it might still beat it.
    
        > (string-case-bench "hello lovely world how are you today? it's been a long way." 10000000)
        (time (repeat n (t1 str)))
    	1722 ms real time
    	1708 ms cpu time (1708 user, 0 system)
    	no collections
    	no bytes allocated
    	no minor faults
    	no major faults
        (time (repeat n (t2 str)))
    	2267 ms real time
    	2248 ms cpu time (2248 user, 0 system)
    	no collections
    	no bytes allocated
    	no minor faults
    	no major faults
        (time (repeat n (t3 str)))
    	254 ms real time
    	236 ms cpu time (236 user, 0 system)
    	no collections
    	no bytes allocated
    	no minor faults
    	no major faults
        #t
        > (string-case-bench "foo" 10000000)
        (time (repeat n (t1 str)))
    	114 ms real time
    	112 ms cpu time (112 user, 0 system)
    	no collections
    	no bytes allocated
    	no minor faults
    	no major faults
        (time (repeat n (t2 str)))
    	1357 ms real time
    	1356 ms cpu time (1356 user, 0 system)
    	no collections
    	no bytes allocated
    	no minor faults
    	no major faults
        (time (repeat n (t3 str)))
    	50 ms real time
    	48 ms cpu time (48 user, 0 system)
    	no collections
    	no bytes allocated
    	no minor faults
    	no major faults
        #t
        > (string-case-bench "hx" 10000000)
        (time (repeat n (t1 str)))
    	253 ms real time
    	252 ms cpu time (252 user, 0 system)
    	no collections
    	no bytes allocated
    	no minor faults
    	no major faults
        (time (repeat n (t2 str)))
    	1387 ms real time
    	1380 ms cpu time (1380 user, 0 system)
    	no collections
    	no bytes allocated
    	no minor faults
    	no major faults
        (time (repeat n (t3 str)))
    	216 ms real time
    	216 ms cpu time (216 user, 0 system)
    	no collections
    	no bytes allocated
    	no minor faults
    	no major faults
        #t

--- string-case-bench.scm
+++ string-case-bench.scm
@@ -29,7 +29,35 @@
 	    'theworld2)
 	   (else 'nomatch)))
 
+
+
+(c-declare "
+#include <string.h>
+")
+(def (@memcmp:string=? s1 s2)
+     (##c-code "
+int l1= ___INT(___STRINGLENGTH(___ARG1));
+int l2= ___INT(___STRINGLENGTH(___ARG2));
+
+if (l1==l2) {
+    ___RESULT= memcmp(___BODY(___ARG1), ___BODY(___ARG2), l1*4)==0
+                 ? ___TRU : ___FAL;
+} else {
+    ___RESULT= ___FAL;
+}
+" s1 s2))
+
+(def (t3 v)
+     (cond ((@memcmp:string=? v "ho") 'ho)
+	   ((@memcmp:string=? v "hi") 'yes)
+	   ((@memcmp:string=? v "hello world") 'theworld)
+	   ((@memcmp:string=? v "hello lovely world how are you today? it's been a long way.")
+	    'theworld2)
+	   (else 'nomatch)))
+
+
 (def (string-case-bench str n)
-     (equal? (time (repeat n (t1 str)))
-	     (time (repeat n (t2 str)))))
+     (equal?* (time (repeat n (t1 str)))
+	      (time (repeat n (t2 str)))
+	      (time (repeat n (t3 str)))))
 
