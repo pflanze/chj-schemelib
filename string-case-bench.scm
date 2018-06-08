92e235349e267dd8bd54ac05a3a469598ecc2e2f
Christian Jaeger <ch@christianjaeger.ch>
2018-06-08 01:24:51 +0100

    create string-case-bench.scm
    Yes it's consistently faster, not sure it was worth the effort though?
    
    > (string-case-bench "hello world" 10000000)
    (time (repeat n (t1 str)))
        447 ms real time
        436 ms cpu time (436 user, 0 system)
        no collections
        no bytes allocated
        no minor faults
        no major faults
    (time (repeat n (t2 str)))
        1152 ms real time
        1152 ms cpu time (1152 user, 0 system)
        no collections
        no bytes allocated
        no minor faults
        no major faults
    theworld
    > (string-case-bench "hi" 10000000)
    (time (repeat n (t1 str)))
        246 ms real time
        244 ms cpu time (240 user, 4 system)
        no collections
        no bytes allocated
        no minor faults
        no major faults
    (time (repeat n (t2 str)))
        673 ms real time
        676 ms cpu time (676 user, 0 system)
        no collections
        no bytes allocated
        no minor faults
        no major faults
    yes
    > (string-case-bench "xhi" 10000000)
    (time (repeat n (t1 str)))
        128 ms real time
        128 ms cpu time (128 user, 0 system)
        no collections
        no bytes allocated
        no minor faults
        no major faults
    (time (repeat n (t2 str)))
        958 ms real time
        956 ms cpu time (956 user, 0 system)
        no collections
        no bytes allocated
        no minor faults
        no major faults
    nomatch

---
+++ string-case-bench.scm
@@ -0,0 +1,31 @@
+;;; Copyright 2018 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+(require easy
+	 string-case)
+
+(include "cj-standarddeclares.scm")
+
+
+(def (t1 v)
+     (string-case v
+		  (("ho") 'ho)
+		  (("hi") 'yes)
+		  (("hello world") 'theworld)
+		  (else 'nomatch)))
+
+(def (t2 v)
+     (cond ((string=? v "ho") 'ho)
+	   ((string=? v "hi") 'yes)
+	   ((string=? v "hello world") 'theworld)
+	   (else 'nomatch)))
+
+(def (string-case-bench str n)
+     (time (repeat n (t1 str)))
+     (time (repeat n (t2 str))))
+
