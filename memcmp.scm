0e896aaec57520acff1b396151a42105bf9305da
Christian Jaeger <ch@christianjaeger.ch>
2018-06-10 00:25:32 +0100

    memcmp: inlining via macros, provide safe variant

--- memcmp.scm
+++ memcmp.scm
@@ -6,16 +6,25 @@
 ;;;    (at your option) any later version.
 
 
-(require)
+(require define-macro-star
+	 cj-symbol)
 
-(export @memcmp:string=?)
+(export (macro memcmp:@string=?) memcmp:@string=?-function
+	(macro memcmp:string=?) memcmp:string=?-function)
+
+(include "cj-standarddeclares.scm")
 
 
-(c-declare "
+(define-macro* (use-memcmp)
+  (if (mod:compiled?)
+      `(c-declare "
 #include <string.h>
 ")
-(define (@memcmp:string=? s1 s2)
-  (##c-code "
+      `(begin)))
+
+(define-macro* (memcmp:@string=? s1 s2)
+  (if (mod:compiled?)
+      `(##c-code "
 int l1= ___INT(___STRINGLENGTH(___ARG1));
 int l2= ___INT(___STRINGLENGTH(___ARG2));
 
@@ -25,5 +34,31 @@ if (l1==l2) {
 } else {
     ___RESULT= ___FAL;
 }
-" s1 s2))
+" ,s1 ,s2)
+      `(memcmp:@string=?-function ,s1 ,s2)))
+
+(define (memcmp:string=?-error s1 s2)
+  (error "memcmp:string=?: need two strings, got:" s1 s2))
+
+(define-macro* (memcmp:string=? s1 s2)
+  (if (mod:compiled?)
+      (with-gensyms
+       (S1 S2)
+       `(let ((,S1 ,s1)
+	      (,S2 ,s2))
+	  (if (and (string? ,S1)
+		   (string? ,S2))
+	      (memcmp:@string=? ,S1 ,S2)
+	      (memcmp:string=?-error ,S1 ,S2))))
+      `(memcmp:string=?-function ,s1 ,s2)))
+
+
+(use-memcmp)
+
+(define (memcmp:@string=?-function s1 s2)
+  (memcmp:@string=? s1 s2))
+
+(define (memcmp:string=?-function s1 s2)
+  (memcmp:string=? s1 s2))
+
 
