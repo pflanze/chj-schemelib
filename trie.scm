f089d2f3ef47ff1d0118a23fa5b3e63d7d6a6b27
Christian Jaeger <ch@christianjaeger.ch>
2018-06-10 14:17:28 +0100

    trie: move vector map code to vectormap.scm

--- trie.scm
+++ trie.scm
@@ -6,103 +6,49 @@
 ;;;    (at your option) any later version.
 
 
-;; a trie based on a sorted vector as map via binsearch
+;; a trie based on a vectormap
 
-;; Requires .cmp as well as .list for the non-"-list" methods on the
+;; Requires (.cmp as well as) .list for the non-"-list" methods on the
 ;; key type.
 
 ;; XX NOTE: this currently has bad complexity for the add operation
 ;; due to copying whole vectors (may not matter for actual usage but
 ;; be warned).
 
+
 (require easy
-	 vector-binsearch
+	 vectormap
 	 cj-cmp
 	 Maybe)
 
 (export (class trie)
 	(method alist.trie)
 	#!optional
-	trie-map-vector?)
+	trie-vectormap?)
 
 (include "cj-standarddeclares.scm")
 
-;; XX move
 
-(def. char.cmp char-cmp)
-(def. string.cmp string-cmp)
-(def. number.cmp number-cmp)
-(def. symbol.cmp symbol-cmp)
-(def. u8vector.cmp u8vector-cmp)
-
-(def (vector-every/start+end t? v istart iend)
-     (let ((len (vector-length v)))
-       (and (<= 0 istart len)
-	    (<= 0 iend len)
-	    (<= istart iend)
-	    (let lp ((i istart))
-	      (if (< i iend)
-		  (and (t? (vector-ref v i))
-		       (lp (inc i)))
-		  #t)))))
-
-(def (sectioned-vector-of/2 pred1 pred2)
-     (lambda (v)
-       (and (vector? v)
-	    (let ((len (vector-length v)))
-	      (and (even? len)
-		   (let ((halflen (arithmetic-shift len -1)))
-		     (and (vector-every/start+end pred1 v 0 halflen)
-			  (vector-every/start+end pred2 v halflen len))))))))
-
-(TEST
- > (def f? (sectioned-vector-of/2 number? symbol?))
- > (f? '[])
- #t
- > (f? '[1 2])
- #f
- > (f? '[1 a])
- #t
- > (f? '[a 1])
- #f
- > (f? '[b a])
- #f
- > (f? '[b])
- #f
- > (f? '[1])
- #f
- > (f? '[1 2 3 a])
- #f
- > (f? '[1 2 3 a b])
- #f
- > (f? '[1 2 3 a b c])
- #t
- > (f? '[1 2 3 a b c d])
- #f)
-
-;;/move
-
-
-(defclass (trie [trie-map-vector? entries]
+(defclass (trie [trie-vectormap? entries]
 		[Maybe? Maybe-value])
 
   (def (entries-alist.trie entries-alist Maybe-value)
-       (trie (alist.trie-map-vector entries-alist)
+       (trie (alist.vectormap entries-alist)
 	     Maybe-value))
     
   (defmethod (show s)
     `(entries-alist.trie
-      ,(.show (trie-map-vector.alist entries))
+      ,(.show (vectormap.alist entries))
       ,(.show Maybe-value)))
 
   (defmethod (entries-alist s)
-    (trie-map-vector.alist entries))
+    (vectormap.alist entries))
 
   (defmethod (entries-length s)
-    (trie-map-vector.length entries))
+    (vectormap.length entries))
 
   (defmethod (empty? s)
-    (trie-map-vector.empty? entries))
+    (vectormap.empty? entries))
   
   ;; report the next branching or value holding point (could be the
   ;; end)
@@ -116,11 +62,11 @@
       (if (Just? (trie.Maybe-value t))
 	  (values l items #t t)
 	  (let* ((es (trie.entries t))
-		 (len (trie-map-vector.length es)))
+		 (len (vectormap.length es)))
 	    (if (= len 1)
 		(lp (inc l)
-		    (cons (trie-map-vector.@the-key es) items)
-		    (trie-map-vector.@the-value es))
+		    (cons (vectormap.@the-key es) items)
+		    (vectormap.@the-value es))
 		(values l items #f t))))))
   
   (defmethod (next-branch s [fixnum-natural0? current-level])
@@ -132,7 +78,7 @@
     (if (null? cs)
 	Maybe-value
 	(let-pair ((c cs*) cs)
-		  (if-let ((s* (trie-map-vector.maybe-ref entries c)))
+		  (if-let ((s* (vectormap.maybe-ref entries c)))
 			  (trie.Maybe-ref-list s* cs*)
 			  (Nothing)))))
 
@@ -152,10 +98,10 @@
 		    (let ((entries (trie.entries s)))
 		      (trie.entries-set
 		       s
-		       (trie-map-vector.set
+		       (vectormap.set
 			entries
 			c
-			(rec (or (trie-map-vector.maybe-ref entries c)
+			(rec (or (vectormap.maybe-ref entries c)
 				 empty-trie)
 			     cs*))))))))
 
@@ -176,7 +122,7 @@
 		       key-str #f)))
 
 
-(def trie-map-vector? (sectioned-vector-of/2 any? trie?))
+(def trie-vectormap? (sectioned-vector-of/2 any? trie?))
 
 
 (def empty-trie (trie (vector) (Nothing)))
@@ -185,94 +131,6 @@
   (fold (lambda (k+v t)
 	  (let-pair ((k v) k+v)
 		    (trie.add t k v))) empty-trie ss))
-
-(def (trie-map-vector.alist entries)
-     (let* ((len (vector-length entries))
-	    (middle (arithmetic-shift len -1)))
-       (let lp ((i (dec middle))
-		(out '()))
-	 (if (negative? i)
-	     out
-	     (lp (dec i)
-		 (cons (cons (vector-ref entries i)
-			     (vector-ref entries (+ middle i)))
-		       out))))))
-
-(def (alist.trie-map-vector as) ;; -> trie-map-vector?
-     (let* ((as (cmp-sort as (on car .cmp))) ;; offer sorted-alist. version ?
-	    (middle (length as))
-	    (len (arithmetic-shift middle 1))
-	    (entries (make-vector len)))
-       (let lp ((as as)
-		(i 0))
-	 (if (null? as)
-	     entries
-	     (let-pair ((a as*) as)
-		       (let-pair ((k v) a)
-				 (vector-set! entries i k)
-				 (vector-set! entries (+ middle i) v)
-				 (lp as* (inc i))))))))
-
-(TEST
- > (alist.trie-map-vector '())
- []
- > (trie-map-vector.alist #)
- ()
- > (alist.trie-map-vector '((#\a . 10) (#\x . 30) (#\c . -1)))
- [#\a #\c #\x 10 -1 30]
- > (trie-map-vector.alist #)
- ((#\a . 10) (#\c . -1) (#\x . 30)))
-
-
-(def (trie-map-vector.set entries c val)
-     ;; if key already exists, the result has the same length
-     (let* ((len (vector-length entries))
-	    (middle (arithmetic-shift len -1)))
-       (if-let ((i (vector-binsearch/start+end
-		    entries c .cmp 0 middle)))
-	       (let ((entries* (vector-copy entries)))
-		 (vector-set! entries* i c)
-		 (vector-set! entries* (+ middle i) val)
-		 entries*)
-	       ;; need to make a larger result than we have. XX could
-	       ;; optimize, except that would just be a constant
-	       ;; optimization whereas there may be a need to optimize
-	       ;; the complexity instead.
-	       (=> entries
-		   trie-map-vector.alist
-		   ((flip cons) (cons c val))
-		   alist.trie-map-vector))))
-
-(def (trie-map-vector.maybe-ref entries c)
-     (let* ((len (vector-length entries))
-	    (middle (arithmetic-shift len -1)))
-       (if-let ((i (vector-binsearch/start+end
-		    entries c .cmp 0 middle)))
-	       (vector-ref entries (+ middle i))
-	       #f)))
-
-(TEST
- > (def tv '[#\a #\c #\x 10 -1 30])
- > (trie-map-vector.set tv #\x 40)
- [#\a #\c #\x 10 -1 40]
- > (trie-map-vector.set tv #\b 40)
- [#\a #\b #\c #\x 10 40 -1 30]
- > tv
- [#\a #\c #\x 10 -1 30])
-
-
-(def (trie-map-vector.length entries)
-     (arithmetic-shift (vector-length entries) -1))
-
-(def (trie-map-vector.empty? entries)
-     (zero? (vector-length entries)))
-
-;; assuming 1 entry:
-(def (trie-map-vector.@the-key entries)
-     (vector-ref entries 0))
-(def (trie-map-vector.@the-value entries)
-     (vector-ref entries 1))
-
 
 (TEST
  > (.show empty-trie)
---
+++ vectormap.scm
@@ -0,0 +1,183 @@
+;;; Copyright 2018 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+;; A map implementation based on a vector via binary search.
+
+;; Requires .cmp
+
+;; XX NOTE: complexity for the add operation is bad due to copying
+;; whole vectors
+
+
+(require easy
+	 vector-binsearch
+	 cj-cmp
+	 Maybe)
+
+(export (methods char.cmp
+		 string.cmp
+		 number.cmp
+		 symbol.cmp
+		 u8vector.cmp) ;; move
+	vector-every/start+end
+	sectioned-vector-of/2
+	;; these are all functions in spite of dot
+	vectormap.alist
+	alist.vectormap
+	vectormap.set
+	vectormap.maybe-ref
+	vectormap.length
+	vectormap.empty?
+	vectormap.@the-key
+	vectormap.@the-value)
+
+(include "cj-standarddeclares.scm")
+
+;; XX move
+
+(def. char.cmp char-cmp)
+(def. string.cmp string-cmp)
+(def. number.cmp number-cmp)
+(def. symbol.cmp symbol-cmp)
+(def. u8vector.cmp u8vector-cmp)
+
+;;/move
+
+(def (vector-every/start+end t? v istart iend)
+     (let ((len (vector-length v)))
+       (and (<= 0 istart len)
+	    (<= 0 iend len)
+	    (<= istart iend)
+	    (let lp ((i istart))
+	      (if (< i iend)
+		  (and (t? (vector-ref v i))
+		       (lp (inc i)))
+		  #t)))))
+
+(def (sectioned-vector-of/2 pred1 pred2)
+     (lambda (v)
+       (and (vector? v)
+	    (let ((len (vector-length v)))
+	      (and (even? len)
+		   (let ((halflen (arithmetic-shift len -1)))
+		     (and (vector-every/start+end pred1 v 0 halflen)
+			  (vector-every/start+end pred2 v halflen len))))))))
+
+(TEST
+ > (def f? (sectioned-vector-of/2 number? symbol?))
+ > (f? '[])
+ #t
+ > (f? '[1 2])
+ #f
+ > (f? '[1 a])
+ #t
+ > (f? '[a 1])
+ #f
+ > (f? '[b a])
+ #f
+ > (f? '[b])
+ #f
+ > (f? '[1])
+ #f
+ > (f? '[1 2 3 a])
+ #f
+ > (f? '[1 2 3 a b])
+ #f
+ > (f? '[1 2 3 a b c])
+ #t
+ > (f? '[1 2 3 a b c d])
+ #f)
+
+
+(def (vectormap.alist entries)
+     (let* ((len (vector-length entries))
+	    (middle (arithmetic-shift len -1)))
+       (let lp ((i (dec middle))
+		(out '()))
+	 (if (negative? i)
+	     out
+	     (lp (dec i)
+		 (cons (cons (vector-ref entries i)
+			     (vector-ref entries (+ middle i)))
+		       out))))))
+
+(def (alist.vectormap as) ;; -> vectormap?
+     (let* ((as (cmp-sort as (on car .cmp))) ;; offer sorted-alist. version ?
+	    (middle (length as))
+	    (len (arithmetic-shift middle 1))
+	    (entries (make-vector len)))
+       (let lp ((as as)
+		(i 0))
+	 (if (null? as)
+	     entries
+	     (let-pair ((a as*) as)
+		       (let-pair ((k v) a)
+				 (vector-set! entries i k)
+				 (vector-set! entries (+ middle i) v)
+				 (lp as* (inc i))))))))
+
+(TEST
+ > (alist.vectormap '())
+ []
+ > (vectormap.alist #)
+ ()
+ > (alist.vectormap '((#\a . 10) (#\x . 30) (#\c . -1)))
+ [#\a #\c #\x 10 -1 30]
+ > (vectormap.alist #)
+ ((#\a . 10) (#\c . -1) (#\x . 30)))
+
+
+(def (vectormap.set entries c val)
+     ;; if key already exists, the result has the same length
+     (let* ((len (vector-length entries))
+	    (middle (arithmetic-shift len -1)))
+       (if-let ((i (vector-binsearch/start+end
+		    entries c .cmp 0 middle)))
+	       (let ((entries* (vector-copy entries)))
+		 (vector-set! entries* i c)
+		 (vector-set! entries* (+ middle i) val)
+		 entries*)
+	       ;; need to make a larger result than we have. XX could
+	       ;; optimize, except that would just be a constant
+	       ;; optimization whereas there may be a need to optimize
+	       ;; the complexity instead.
+	       (=> entries
+		   vectormap.alist
+		   ((flip cons) (cons c val))
+		   alist.vectormap))))
+
+(def (vectormap.maybe-ref entries c)
+     (let* ((len (vector-length entries))
+	    (middle (arithmetic-shift len -1)))
+       (if-let ((i (vector-binsearch/start+end
+		    entries c .cmp 0 middle)))
+	       (vector-ref entries (+ middle i))
+	       #f)))
+
+(TEST
+ > (def tv '[#\a #\c #\x 10 -1 30])
+ > (vectormap.set tv #\x 40)
+ [#\a #\c #\x 10 -1 40]
+ > (vectormap.set tv #\b 40)
+ [#\a #\b #\c #\x 10 40 -1 30]
+ > tv
+ [#\a #\c #\x 10 -1 30])
+
+
+(def (vectormap.length entries)
+     (arithmetic-shift (vector-length entries) -1))
+
+(def (vectormap.empty? entries)
+     (zero? (vector-length entries)))
+
+;; assuming 1 entry:
+(def (vectormap.@the-key entries)
+     (vector-ref entries 0))
+(def (vectormap.@the-value entries)
+     (vector-ref entries 1))
+
