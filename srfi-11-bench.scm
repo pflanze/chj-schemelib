01cd87959b15d5ce5c70a329dab3f0e2538ccd3b
Christian Jaeger <ch@christianjaeger.ch>
2018-06-14 18:37:26 +0100

    create srfi-11-bench.scm

---
+++ srfi-11-bench.scm
@@ -0,0 +1,20 @@
+;;; Copyright 2018 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+(require easy
+	 srfi-11)
+
+(export srfi-11-bench)
+
+(include "cj-standarddeclares.scm")
+
+
+(def (srfi-11-bench n)
+     (repeat n (letv ((a b) (values (* n n) (* (+ n 1) n)))
+		     b)))
+
