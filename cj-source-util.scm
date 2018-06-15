287613e60f6234c355bdf5f9c445eec9e0106a57
Christian Jaeger <ch@christianjaeger.ch>
2018-06-15 15:53:34 +0100

    cj-source-util: split off tests into cj-source-util-test.scm
    for lowering dependencies

--- cj-source-util.scm
+++ cj-source-util.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2010, 2011 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2010-2018 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
@@ -8,7 +8,9 @@
 
 ;; XX move to cj-source-lambda.scm?
 
-(require test)
+(require)
+
+;; tests see cj-source-util-test.scm
 
 
 (define (schemedefinition-arity:template->checker t)
@@ -107,66 +109,6 @@
    (schemedefinition-arity:pattern->template
     x)))
 
-(TEST
- > (define c (schemedefinition-arity-checker '(a b c . d)))
- > (c 2)
- not-enough
- > (c 3)
- ok
- > (c 4)
- ok
- > (c 500)
- ok
- > (define c (schemedefinition-arity-checker '(a b c #!rest d)))
- > (c 2)
- not-enough
- > (c 3)
- ok
- > (c 4)
- ok
- > (c 500)
- ok
- > (define c (schemedefinition-arity-checker '(a b c #!optional d)))
- > (c 2)
- not-enough
- > (c 3)
- ok
- > (c 4)
- ok
- > (c 5)
- too-many
- > (define c (schemedefinition-arity-checker '(a b c #!key d)))
- > (c 2)
- not-enough
- > (c 3)
- ok
- > (c 4)
- ok ;; uhm, actually invalid!
- > (c 5)
- ok
- > (c 6)
- too-many
- > (define c (schemedefinition-arity-checker '(a b c #!rest d)))
- > (c 2)
- not-enough
- > (c 3)
- ok
- > (c 4)
- ok
- > (c 5)
- ok
- > (define c (schemedefinition-arity-checker '(a b c #!key d #!rest e)))
- > (c 2)
- not-enough
- > (c 3)
- ok
- > (c 4)
- ok ;; uhm, actually invalid!
- > (c 5)
- ok
- > (c 6)
- ok
- )
 
 (include "improper-length.scm")
 
@@ -179,28 +121,4 @@
 	  ((not-enough) (err "not enough arguments"))
 	  ((too-many) (err "too many arguments"))
 	  (else (error "bug in safer-apply"))))))
-
-(TEST
- > (define c (schemedefinition-arity:pattern->template '(a b c #!optional d)))
- > (define f (lambda (a b c #!optional d) d))
- > (safer-apply c f '(10 20 30 40) error values)
- 40
- > (safer-apply c f '(10 20 30 40 50) vector values)
- #("too many arguments")
- > (safer-apply c f '(10 20 30) error values)
- #f)
-
-
-;; compare with Gambits apply:
-; > (apply inc '(1 2 3))
-; *** ERROR IN (console)@64.1 -- Wrong number of arguments passed to procedure
-; (inc 1 2 3)
-; 1> 
-; > (apply inc '())
-; *** ERROR IN (console)@65.1 -- Wrong number of arguments passed to procedure
-; (inc)
-; 1> 
-; > (apply inc '(1))
-; 2
-
 
