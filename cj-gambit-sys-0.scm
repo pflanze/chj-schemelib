73ff491843ec43edbcbbc5dd3d7e12c86fc6c018
Christian Jaeger <ch@christianjaeger.ch>
2018-07-29 21:51:02 +0100

    cj-gambit-sys-0: make fully namespace safe

--- cj-gambit-sys-0.scm
+++ cj-gambit-sys-0.scm
@@ -13,7 +13,7 @@
 	(I (gensym 'I)))
     `(##let ((,V ,v)
 	     (,I ,i))
-	    (declare (not safe))
+	    (##declare (not safe))
 	    (##vector-ref ,V ,I))))
 
 (define-macro* (@vector-set! v i val)
@@ -24,13 +24,13 @@
     `(##let ((,V ,v)
 	     (,I ,i)
 	     (,VAL ,val))
-	    (declare (not safe))
+	    (##declare (not safe))
 	    (##vector-set! ,V ,I ,VAL))))
 
 (define-macro* (@vector-length v)
   ;; need unsafe mode here, too?
   (let ((V (gensym 'V)))
     `(##let ((,V ,v))
-	    (declare (not safe))
+	    (##declare (not safe))
 	    (##vector-length ,V))))
 
