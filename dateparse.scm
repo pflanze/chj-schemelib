0e82cd563a7dc7bd0a20be71d4200425263c7e3a
Christian Jaeger <ch@christianjaeger.ch>
2018-10-18 23:56:09 +0100

    dateparse: return #f on errors
    This is (definitely) more proper than the odd exceptions. (Also, does
    enable caching of invalid inputs.)

--- dateparse.scm
+++ dateparse.scm
@@ -8,15 +8,21 @@
 	 test)
 
 ;; using `dateparse` from my chj-bin repository
-(def (slow-dateparse #(string? s)) -> unixtime?
+(def (slow-dateparse #(string? s)) -> (maybe unixtime?)
      (string->number (xbacktick "dateparse" "-u" "-B" s)))
+
+
+(def (maybe-number->string v)
+     (if v
+	 (number->string v)
+	 "Nothing"))
 
 (def dateparse:basedir ".dateparse-cachedir/")
 (%try (create-directory dateparse:basedir)) ;;XX should be done by fscache
 (def dateparse:cache (fscache dateparse:basedir
 			      car
 			      slow-dateparse
-			      number->string
+			      maybe-number->string
 			      string->number))
 
 (def dateparse (.call* dateparse:cache))
@@ -27,5 +33,7 @@
  > (dateparse "2001/10/3 12:33 MET")
  1002108780
  > (dateparse "2001/10/3 12:33 MEST")
- 1002105180)
+ 1002105180
+ > (dateparse "2001/100/3 12:33 GMT")
+ #f)
 
