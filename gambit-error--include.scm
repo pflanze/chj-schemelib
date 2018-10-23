ed9edb4ed44048e767873a6eca6b20935bbe93d5
Christian Jaeger <ch@christianjaeger.ch>
2018-10-23 14:29:08 +0100

    gambit-error--include: no need to modify copy anymore

--- gambit-error--include.scm
+++ gambit-error--include.scm
@@ -1,22 +1,21 @@
 
 ;; from lib/_kernel#.scm 144b72bf174d1f4baa17aaa11ca5909726076d50
-;; but with   constructor: #f commented out
 
 (define-library-type-of-exception heap-overflow-exception
   id: d69cd396-01e0-4dcb-87dc-31acea8e0e5f
-;;  constructor: #f
+  constructor: #f
   opaque:
 )
 
 (define-library-type-of-exception stack-overflow-exception
   id: f512c9f6-3b24-4c5c-8c8b-cabd75b2f951
-;;  constructor: #f
+  constructor: #f
   opaque:
 )
 
 (define-library-type-of-exception nonprocedure-operator-exception
   id: f39d07ce-436d-40ca-b81f-cdc65d16b7f2
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (operator  unprintable: read-only:)
@@ -27,7 +26,7 @@
 
 (define-library-type-of-exception wrong-number-of-arguments-exception
   id: 2138cd7f-8c42-4164-b56a-a8c7badf3323
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (procedure unprintable: read-only:)
@@ -36,7 +35,7 @@
 
 (define-library-type-of-exception keyword-expected-exception
   id: 3fd6c57f-3c80-4436-a430-57ea4457c11e
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (procedure unprintable: read-only:)
@@ -45,7 +44,7 @@
 
 (define-library-type-of-exception unknown-keyword-argument-exception
   id: 3f9f8aaa-ea21-4f2b-bc06-f65950e6c408
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (procedure unprintable: read-only:)
@@ -54,7 +53,7 @@
 
 (define-library-type-of-exception cfun-conversion-exception
   id: 9f09b552-0fb7-42c5-b0d4-212155841d53
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (procedure unprintable: read-only:)
@@ -65,7 +64,7 @@
 
 (define-library-type-of-exception sfun-conversion-exception
   id: 54dfbc02-718d-4a34-91ab-d1861da7500a
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (procedure unprintable: read-only:)
@@ -76,13 +75,13 @@
 
 (define-library-type-of-exception multiple-c-return-exception
   id: 73c66686-a08f-4c7c-a0f1-5ad7771f242a
-;;  constructor: #f
+  constructor: #f
   opaque:
 )
 
 (define-library-type-of-exception number-of-arguments-limit-exception
   id: f9519b37-d6d4-4748-8eb1-a0c8dc18c5e7
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (procedure unprintable: read-only:)
@@ -91,7 +90,7 @@
 
 (define-library-type-of-exception type-exception
   id: cf06eccd-bf2c-4b30-a6ce-394b345a0dee
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (procedure unprintable: read-only:)
@@ -102,7 +101,7 @@
 
 (define-library-type-of-exception os-exception
   id: c1fc166b-d951-4871-853c-2b6c8c12d28d
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (procedure unprintable: read-only:)
@@ -113,7 +112,7 @@
 
 (define-library-type-of-exception no-such-file-or-directory-exception
   id: 299ccee1-77d2-4a6d-ab24-2ebf14297315
-;;  constructor: #f
+  constructor: #f
   opaque:
 
   (procedure unprintable: read-only:)
