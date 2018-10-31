68e0cefb1dce653d59233bc1fa6cbf3afc688bd5
Christian Jaeger <ch@christianjaeger.ch>
2018-10-31 19:01:40 +0000

    update copyright year
    (Last real coding was in 2014 but that's newer anyway, too.)

--- cj-posixpath.scm
+++ cj-posixpath.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2013 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2013-2018 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
