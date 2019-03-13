01cd1025d17f4c2437f9f7f4c0e7c80d41457709
Christian Jaeger <ch@christianjaeger.ch>
2019-03-13 21:46:44 +0000

    cj-posix-example: update
    This hasn't seen updates for a while, apparently.

--- posix/cj-posix-example.scm
+++ posix/cj-posix-example.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2013-2016 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2006-2019 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
@@ -10,13 +10,12 @@
 	 posix/cj-posix
 	 string-util ;; which one? grr, so..:
 	 string-util-1
-	 string-util-2
+	 string-util-2 ;; incl. string-find-char
 	 string-util-3
 	 posix/interrupts
 	 (test TEST)
-	 )
+         (srfi-11 values->vector))
 
-;; cj Thu, 21 Dec 2006 01:41:15 +0100
 
 (define BUFSIZ 10)
 
@@ -25,30 +24,44 @@
   (list->string
    (map integer->char (u8vector->list (subu8vector vec from to)))))
 
+(define (string-tr str from to)
+  ;; inefficient, too
+  (list->string
+   (map (lambda (c)
+          (cond ((string-find-char from c)
+                 => (lambda (i)
+                      (string-ref to i)))
+                (else c)))
+        (string->list str))))
+
+
 (define (tr)
-  ;; install a noop handler (to override the builtin one which clobbers the process statii):
+  ;; install a noop handler (to override the builtin one which
+  ;; clobbers the process statii):
   (interrupt-install-handler! SIGCHLD (& #f))
-  (let* ((p (pipe))
-	 (pid (fork*
+  (let* ((p (posix:pipe))
+	 (pid (posix:fork*
 	       (lambda ()
-		 (close (filedescriptors-read p))
-		 (dup2 (filedescriptors-write p) 1)
-		 (exec ;;"bash" "-"
-		  "perl" "-MTime::HiRes=sleep" "-we" "$|++; for(1..13){sleep 0.01; print \"abc\"; }")))))
-    (close (filedescriptors-write p))
+		 (posix:close (filedescriptors-read p))
+		 (posix:dup2 (filedescriptors-write p) 1)
+		 (posix:exec ;;"bash" "-"
+		  "perl"
+                  "-MTime::HiRes=sleep"
+                  "-we"
+                  "$|++; for(1..13){sleep 0.01; print \"abc\"; }")))))
+    (posix:close (filedescriptors-write p))
     (let ((p (fd->port (filedescriptors-read p) 'input))
 	  (buf (make-u8vector BUFSIZ)))
       (let lp ()
 	(let ((num (read-subu8vector buf 0 BUFSIZ p)))
 	  (if (eq? num 0)
-	      (waitpid* pid)
-	      (let ((str (u8vector->string buf 0 num)))
-		(string-tr! str "a" "Q")
-		(println str)
+	      (posix:waitpid* pid)
+	      (begin
+		(println (string-tr (u8vector->string buf 0 num) "a" "Q"))
 		(lp))))))))
 
 (TEST
- > (with-output-to-string "" tr)
+ > (fst (with-output-to-string tr))
  "QbcQbcQbcQ\nbcQbcQbcQb\ncQbcQbcQbc\nQbcQbcQbc\n"
  ;; (Note that read-subu8vector does not return upon the first write,
  ;; it waits until the buffer is exactly full (or till close). This is
