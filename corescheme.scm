4bda298b5c67fe6674681762a58dab8b52cd27a3
Christian Jaeger <ch@christianjaeger.ch>
2019-03-17 00:32:45 +0000

    copyright

--- corescheme.scm
+++ corescheme.scm
@@ -1,4 +1,10 @@
-;; Copyright 2016 by Christian Jaeger <ch@christianjaeger.ch>
+;; Copyright 2016-2019 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
 
 ;; Parse source into a core Scheme AST.
 
