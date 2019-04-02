9df0c78cb884895820a8c6bf0003414b2c201399
Christian Jaeger <ch@christianjaeger.ch>
2019-04-02 08:17:42 +0100

    C: add |C1|

--- C.scm
+++ C.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2013 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2013-2019 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
@@ -8,8 +8,12 @@
 
 (require define-macro-star
 	 cj-symbol
+	 cj-symbol-with
 	 ;; (cj-env-1 identity) cj-source, sigh
 	 (srfi-1 filter))
+
+(export (macro C)
+        (macro C1))
 
 
 ;; cut, Curry, using |_| as placeholder, without depending on the cut
@@ -25,3 +29,14 @@
 	       (or maybe-p v))
 	     args
 	     maybe-p-s))))
+
+(define-macro* (C1 fn . args)
+  (with-gensym v
+               `(lambda (,v)
+                  (,fn ,@args ,v))))
+
+;; (define-macro* (C2 fn . args)
+;;   (with-gensyms (v1 v2)
+;;                 `(lambda (,v1)
+;;                    (lambda (,v2)
+;;                      (,fn ,@args ,v1 ,v2)))))
