24f84a87c5d168a85378fc2cf7dc8f357315e634
Christian Jaeger <ch@christianjaeger.ch>
2019-08-13 18:53:43 +0100

    wbtree-test: cleanup/update

--- wbtree-test.scm
+++ wbtree-test.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2010 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2010-2019 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
@@ -10,16 +10,18 @@
 	 (cj-env IF)
 	 test
          pseudorandom
-	 (test-lib-1 %try))
+	 (test-lib-1 %try)
+         (stream list-uniq)
+         (cj-functional =>>))
+
 
 (define wbtreeparameter-string
   (wbtreeparameter string-cmp
-		      string?))
+                   string?))
 
 (define wbtreeparameter-number
   (wbtreeparameter real-cmp
-		      number? ;; ##fixnum? but well not inlined anyway anymore
-		      ))
+                   number?))
 
 (define-macro* ($s . body)
   `(let (($wbtreeparameter wbtreeparameter-string))
@@ -30,21 +32,27 @@
      (let (($wbtreeparameter wbtreeparameter-string))
        ,body)))
 
+
 (TEST
- > ($s(wbtree:members (wbtree:gt (list->wbtree '("a" "b" "f" "z" "e" "g")) "f")))
+ > (define (t op elements element)
+     ($s (wbtree->list
+          (op wbtreeparameter-string (list->wbtree elements) element))))
+ > (t wbtree:gt* '("a" "b" "f" "z" "e" "g") "f")
  ("g" "z")
- > ($s(wbtree:members (wbtree:gt (list->wbtree '("a" "b" "f" "z" "e" "g" "r" "c")) "f")))
+ > (t wbtree:gt* '("a" "b" "f" "z" "e" "g" "r" "c") "f")
  ("g" "r" "z")
- > ($s(wbtree:members (wbtree:gt (list->wbtree '("a" "b" "f" "z" "e" "g" "r" "c" "n")) "f")))
+ > (t wbtree:gt* '("a" "b" "f" "z" "e" "g" "r" "c" "n") "f")
  ("g" "n" "r" "z")
- > ($s(wbtree:members (wbtree:gt (list->wbtree '("a" "b" "h" "f" "z" "e" "g" "r" "c" "n")) "f")))
+ > (t wbtree:gt* '("a" "b" "h" "f" "z" "e" "g" "r" "c" "n") "f")
  ("g" "h" "n" "r" "z")
- > ($s(wbtree:members (wbtree:gt (list->wbtree '("a" "b" "h" "f" "z" "e" "g" "r" "c" "n" "o")) "f")))
+ > (t wbtree:gt* '("a" "b" "h" "f" "z" "e" "g" "r" "c" "n" "o") "f")
  ("g" "h" "n" "o" "r" "z")
- > ($s(wbtree:members (wbtree:lt (list->wbtree '("a" "b" "h" "f" "z" "e" "g" "r" "c" "n" "o")) "l")))
+ > (t wbtree:lt* '("a" "b" "h" "f" "z" "e" "g" "r" "c" "n" "o") "l")
  ("a" "b" "c" "e" "f" "g" "h")
- > ($s(wbtree:members (wbtree:gt (list->wbtree '("a" "b" "h" "f" "z" "e" "g" "r" "c" "n" "o")) "l")))
- ("n" "o" "r" "z")
+ > (t wbtree:gt* '("a" "b" "h" "f" "z" "e" "g" "r" "c" "n" "o") "l")
+ ("n" "o" "r" "z"))
+
+(TEST
  > ($sdefine t (wbtree:set empty-wbtree "a"))
  > ($sdefine t (wbtree:set t "b")))
 
@@ -72,21 +80,24 @@
        "b"
        4
        #((wbtree) "a" 1 empty-wbtree empty-wbtree)
-       #((wbtree) "c" 2 empty-wbtree #((wbtree) "d" 1 empty-wbtree empty-wbtree)))
+       #((wbtree) "c" 2 empty-wbtree
+         #((wbtree) "d" 1 empty-wbtree empty-wbtree)))
      > ($sdefine t (wbtree:set t "d"))
      > t
      #(wbtree
        "b"
        4
        #((wbtree) "a" 1 empty-wbtree empty-wbtree)
-       #((wbtree) "c" 2 empty-wbtree #((wbtree) "d" 1 empty-wbtree empty-wbtree)))
+       #((wbtree) "c" 2 empty-wbtree
+         #((wbtree) "d" 1 empty-wbtree empty-wbtree)))
      > ($sdefine t (wbtree:set t "e"))
      > t
      #(wbtree
        "c"
        5
        #((wbtree) "b" 2 #((wbtree) "a" 1 empty-wbtree empty-wbtree) empty-wbtree)
-       #((wbtree) "d" 2 empty-wbtree #((wbtree) "e" 1 empty-wbtree empty-wbtree))))
+       #((wbtree) "d" 2 empty-wbtree
+         #((wbtree) "e" 1 empty-wbtree empty-wbtree))))
     (TEST
      > t
      #((wbtree)
@@ -111,28 +122,28 @@
 
 
 (TEST
- > ($s(wbtree:member? t "u"))
+ > ($s (wbtree:member? t "u"))
  #f
- > ($s(wbtree:member? t "a"))
+ > ($s (wbtree:member? t "a"))
  #t
- > ($s(wbtree:member? t "b"))
+ > ($s (wbtree:member? t "b"))
  #t
- > ($s(wbtree:member? t "e"))
+ > ($s (wbtree:member? t "e"))
  #t
- > ($s(wbtree:member? t "f"))
+ > ($s (wbtree:member? t "f"))
  #f
- > ($s(wbtree:members t))
+ > ($s (wbtree->list t))
  ("a" "b" "c" "d" "e")
  > ($sdefine t (wbtree:set t "da"))
- > (equal? t ($s(wbtree:set t "da")))
+ > (equal? t ($s (wbtree:set t "da")))
  #t
- > (define e (with-exception-catcher identity (lambda () ($s(wbtree:add t "da")))))
+ > (define e (with-exception-catcher identity (lambda () ($s (wbtree:add t "da")))))
  > (wbtree-duplicate-exception? e)
  #t
  > (wbtree-duplicate-exception-old-element e)
  "da"
 
- > ($s(wbtree:members t))
+ > ($s (wbtree->list t))
  ("a" "b" "c" "d" "da" "e")
  ;; aha ye: uniq:
  > ($s (wbtreesort '("def" "abc" "a" "a" "f") #t))
@@ -141,7 +152,8 @@
  (exception
   text:
   "This object was raised: [(wbtree-duplicate-exception) \"a\" \"a\"]\n")
- > (define t-input '("def" "abc" "a" "a" "f" "n" "abe" "abba" "berta" "zwerg" "Zwerg"))
+ > (define t-input
+     '("def" "abc" "a" "a" "f" "n" "abe" "abba" "berta" "zwerg" "Zwerg"))
  > ($sdefine t (list->wbtree t-input #t))
  )
 
@@ -154,13 +166,16 @@
        #((wbtree)
 	 "abba"
 	 4
-	 #((wbtree) "a" 2 #((wbtree) "Zwerg" 1 empty-wbtree empty-wbtree) empty-wbtree)
+	 #((wbtree) "a" 2 #((wbtree) "Zwerg" 1 empty-wbtree empty-wbtree)
+           empty-wbtree)
 	 #((wbtree) "abc" 1 empty-wbtree empty-wbtree))
        #(wbtree
 	 "f"
 	 5
-	 #((wbtree) "def" 2 #((wbtree) "berta" 1 empty-wbtree empty-wbtree) empty-wbtree)
-	 #((wbtree) "n" 2 empty-wbtree #((wbtree) "zwerg" 1 empty-wbtree empty-wbtree)))))
+	 #((wbtree) "def" 2 #((wbtree) "berta" 1 empty-wbtree empty-wbtree)
+           empty-wbtree)
+	 #((wbtree) "n" 2 empty-wbtree #((wbtree) "zwerg" 1 empty-wbtree
+                                         empty-wbtree)))))
     (TEST
      > t
      #((wbtree)
@@ -178,30 +193,30 @@
 	 #((wbtree) "n" 2 empty-wbtree "zwerg")))))
 
 (TEST
- ;; > (define t-input-sorted (sort t-input string<?))  but it contains a double. so instead:
- > (define t-input-sorted '("Zwerg" "a" "abba" "abc" "abe" "berta" "def" "f" "n" "zwerg"))
- > (equal? ($s (wbtree:members t)) t-input-sorted)
+ > (define t-input-sorted
+     (=>> (sort t-input string<?)
+          ;; eliminate duplicate
+          (list-uniq string=?)))
+ > (equal? ($s (wbtree->list t)) t-input-sorted)
  #t
- > ($s(wbtree:rank t "a"))
+ > ($s (wbtree:rank t "a"))
  1
  > ($s (wbtree:maybe-ref&rank t "a"))
  ("a" . 1)
- > ($s(wbtree:index t 0))
+ > ($s (wbtree:index t 0))
  "Zwerg"
- > ($s(wbtree:index t 1))
+ > ($s (wbtree:index t 1))
  "a"
- ;; > (wbtree:rank t "b")
- ;; *** ERROR IN #<procedure #5>, "wbwbtree.scm"@436.10 -- This object was raised: not-found
- ;; 1> 
- > ($s(wbtree:max t))
+ > (%try ($s (wbtree:rank t "b")))
+ (exception text: "This object was raised: not-found\n")
+ > ($s (wbtree:max t))
  "zwerg"
- > ($s(wbtree:size t))
+ > ($s (wbtree:size t))
  10
- > ($s(wbtree:index t 9))
+ > ($s (wbtree:index t 9))
  "zwerg"
- ;; > (wbtree:index t 10)
- ;; *** ERROR IN (console)@27.1 -- This object was raised: not-found
- ;; 1> 
+ > (%try ($s (wbtree:index t 10)))
+ (exception text: "This object was raised: not-found\n")
  > ($s (map (lambda (v)
 	      (let ((p (wbtree:maybe-ref&rank t v)))
 		(let ((a (wbtree:index t (cdr p)))
@@ -224,30 +239,32 @@
   ("zwerg" . 9))
 
  ;; set operations:
- > ($s(wbtree:members (wbtree:union (list->wbtree '("a" "b" "f"))(list->wbtree '("a" "z" "e" "g")))))
+ > (define (t op elements1 elements2)
+     ($s (wbtree->list (op wbtreeparameter-string
+                           (list->wbtree elements1)
+                           (list->wbtree elements2)))))
+ > (t wbtree:union* '("a" "b" "f") '("a" "z" "e" "g"))
  ("a" "b" "e" "f" "g" "z")
- > ($s(wbtree:members (wbtree:difference (list->wbtree '("a" "b" "f"))(list->wbtree '("a" "z" "e" "g")))))
+ > (t wbtree:difference* '("a" "b" "f") '("a" "z" "e" "g"))
  ("b" "f")
- > ($s(wbtree:members (wbtree:difference (list->wbtree '("a" "b" "f"))(list->wbtree '("a" "b" "z" "e" "g")))))
+ > (t wbtree:difference* '("a" "b" "f") '("a" "b" "z" "e" "g"))
  ("f")
- > ($s(wbtree:members (wbtree:difference (list->wbtree '("a" "b" "f"))(list->wbtree '("a" "b" "z" "e" "f" "g")))))
+ > (t wbtree:difference* '("a" "b" "f") '("a" "b" "z" "e" "f" "g"))
  ()
- > ($s(wbtree:members (wbtree:difference (list->wbtree '("a" "z" "e" "g"))(list->wbtree '("a" "b" "f")))))
+ > (t wbtree:difference* '("a" "z" "e" "g") '("a" "b" "f"))
  ("e" "g" "z")
- > ($s(wbtree:members (wbtree:intersection (list->wbtree '("a" "z" "e" "g"))(list->wbtree '("a" "b" "f")))))
+ > (t wbtree:intersection* '("a" "z" "e" "g") '("a" "b" "f"))
  ("a")
- > ($s(wbtree:members (wbtree:intersection (list->wbtree '("z" "e" "g"))(list->wbtree '("a" "b" "f")))))
+ > (t wbtree:intersection* '("z" "e" "g") '("a" "b" "f"))
  ()
- > ($s(wbtree:members (wbtree:intersection (list->wbtree '("a" "z" "e" "g"))(list->wbtree '("a" "g" "n" "b" "f")))))
+ > (t wbtree:intersection* '("a" "z" "e" "g") '("a" "g" "n" "b" "f"))
  ("a" "g")
- > ($s(wbtree:members (wbtree:intersection (list->wbtree '("a" "z" "e" "f" "g"))(list->wbtree '("a" "g" "n" "b" "f")))))
+ > (t wbtree:intersection* '("a" "z" "e" "f" "g") '("a" "g" "n" "b" "f"))
  ("a" "f" "g")
- > ($s (wbtree:members (wbtree:delete (list->wbtree '("a" "z" "e" "f" "g")) "f")))
+ > ($s (wbtree->list (wbtree:delete (list->wbtree '("a" "z" "e" "f" "g")) "f")))
  ("a" "e" "g" "z")
- > ($s (wbtree:members (wbtree:delete (list->wbtree '("a" "z" "e" "f" "g")) "ff")))
- ("a" "e" "f" "g" "z")
- )
-
+ > ($s (wbtree->list (wbtree:delete (list->wbtree '("a" "z" "e" "f" "g")) "ff")))
+ ("a" "e" "f" "g" "z"))
 
 
 (define wbtreeparameter-pair-string
@@ -255,50 +272,54 @@
 		      pair?))
 
 
-(define-macro* ($ . body)
+(define-macro* ($ps . body)
   `(let (($wbtreeparameter wbtreeparameter-pair-string))
      ,@body))
 
-(define-macro* ($define var body)
+(define-macro* ($psdefine var body)
   `(define ,var
      (let (($wbtreeparameter wbtreeparameter-pair-string))
        ,body)))
 
 (TEST
- > ($define t2 (list->wbtree '(("a". 1) ("z". 2) ("e". 3) ("f". 4) ("g". 5))))
- > t2
+ > ($psdefine
+    tp (list->wbtree '(("a" . 1) ("z" . 2) ("e" . 3) ("f" . 4) ("g" . 5))))
+ > tp
  #((wbtree)
    ("f" . 4)
    5
    #((wbtree) ("e" . 3) 2 ("a" . 1) empty-wbtree)
    #((wbtree) ("g" . 5) 2 empty-wbtree ("z" . 2)))
- > ($ (wbtree:member? t2 '("a" . x)))
+ > ($ps (wbtree:member? tp '("a" . x)))
  #t
- > ($ (wbtree:member? t2 '("aa" . x)))
+ > ($ps (wbtree:member? tp '("aa" . x)))
  #f
- > ($ (wbtree:maybe-ref t2 '("a" . x)))
+ > ($ps (wbtree:maybe-ref tp '("a" . x)))
  ("a" . 1)
- > ($ (wbtree:maybe-ref t2 '("aa" . x)))
+ > ($ps (wbtree:maybe-ref tp '("aa" . x)))
  #f
 
- > ($ (wbtree:between t2 '("aa") '("x")))
+ > ($ps (wbtree:between tp '("aa") '("x")))
  #((wbtree) ("f" . 4) 3 ("e" . 3) ("g" . 5))
- > ($ (wbtree:between t2 '("a") '("x")))
+ > ($ps (wbtree:between tp '("a") '("x")))
  #((wbtree) ("f" . 4) 3 ("e" . 3) ("g" . 5))
- > ($ (wbtree:between t2 '("") '("x")))
+ > ($ps (wbtree:between tp '("") '("x")))
  #((wbtree) ("f" . 4) 4 #((wbtree) ("a" . 1) 2 empty-wbtree ("e" . 3)) ("g" . 5))
- > ($ (wbtree:between t2 '("a") '("g")))
+ > ($ps (wbtree:between tp '("a") '("g")))
  #((wbtree) ("f" . 4) 2 ("e" . 3) empty-wbtree)
- > ($ (wbtree:members (wbtree:between t2 '("a") '("g"))))
+
+ > ($ps (wbtree->list (wbtree:between tp '("a") '("g"))))
  (("e" . 3) ("f" . 4))
- > ($ (wbtree:members (wbtree:between t2 '("") '("g"))))
+ > ($ps (wbtree->list (wbtree:between tp '("") '("g"))))
  (("a" . 1) ("e" . 3) ("f" . 4))
- > ($ (wbtree:members (wbtree:between t2 '("") '("zz"))))
+ > ($ps (wbtree->list (wbtree:between tp '("") '("zz"))))
  (("a" . 1) ("e" . 3) ("f" . 4) ("g" . 5) ("z" . 2))
- > ($ (wbtree:members (wbtree:between t2 '("") '("z"))))
- (("a" . 1) ("e" . 3) ("f" . 4) ("g" . 5))
- 
- )
+ > ($ps (wbtree->list (wbtree:between tp '("") '("z"))))
+ (("a" . 1) ("e" . 3) ("f" . 4) ("g" . 5)))
+
+
+;; ==================================================================
+;; Randomized/property testing
 
 (TEST
  > (define *stringstream1 (box (pseudorandomsource*->a-z-string-stream
@@ -316,18 +337,21 @@
  > ($sdefine t1 (prandomwbtree *stringstream1 10000))
  > ($sdefine t2 (prandomwbtree *stringstream1 10000))
  > ($sdefine t3 (prandomwbtree *stringstream1 10000))
- ;; and one with different (more spaced when watched from the above perspective?) keys:
- ;; Use less elements to initialize it to attain about the same size (fewer duplicates)
+ ;; and one with different (more spaced when watched from the above
+ ;; perspective?) keys: Use fewer elements to initialize it to attain
+ ;; about the same size (fewer duplicates)
  > ($sdefine t4 (prandomwbtree *stringstream2 5100))
 
  > ($s (equal? (F (wbtrees:intersection-stream (list t1 t2 t3)))
-	       (wbtree:members (RA wbtree:intersection t1 t2 t3))))
+	       (wbtree->list (RA wbtree:intersection t1 t2 t3))))
  #t
  > ($s (equal? (F (wbtrees:intersection-stream (list t1 t2 t4)))
-	       (wbtree:members (RA wbtree:intersection t1 t2 t4))))
+	       (wbtree->list (RA wbtree:intersection t1 t2 t4))))
  #t
- > (define* (wbtree3:intersection-list t1 t2 t3) (wbtree:members (RA wbtree:intersection t1 t2 t3)))
- > ($s (equal?* (wbtree3:intersection-list t1 t2 t3) (wbtree3:intersection-list t2 t1 t3) (wbtree3:intersection-list t3 t2 t1)))
- #t
- )
+ > (define* (wbtree3:intersection-list t1 t2 t3)
+     (wbtree->list (RA wbtree:intersection t1 t2 t3)))
+ > ($s (equal?* (wbtree3:intersection-list t1 t2 t3)
+                (wbtree3:intersection-list t2 t1 t3)
+                (wbtree3:intersection-list t3 t2 t1)))
+ #t)
 
