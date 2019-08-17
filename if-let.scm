7eabc552528adfc1d121821c2fb7f276291d0318
Christian Jaeger <ch@christianjaeger.ch>
2019-08-17 18:05:43 +0100

    if-let: tab-expand

--- if-let.scm
+++ if-let.scm
@@ -7,15 +7,15 @@
 
 
 (require easy
-	 test)
+         test)
 
 (export (macro iflet)
-	(macro if-let*)
-	(macro if-let)
-	(macro and-let)
-	#!optional
-	if-let*-expand
-	if-let-expand)
+        (macro if-let*)
+        (macro if-let)
+        (macro and-let)
+        #!optional
+        if-let*-expand
+        if-let-expand)
 
 
 ;; There is an if-let in Clojure: https://clojuredocs.org/clojure.core/if-let
@@ -34,10 +34,10 @@
 
 (defmacro (iflet bind+test yes #!optional no)
   (mcase bind+test
-	 (`(`var `test)
-	  (assert* symbol? var)
-	  `(cond (,test => (lambda (,var) ,yes))
-		 (else ,no)))))
+         (`(`var `test)
+          (assert* symbol? var)
+          `(cond (,test => (lambda (,var) ,yes))
+                 (else ,no)))))
 
 (TEST
  > (iflet (a 2) 3)
@@ -51,17 +51,17 @@
 
 
 (def (if-let*-expand COND [(source-of list?) assignments] yes no)
-  (with-gensym
-   NO
-   `(let ((,NO (lambda () ,no)))
-      ,(fold-right (lambda (assignment yes)
-		     (mcase assignment
-			    (`(`var `test-expr)
-			     (assert* symbol? var)
-			     `(,COND (,test-expr => (lambda (,var) ,yes))
-				    (else (,NO))))))
-		   yes
-		   (source-code assignments)))))
+     (with-gensym
+      NO
+      `(let ((,NO (lambda () ,no)))
+         ,(fold-right (lambda (assignment yes)
+                        (mcase assignment
+                               (`(`var `test-expr)
+                                (assert* symbol? var)
+                                `(,COND (,test-expr => (lambda (,var) ,yes))
+                                        (else (,NO))))))
+                      yes
+                      (source-code assignments)))))
 
 (defmacro (if-let* assignments yes #!optional no)
   (if-let*-expand `cond assignments yes no))
@@ -77,17 +77,17 @@
  > (if-let* ((a #f)) a 4)
  4
  > (if-let* ((a 2)
-	     (b 3))
-	    (list a b)
-	    4)
+             (b 3))
+            (list a b)
+            4)
  (2 3)
  > (def (f x)
-	(if-let* ((a (and x (* x x)))
-		  (b (dec a))
-		  (c (< b 10))
-		  (d (list a b)))
-		 d
-		 'no))
+        (if-let* ((a (and x (* x x)))
+                  (b (dec a))
+                  (c (< b 10))
+                  (d (list a b)))
+                 d
+                 'no))
  > (f #f)
  no
  > (f 10)
@@ -95,9 +95,9 @@
  > (f 2)
  (4 3)
  > (%try (if-let* ((GEN:-11015 #f)
-		   (b 3))
-		  (list a b)
-		  GEN:-11015))
+                   (b 3))
+                  (list a b)
+                  GEN:-11015))
  (exception text: "Unbound variable: GEN:-11015\n"))
 
 
@@ -111,7 +111,7 @@
 (def (if-let-expand COND [(source-of list?) assignments] yes no)
      (let* ((assignments*
              (source-code assignments))
-	    (assignments**
+            (assignments**
              (map (lambda (assignment)
                     (mcase assignment
                            (`(`var `test-expr)
@@ -120,18 +120,18 @@
                                        (values var test-expr (gensym var*)))))))
                   assignments*)))
        (with-gensym
-	NO
-	`(let ((,NO (lambda () ,no)))
-	   ,(fold-right (lambda-values
-			 ((var test-expr tmpvar) yes)
-			 `(,COND (,test-expr => (lambda (,tmpvar) ,yes))
+        NO
+        `(let ((,NO (lambda () ,no)))
+           ,(fold-right (lambda-values
+                         ((var test-expr tmpvar) yes)
+                         `(,COND (,test-expr => (lambda (,tmpvar) ,yes))
                                  (else (,NO))))
-			`(let ,(map (lambda-values
-				     ((var test-expr tmpvar))
-				     `(,var ,tmpvar))
-				    assignments**)
-			   ,yes)
-			assignments**)))))
+                        `(let ,(map (lambda-values
+                                     ((var test-expr tmpvar))
+                                     `(,var ,tmpvar))
+                                    assignments**)
+                           ,yes)
+                        assignments**)))))
 
 (defmacro (if-let assignments yes #!optional no)
   ;; if no is #f, just pass it on as code, and it will result in #f, too ":)"
@@ -149,38 +149,38 @@
  > (if-let ((a #f)) a)
  #f
  > (if-let ((a 2)
-	    (b 3))
-	   (list a b)
-	   4)
+            (b 3))
+           (list a b)
+           4)
  (2 3)
  > (if-let ((a 2)
-	    (b #f))
-	   (list a b)
-	   5)
+            (b #f))
+           (list a b)
+           5)
  5
  > (if-let ((a #f)
-	    (b 3))
-	   (list a b)
-	   5)
+            (b 3))
+           (list a b)
+           5)
  5
 
  > (%try (let ((x 10))
-	   (if-let ((GEN:a-10740 (and x (* x x)))
-		    (GEN:b-10741 (dec GEN:a-10740))
-		    (GEN:c-10742 (< GEN:b-10741 10))
-		    (d (list GEN:a-10740 GEN:b-10741)))
-		   d
-		   'no)))
+           (if-let ((GEN:a-10740 (and x (* x x)))
+                    (GEN:b-10741 (dec GEN:a-10740))
+                    (GEN:c-10742 (< GEN:b-10741 10))
+                    (d (list GEN:a-10740 GEN:b-10741)))
+                   d
+                   'no)))
  (exception text: "Unbound variable: GEN:a-10740\n")
  > (def (f x)
-	(let ((a 100)
-	      (b x))
-	  (if-let ((a (and x (* x x)))
-		   (b (dec a))
-		   (c (< b 10))
-		   (d (list a b)))
-		  d
-		  'no)))
+        (let ((a 100)
+              (b x))
+          (if-let ((a (and x (* x x)))
+                   (b (dec a))
+                   (c (< b 10))
+                   (d (list a b)))
+                  d
+                  'no)))
  > (f #f)
  no
  > (f 10)
@@ -188,9 +188,9 @@
  > (f 2)
  (100 2)
  > (%try (if-let ((GEN:-11015 #f)
-		  (b 3))
-		 (list a b)
-		 GEN:-11015))
+                  (b 3))
+                 (list a b)
+                 GEN:-11015))
  (exception text: "Unbound variable: GEN:-11015\n"))
 
 
