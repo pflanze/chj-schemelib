339e0bc460371be8d73475c4d0b59fc3babab4b0
Christian Jaeger <ch@christianjaeger.ch>
2019-08-30 04:17:02 +0100

    Update copyright year

--- define-macro-star.scm
+++ define-macro-star.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2010, 2011 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2010-2019 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
