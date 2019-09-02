deb9d96d58a33bab3db6008ba444f21fd1972545
Christian Jaeger <ch@christianjaeger.ch>
2019-09-02 03:25:14 +0100

    dot-oo-introspection: change copyright year to when this code was started

--- dot-oo-introspection.scm
+++ dot-oo-introspection.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2013-2019 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2017-2019 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
