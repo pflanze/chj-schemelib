2062da369f2451293db737c89805ac9e00ccd71b
Christian Jaeger <ch@christianjaeger.ch>
2019-09-07 18:58:56 +0100

    scheme-meta: fix uvector? and svector?: add 64 bit variants

--- scheme-meta.scm
+++ scheme-meta.scm
@@ -107,12 +107,14 @@
 (define (uvector? v)
   (or (u8vector? v)
       (u16vector? v)
-      (u32vector? v)))
+      (u32vector? v)
+      (u64vector? v)))
 
 (define (svector? v)
   (or (s8vector? v)
       (s16vector? v)
-      (s32vector? v)))
+      (s32vector? v)
+      (s64vector? v)))
 
 (define (fvector? v)
   (or (f32vector? v)
