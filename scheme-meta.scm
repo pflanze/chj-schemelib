1aeb7a6f9039ff488a895694fc229188db5d83db
Christian Jaeger <ch@christianjaeger.ch>
2019-09-07 19:16:24 +0100

    scheme-meta: make homogenous-vector? accessible via include

--- scheme-meta.scm
+++ scheme-meta.scm
@@ -106,27 +106,7 @@
       (list 'quote v)))
 
 
-
-(define (uvector? v)
-  (or (u8vector? v)
-      (u16vector? v)
-      (u32vector? v)
-      (u64vector? v)))
-
-(define (svector? v)
-  (or (s8vector? v)
-      (s16vector? v)
-      (s32vector? v)
-      (s64vector? v)))
-
-(define (fvector? v)
-  (or (f32vector? v)
-      (f64vector? v)))
-
-(define (homogenous-vector? v)
-  (or (uvector? v)
-      (svector? v)
-      (fvector? v)))
+(include "scheme-meta-homogenous-vector--include.scm")
 
 ;; XX bit vectors ?
 
