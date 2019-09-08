cddb09a0a92182f119db2e9e9a05c778edd7c640
Christian Jaeger <ch@christianjaeger.ch>
2019-09-08 09:28:12 +0100

    vector-binsearch: take optional report-gap? argument

--- vector-binsearch.scm
+++ vector-binsearch.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2018 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2018-2019 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
@@ -9,6 +9,7 @@
 (require easy
 	 cj-cmp
 	 range
+         warn-plus
 	 test
 	 test-random)
 
@@ -23,38 +24,98 @@
 (include "cj-standarddeclares.scm")
 
 
-(def (@vector-binsearch/start+end v val cmp istart iend)
-     (let lp ((istart istart)
-	      (iend iend))
+(def (@vector-binsearch/start+end v val cmp orig-istart orig-iend
+                                  report-gap?)
+     (let lp ((istart orig-istart)
+	      (iend orig-iend))
        (let ((d (fx- iend istart)))
-	 (and (fxpositive? d)
-	      (let ((i (fx+ istart (arithmetic-shift d -1))))
-		(match-cmp (cmp val (vector-ref v i))
-			   ((lt) (lp istart i))
-			   ((eq) i)
-			   ((gt) (lp (inc i) iend))))))))
+	 (if (fxpositive? d)
+             (let ((i (fx+ istart (arithmetic-shift d -1))))
+               (match-cmp (cmp val (vector-ref v i))
+                          ((lt) (lp istart i))
+                          ((eq) i)
+                          ((gt) (lp (inc i) iend))))
+             (and report-gap?
+                  (fx< iend orig-iend)
+                  (let ((yend (vector-ref v iend))
+                        (maybe-cons (lambda (i0 i1)
+                                      (and (<= orig-istart i0)
+                                           (<= i1 orig-iend)
+                                           (cons i0 i1)))))
+                    (match-cmp (cmp val yend)
+                               ((lt) (maybe-cons (dec iend) iend))
+                               ((gt)
+                                (WARN-ONCE "case gt never happens?")
+                                (maybe-cons iend (inc iend)))
+                               ((eq) (error "bug")))))))))
 
 
 (def (vector-binsearch/start+end [vector? v] ;; must be sorted using cmp
 				 val
 				 [function? cmp]
 				 [fixnum-natural0? istart]
-				 [fixnum-natural0? iend])
-     -> (maybe fixnum-natural0?)
+				 [fixnum-natural0? iend]
+                                 #!optional
+                                 [boolean? report-gap?])
+     -> (maybe (either fixnum-natural0?
+                       ;; if report-gap? is true:
+                       pair?))
 
      (let ((len (vector-length v)))
        (assert (<= istart len))
        (assert (<= iend len))
        (assert (<= istart iend)))
-     (@vector-binsearch/start+end v val cmp istart iend))
+     (@vector-binsearch/start+end v val cmp istart iend
+                                  report-gap?))
 
 
 (def (vector-binsearch [vector? v] ;; must be sorted using cmp
 		       val
-		       [function? cmp])
-     -> (maybe fixnum-natural0?)
+		       [function? cmp]
+                       #!optional
+                       [boolean? report-gap?])
+     -> (maybe (either fixnum-natural0?
+                       ;; if report-gap? is true:
+                       pair?))
 
-     (@vector-binsearch/start+end v val cmp 0 (vector-length v)))
+     (@vector-binsearch/start+end v val cmp 0 (vector-length v)
+                                  report-gap?))
+
+
+(TEST
+ > (vector-binsearch (vector 122 124) 122 real-cmp)
+ 0
+ > (vector-binsearch (vector 122 124) 124 real-cmp)
+ 1
+ > (vector-binsearch (vector 122 124) 123 real-cmp)
+ #f
+ > (vector-binsearch (vector 122 124) 123 real-cmp #f)
+ #f
+ > (vector-binsearch (vector 122 124) 123 real-cmp #t)
+ (0 . 1)
+ > (def t (C vector-binsearch (vector 10 122 124 140) _ real-cmp #t))
+ > (t 123)
+ (1 . 2)
+ > (t 9)
+ #f
+ > (t 10)
+ 0
+ > (t 11)
+ (0 . 1)
+ > (t 121)
+ (0 . 1)
+ > (t 122)
+ 1
+ > (t 123)
+ (1 . 2)
+ > (t 124)
+ 2
+ > (t 139)
+ (2 . 3)
+ > (t 140)
+ 3
+ > (t 141)
+ #f)
 
 
 
@@ -97,9 +158,10 @@
 ;; values within the same min max boundaries and those outside.
 
 (TEST
- ;; with no values, any value is outside it
+ ;; with no values, any value is outside of it
  > (vector-binsearch (vector) 123 real-cmp)
  #f
+
  > (def (t nvals)
 	(let* ((range (inc (random-integer 1000)))
 	       (shift (- (random-integer 1300) 800))
