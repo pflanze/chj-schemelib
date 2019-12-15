bb994a6f1096cd3cb01b193ac9d56ed6ce20c1d8
Christian Jaeger <ch@christianjaeger.ch>
2019-12-15 08:13:22 +0000

    monad/syntax,maybe: new approach
    - use easy (for easy nice syntax)
    
    - keep |use-monad| and |in-monad|, also they continue to have to
      unhygienically set all the monad operators, as using a single one
      would require compile time lexical communication which the current
      macro system doesn't support
    
    - but use dot instead of : as prefix separator, for "automatic dot-oo
      compatibility"
    
    - change |>>| to alias as a macro, not function, to allow the
      implementation to implement it as a macro, too. Unsolved: OO
      dispatch won't work for |.>>| now, unless adding a variant of
      def. to allow to define it in the presense of macros, but even then,
      it would be bypassing the macro part of the operation in the generic
      case. Unsure if it matters, though, let's see? (Neat: macro approach
      naturally inlines/optimizes of course; well... duality understanding
      (with alternative of being compiler's work) still pending.)
    
    - mlet can now do both single and multi binding like let, thus
      eliminate letm
    
    - add mdo-in and mlet-in for sugar for implicit in-monad
    
    - operators set by use-monad / in-monad have no "m" or whatever
      prefix; lift, let, do get "m" prefix consistently now instead of
      "m:"
    
    - use the later introduced right-associate to simplify the code
    
    - make maybe.scm depend on monad/syntax, hey, fair, it implements a
      monad. Only for testing right now though, perhaps feasible to keep
      detangled? (But see later commits.)
    
    Open: adapt the other files in monad/.

--- maybe.scm
+++ maybe.scm
@@ -8,10 +8,12 @@
 
 (require easy ;; dot-oo would be enough, tough
          (predicates-1 any?)
-         test)
+         test
+         monad/syntax)
 
 (export maybe?
-        (method maybe.>>=))
+        (methods maybe.>>= maybe.return)
+        (macro  maybe.>>))
 
 
 (def maybe? any?) ;; oh my
@@ -23,10 +25,28 @@
 ;; >> requires a thunk [or promise]. Don't define that just yet?
 ;; Well, go ahead:
 
-(def. (maybe.>> a b)
-  ;; or should it force a and leave b alone?
-  (and a
-       (force b)))
+;; (def. (maybe.>> a b)
+;;   ;; or should it force a and leave b alone?
+;;   (and a
+;;        (force b)))
+
+;; But the one actually chosen by the macrology in monad/syntax:
+
+(defmacro (maybe.>> a b)
+  `(and ,a ,b))
+
+;; This will mean that higher-order(?, generic) monadic functions like
+;; mfor-each will not work for maybe! Any solution or just get on with
+;; it? That's not a typing issue (even), but one of eagerness. Hmm,
+;; for indirect monads it's not a problem so could make an indirect
+;; one for Maybe or even maybe *?* (Or just accept that mdo is more
+;; like a function, not really threading of execution model. It's
+;; threading of data not order of evaluation. Hm?)
+
+
+
+(def. (maybe.return v)
+  v)
 
 
 (TEST
@@ -34,4 +54,35 @@
  3
  > (.>>= #f inc*)
  #f)
+
+
+(TEST
+ > (def actions '())
+ > (def (t msg val)
+        (push! actions msg)
+        val)
+ > (in-monad maybe (mdo (t 'a 2)
+                        (t 'b 3)
+                        (t 'c 4)))
+ 4
+ > actions
+ (c b a)
+ > (in-monad maybe (mdo (t 'd 2)
+                        (t 'e #f)
+                        (t 'f 4)))
+ #f
+ > actions
+ (e d c b a)
+ > (in-monad maybe (mlet (x (t 'g 2)) x))
+ 2
+ > (in-monad maybe (mlet ((x (t 'h #f))
+                          (y (t 'i 3)))
+                         x))
+ #f
+ > actions
+ (h g e d c b a)
+
+ > (expansion mdo-in maybe a b c)
+ (letrec ((>>= maybe.>>=) (return maybe.return)) (and a b c)))
+
 
--- monad/syntax.scm
+++ monad/syntax.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2010, 2011 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2010-2019 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
@@ -6,91 +6,56 @@
 ;;;    (at your option) any later version.
 
 
-(require define-macro-star
-	 cj-env)
+(require easy)
 
 ;;;
 ;;;; Infrastructure to write monads
 ;;;
 
-;; general, but depending on the following in the user's scope: >> >>=
 
-(define (m:begin-expand-for maybe-monadprefix)
-  (define >> (if maybe-monadprefix
-		 (symbol-append maybe-monadprefix ":>>")
-		 '>>))
-  (named self
-	 (lambda exprs
-	   (match-list*
-	    exprs
-	    ((e1 e2 . exprs*)
-	     ;; give better location reporting; since >> will only
-	     ;; check the type of its first argument, not the second,
-	     ;; the error was probably because of it, hence mark the
-	     ;; application location with it:
-	     (sourcify
-	      `(,>> ,e1 ,(apply self e2 exprs*))
-	      e1))
-	    ((e1)
-	     e1)))))
+;; This variable, if bound to a non-#f value, determines the monad
+;; "type", rather the prefix for .>>, .>>=, ... to use. If #f, the
+;; generic .>>, .>>= etc. are used.
 
-(define-macro* (m:begin . exprs)
-  (apply (m:begin-expand-for #f) exprs))
+;;(def $M #f)
+;;XXX bah   no?  would  have  to  wll    eval  access  not lexical,  just not ofcrs.f
+;;w bout a macro b  expand it in  macro? no ahve access.  CANT  DO IT !!!!!!!!!!!
+;; SICK macro sys FCK_X
 
-(define-macro* (letm bind expr)
-  (match-list*
-   bind
-   ((var1 expr1)
-    `(>>= ,expr1
-	  (lambda (,var1) ,expr)))))
-
-;; (define (m:lift f . args)
-;;   ..)
-
-;; can't use a function because the >>= >> or m:let needs to be take
-;; from the context syntactically.
-
-(define-macro* (m:lift f . args)
-  (let ((vs (map (lambda_ (gensym)) args))) ;; XX do I have sth or not for this?
-    `(m:let ,(map list vs args)
-	    (return ,f ,@vs))))
-
-;; totally general
-
-(define-macro* (m:let binds expr)
-  (match-list*
-   binds
-   ((bind . binds*)
-    `(letm ,bind
-	   (m:let ,binds*
-		  ,expr)))
-   (()
-    expr)))
-
-
-;; could write variants taking the kind of monad as parameter; or, if
-;; mixing of monads can always be controlled with the following, that
-;; seems better:
 
 (define-macro* (use-monad monadname)
   (assert* symbol? monadname
 	   (lambda (monadname*)
 	     `(begin
-		(define >> ,(symbol-append monadname* ":>>"))
-		(define >>= ,(symbol-append monadname* ":>>="))
-		(define return ,(symbol-append monadname* ":return"))
-		;; and a proxy macro..
-		(##define-syntax
-		 m:inline! ;; with m: prefix?
+
+                ;; OK to keep those as functions?
+		(define >>= ,(symbol-append monadname* ".>>="))
+		(define return ,(symbol-append monadname* ".return"))
+
+		;; (define >> ,(symbol-append monadname* ".>>"))
+                ;; Because monadname.>> may need to be a macro, use a
+                ;; macro here as well, okay? HACK?
+                (##define-syntax
+		 >>
 		 (lambda (stx)
 		   (##sourcify-deep
 		    (apply
 		     (lambda (_name . args)
-		       (cons ',(symbol-append monadname* ":inline!") args))
+		       (cons ',(symbol-append monadname* ".>>") args))
+		     (##source-code stx))
+		    stx)))
+
+                (##define-syntax
+		 minline! ;; with m: prefix?
+		 (lambda (stx)
+		   (##sourcify-deep
+		    (apply
+		     (lambda (_name . args)
+		       (cons ',(symbol-append monadname* ".inline!") args))
 		     (##source-code stx))
 		    stx)))))))
 
-(define-macro* (in-monad monadname . body)
+(defmacro (in-monad monadname . body)
   (assert* symbol? monadname
 	   (lambda (monadname*)
 	     `(let ()
@@ -100,4 +65,73 @@
 		  ;; about further defines after the ##define-syntax
 		  ;; from use-monad?
 		  ,@body)))))
+
+
+;; General (except the -in variants), but depending on the above in
+;; their scope or using the slower generics:
+
+(def (mdo-expand stx exprs)
+     (right-associate (lambda (e r)
+                        (sourcify
+                         `(>> ,e ,r)
+                         e))
+                      exprs
+                      (lambda (errmsg)
+                        (source-error stx errmsg))))
+
+(defmacro (mdo . exprs)
+  (mdo-expand stx exprs))
+
+(defmacro (mdo-in monadname . exprs)
+  `(in-monad ,monadname
+             ,(mdo-expand stx exprs)))
+
+(TEST
+ > (expansion#mdo a b c)
+ (>> a (>> b c)))
+
+
+
+(def (mlet-expand stx bindS expr)
+     (def (expand1 bind expr)
+          (mcase bind
+                 (`(`var1 `expr1)
+                  `(>>= ,expr1
+                        (lambda (,var1) ,expr)))))
+     (assert* pair? bindS
+              (lambda (bindS*)
+                (if (pair? (source-code (car bindS*)))
+                    ;; normal multiple let
+                    (fold-right expand1
+                                expr
+                                bindS*)
+                    ;; single "beautiful" let
+                    (expand1 bindS expr)))))
+
+(defmacro (mlet bindS expr)
+  (mlet-expand stx bindS expr))
+
+(defmacro (mlet-in monadname bindS expr)
+  `(in-monad ,monadname
+             ,(mlet-expand stx bindS expr)))
+
+(TEST
+ > (expansion#mlet ((x 2) (y 3)) x)
+ (>>= 2 (lambda (x) (>>= 3 (lambda (y) x))))
+ > (expansion#mlet ((x 2)) x)
+ (>>= 2 (lambda (x) x))
+ > (expansion#mlet (x 2) x)
+ (>>= 2 (lambda (x) x)))
+
+
+
+;; mlift: can't use a function because the >>= >> or mlet needs to be
+;; taken from the context syntactically.
+
+(defmacro (mlift f . args)
+  (let ((vs (map (lambda_ (gensym)) args))) ;; XX do I have sth or not for this?
+    `(mlet ,(map list vs args)
+           (return ,f ,@vs))))
+
+;; XXX: mlift or lift? Is this not one of the methods like return ?
 
