99b1a975db2468bebf90858f77e509bc35b90544
Christian Jaeger <ch@christianjaeger.ch>
2020-01-20 12:59:52 +0000

    more-io-util: tab-expand

--- more-io-util.scm
+++ more-io-util.scm
@@ -9,32 +9,32 @@
 ;; Newer, better, higher-level IO routines (than the cj-io-util ~mess)
 
 (require easy
-	 (cj-env-2 future)
-	 (cj-io-util open-process*)
-	 (cj-path path-string?)
-	 (Result Ok Error)
-	 test)
+         (cj-env-2 future)
+         (cj-io-util open-process*)
+         (cj-path path-string?)
+         (Result Ok Error)
+         test)
 
 (export (jclass command)
-	process-run
-	string-writer
-	string-reader
-	file-contents
-	
-	#!optional
-	process-spec?)
+        process-run
+        string-writer
+        string-reader
+        file-contents
+        
+        #!optional
+        process-spec?)
 
 
 (def (process-spec? v)
      (or (null? v)
-	 (and (pair? v)
-	      (let-pair ((a v*) v)
-			(and (keyword? a)
-			     (pair? v*)
-			     (let-pair ((b v**) v*)
-				       (and (or (symbol? b)
-						(string? b))
-					    (process-spec? v**))))))))
+         (and (pair? v)
+              (let-pair ((a v*) v)
+                        (and (keyword? a)
+                             (pair? v*)
+                             (let-pair ((b v**) v*)
+                                       (and (or (symbol? b)
+                                                (string? b))
+                                            (process-spec? v**))))))))
 
 (TEST
  > (process-spec? '())
@@ -52,17 +52,17 @@
 
 
 (jclass (command [path-string? path]
-		 #!key
-		 ([process-spec? additional-spec] '())
-		 #!rest
-		 [(list-of string?) arguments])
+                 #!key
+                 ([process-spec? additional-spec] '())
+                 #!rest
+                 [(list-of string?) arguments])
 
-	(def-method (process-spec c . args)
-	  `(path: ,(.path c)
-		  arguments: ,(.arguments c)
-		  ;; XX do the following two need merging?
-		  ,@(.additional-spec c)
-		  ,@args)))
+        (def-method (process-spec c . args)
+          `(path: ,(.path c)
+                  arguments: ,(.arguments c)
+                  ;; XX do the following two need merging?
+                  ,@(.additional-spec c)
+                  ,@args)))
 
 (TEST
  > (command "a" additional-spec: '(foo: "a") "b")
@@ -73,16 +73,16 @@
 
 (def (process-run cmd reader writer ok?)
      (let* ((p (open-process* (.process-spec cmd
-					     stdin-redirection: #t
-					     stdout-redirection: #t)))
-	    (f (future (writer p)))
-	    (res (reader p)))
+                                             stdin-redirection: #t
+                                             stdout-redirection: #t)))
+            (f (future (writer p)))
+            (res (reader p)))
        (close-input-port p)
        (thread-join! f) ;; should exceptions be suppressed here?
        (let ((s (process-status p)))
-	 (if (ok? s)
-	     (Ok res)
-	     (Error s)))))
+         (if (ok? s)
+             (Ok res)
+             (Error s)))))
 
 (def (string-writer #(string? s))
      (lambda (p)
@@ -95,14 +95,14 @@
 
 (TEST
  > (process-run (command "tr" "a" "b")
-		string-reader
-		(string-writer "Hallo")
-		zero?)
+                string-reader
+                (string-writer "Hallo")
+                zero?)
  #((Ok) "Hbllo")
  > (process-run (command "tr" "a" "b")
-		string-reader
-		(string-writer "Hallo")
-		(complement zero?))
+                string-reader
+                (string-writer "Hallo")
+                (complement zero?))
  #((Error) 0)
  ;; BTW status codes should have their own type, too, right. Then we
  ;; would see it perfectly well-informed here, too !
