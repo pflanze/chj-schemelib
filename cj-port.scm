334c130e55af04e5d664621a0ce14df8acf25745
Christian Jaeger <ch@christianjaeger.ch>
2020-01-25 04:23:10 +0000

    cj-port: add |port-set-unescaped!|, use in with-*-to-string, test

--- cj-port.scm
+++ cj-port.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2010-2019 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2010-2020 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
@@ -9,9 +9,11 @@
 (require define-macro-star
          srfi-11
          cj-typed
+         (cj-env when)
 	 test)
 
-(export with-output-to-string
+(export port-set-unescaped!
+        with-output-to-string
 	with-error-to-string
 	(macro %with-output-to-string)
 	(macro %with-error-to-string)
@@ -19,22 +21,31 @@
         port-name)
 
 
-(define (make-with-_-to-string current-_-port)
+(define (port-set-unescaped! port)
+  (output-port-readtable-set!
+   port                                                 
+   (readtable-max-unescaped-char-set (output-port-readtable port)
+                                     #\U0010ffff)))
+
+
+(define (make-with-_-to-string current-_-port unescaped?)
   (lambda (thunk)
     (let* ((res 'make-with-_-to-string-unbound)
            (str (call-with-output-string
                  ""
                  (lambda (port)
+                   (when unescaped?
+                     (port-set-unescaped! port))
                    (set! res
                          (parameterize ((current-_-port port))
                                        (thunk)))))))
       (values str res))))
 
 (define with-output-to-string
-  (make-with-_-to-string current-output-port))
+  (make-with-_-to-string current-output-port #t))
 
 (define with-error-to-string
-  (make-with-_-to-string current-error-port))
+  (make-with-_-to-string current-error-port #t))
 
 
 (define-macro* (%with-output-to-string expr)
@@ -54,6 +65,11 @@
 (define (pretty-print-to-string v)
   (with-output-to-string (lambda ()
 			   (pretty-print v))))
+
+
+(TEST
+ > (fst (pretty-print-to-string (string->symbol "vorwärts")))
+ "vorwärts\n")
 
 
 ;; should that be in gambit.scm or something?
