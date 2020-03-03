6f361b2f723c740018619133d0abfde75ff86263
Christian Jaeger <ch@christianjaeger.ch>
2020-03-03 20:00:55 +0000

    cj-source: tab-expand

--- cj-source.scm
+++ cj-source.scm
@@ -16,43 +16,43 @@
 
 
 '(export source?
-	 source-code
-	 source-location
+         source-code
+         source-location
          cj-source:port-name?
-	 location?
-	 location-container
-	 container->path
-	 location-position
-	 position?
-	 position-line
-	 position-column
-	 position-string
-	 maybe-position-string
-	 make-position
-	 make-location
-	 make-source
-	 sourcify
-	 cj-sourcify-deep
-	 possibly-sourcify
-	 cj-possibly-sourcify-deep
-	 cj-desourcify
-	 read-all-source
-	 (type source-error)
-	 location-string
-	 show-location-location
-	 show-source-location
-	 source-warn
-	 location-warn
-	 location-warn-to-string
-	 location-warn*
-	 location-warn-to-string*
-	 location-warn-to-string/normalize
-	 show-source-error
-	 source-error->string
-	 show-procedure-location
-	 source-quote ;; deprecated, use source-quote* instead?
-	 source-dequote ;; ditto
-	 source-quote*)
+         location?
+         location-container
+         container->path
+         location-position
+         position?
+         position-line
+         position-column
+         position-string
+         maybe-position-string
+         make-position
+         make-location
+         make-source
+         sourcify
+         cj-sourcify-deep
+         possibly-sourcify
+         cj-possibly-sourcify-deep
+         cj-desourcify
+         read-all-source
+         (type source-error)
+         location-string
+         show-location-location
+         show-source-location
+         source-warn
+         location-warn
+         location-warn-to-string
+         location-warn*
+         location-warn-to-string*
+         location-warn-to-string/normalize
+         show-source-error
+         source-error->string
+         show-procedure-location
+         source-quote ;; deprecated, use source-quote* instead?
+         source-dequote ;; ditto
+         source-quote*)
 
 
 (include "cj-standarddeclares-1--include.scm")
@@ -80,9 +80,9 @@
   (let ((msg (string-append "not a "typename" object:")))
     (lambda (proc)
       (lambda (o)
-	(if (type? o)
-	    (proc o)
-	    (error msg o))))))
+        (if (type? o)
+            (proc o)
+            (error msg o))))))
 
 (define source-check (check source? "source"))
 
@@ -90,8 +90,8 @@
   (if (##source? x)
       (##source-code x)
       (begin
-	;;(warn "not source code:" x)
-	x)))
+        ;;(warn "not source code:" x)
+        x)))
 
 (define (mk-source/? type?)
   (lambda (v)
@@ -126,9 +126,9 @@
 (define (make-position* line maybe-column)
   (if (<= 0 line)
       (if (or (not maybe-column)
-	      (<= 0 maybe-column))
-	  (vector 'position* line maybe-column)
-	  (error "column out of range:" maybe-column))
+              (<= 0 maybe-column))
+          (vector 'position* line maybe-column)
+          (error "column out of range:" maybe-column))
       (error "line out of range:" line)))
 (define (position*? v)
   (and (vector? v)
@@ -172,33 +172,33 @@
 (define (location?* v)
   (or (location? v)
       (and (location*? v)
-	   (@position*-maybe-column (@location*-position v))
-	   #t)))
+           (@position*-maybe-column (@location*-position v))
+           #t)))
 
 (define location-check (check location? "location"))
 
 (define (location-container v)
   (cond ((location? v)
-	 (##locat-container v))
-	((location*? v)
-	 (@location*-container v))
-	(else
-	 (error "not a location object:" v))) )
+         (##locat-container v))
+        ((location*? v)
+         (@location*-container v))
+        (else
+         (error "not a location object:" v))) )
 
 (define (container->path container)
   (if (with-exception-catcher (lambda (e) #f) 
-			      (lambda () (eval '##container->path-hook)))
+                              (lambda () (eval '##container->path-hook)))
       (or (##container->path-hook container)
           container)
     container))
 
 (define (location-position v)
   (cond ((location? v)
-	 (##locat-position v))
-	((location*? v)
-	 (@location*-position v))
-	(else
-	 (error "not a location object:" v))))
+         (##locat-position v))
+        ((location*? v)
+         (@location*-position v))
+        (else
+         (error "not a location object:" v))))
 
 (define (position? o)
   ;; hmm
@@ -208,37 +208,37 @@
 
 (define (position-line v)
   (cond ((position? v)
-	 (+ 1 (bitwise-and v 65535)))
-	((position*? v)
-	 (@position*-line v))
-	(else
-	 (error "not a position object:" v))))
+         (+ 1 (bitwise-and v 65535)))
+        ((position*? v)
+         (@position*-line v))
+        (else
+         (error "not a position object:" v))))
 
 (define (position-column v)
   (cond ((position? v)
-	 ;; XX should use bit shift instead
-	 (+ 1 (quotient v 65536)))
-	((position*? v)
-	 (or (@position*-maybe-column v)
-	     (error "position-column: position* does not contain column:" v)))
-	(else
-	 (error "not a position object:" v))))
+         ;; XX should use bit shift instead
+         (+ 1 (quotient v 65536)))
+        ((position*? v)
+         (or (@position*-maybe-column v)
+             (error "position-column: position* does not contain column:" v)))
+        (else
+         (error "not a position object:" v))))
 
 ;; copy-paste
 (define (position-maybe-column v)
   (cond ((position? v)
-	 ;; XX should use bit shift instead
-	 (+ 1 (quotient v 65536)))
-	((position*? v)
-	 (@position*-maybe-column v))
-	(else
-	 (error "not a position object:" v))))
+         ;; XX should use bit shift instead
+         (+ 1 (quotient v 65536)))
+        ((position*? v)
+         (@position*-maybe-column v))
+        (else
+         (error "not a position object:" v))))
 
 
 (define (position-string pos)
   (string-append (scm:object->string (position-line pos))
-		 "."
-		 (scm:object->string (position-column pos))))
+                 "."
+                 (scm:object->string (position-column pos))))
 
 (define (maybe-position-string maybe-pos)
   (if maybe-pos
@@ -248,16 +248,16 @@
 
 (define (make-position line column)
   (let ((l (- line 1))
-	(c (- column 1)))
+        (c (- column 1)))
     (if (<= 0 l 65535)
-	(if (<= 0 c)
-	    (let ((r (bitwise-ior l
-				  (arithmetic-shift c 16))))
-	      (if (##fixnum? r)
-		  r
-		  (error "column out of range:" column)))
-	    (error "column out of range:" column))
-	(error "line out of range:" line))))
+        (if (<= 0 c)
+            (let ((r (bitwise-ior l
+                                  (arithmetic-shift c 16))))
+              (if (##fixnum? r)
+                  r
+                  (error "column out of range:" column)))
+            (error "column out of range:" column))
+        (error "line out of range:" line))))
 
 (define (make-location container position)
   ((position-check
@@ -267,14 +267,14 @@
 
 (define (make-source code locat)
   (let ((cont
-	 (lambda (locat)
-	   (##make-source code locat))))
+         (lambda (locat)
+           (##make-source code locat))))
     (if locat
-	((location-check
-	  cont)
-	 locat)
-	;; have to accept #f as locat, just as ##make-source
-	(cont locat))))
+        ((location-check
+          cont)
+         locat)
+        ;; have to accept #f as locat, just as ##make-source
+        (cont locat))))
 
 (define (sourcify x src)
   ((source-check
@@ -286,29 +286,29 @@
   (let ((master-loc (source-location master)))
     (let rec ((s s))
       ((lambda (process)
-	 (if (source? s)
-	     (make-source (process (source-code s))
-			  (source-location s))
-	     (make-source (process s)
-			  master-loc)))
+         (if (source? s)
+             (make-source (process (source-code s))
+                          (source-location s))
+             (make-source (process s)
+                          master-loc)))
        ;; "where process ="
        (lambda (c)
-	 (cond ((pair? c)
-		(improper-map rec c))
-	       ((vector? c)
-		;; quoted vectors (syntax for constants)
-		(vector-map-1 rec c))
-	       ((box? c)
-		(box (rec (unbox c))))
-	       ((or (##structure? c)
-		    ;; some more?
-		    )
-		;; v- again, how to give location info/exceptions generally?
-		(error "cj-sourcify-deep: type of this object unexpected:" c))
-	       (else
-		;; also e.g. (u8vector? c):
-		;; doesn't contain anything, so:
-		c)))))))
+         (cond ((pair? c)
+                (improper-map rec c))
+               ((vector? c)
+                ;; quoted vectors (syntax for constants)
+                (vector-map-1 rec c))
+               ((box? c)
+                (box (rec (unbox c))))
+               ((or (##structure? c)
+                    ;; some more?
+                    )
+                ;; v- again, how to give location info/exceptions generally?
+                (error "cj-sourcify-deep: type of this object unexpected:" c))
+               (else
+                ;; also e.g. (u8vector? c):
+                ;; doesn't contain anything, so:
+                c)))))))
 
 
 (define (possibly-sourcify s master)
@@ -324,15 +324,15 @@
 (define (cj-desourcify x)
   (let ((x (if (##source? x) (##source-code x) x)))
     (cond ((pair? x)
-	   (cons (cj-desourcify (car x))
-		 (cj-desourcify (cdr x))))
-	  ((##vector? x)
-	   (vector-map-1 cj-desourcify x))
-	  ((box? x)
-	   (box (cj-desourcify (unbox x))))
-	  ;; XX more?
-	  (else
-	   x))))
+           (cons (cj-desourcify (car x))
+                 (cj-desourcify (cdr x))))
+          ((##vector? x)
+           (vector-map-1 cj-desourcify x))
+          ((box? x)
+           (box (cj-desourcify (unbox x))))
+          ;; XX more?
+          (else
+           x))))
 
 
 (define (read-all-source #!optional (port (current-input-port)))
@@ -340,7 +340,7 @@
   (let recur ()
     (let ((expr (##read-expr-from-port port)))
       (if (eof-object? expr) '()
-	  (cons expr (recur))))))
+          (cons expr (recur))))))
 
 
 
@@ -367,16 +367,16 @@
 (define (location-string l #!key non-highlighting? normalize omit-column?)
   (let ((c (location-container l)))
     (string-append (scm:object->string
-		    (if (and normalize (string? c))
-			(normalize c)
-			c))
-		   (if non-highlighting?
-		       " @ "
-		       "@")
-		   (let ((pos (location-position l)))
-		     (if omit-column?
-			 (object->string (position-line pos))
-			 (position-string pos))))))
+                    (if (and normalize (string? c))
+                        (normalize c)
+                        c))
+                   (if non-highlighting?
+                       " @ "
+                       "@")
+                   (let ((pos (location-position l)))
+                     (if omit-column?
+                         (object->string (position-line pos))
+                         (position-string pos))))))
 
 
 (define (display/maybe-port val maybe-port)
@@ -391,23 +391,23 @@
 
 ;; yes, kinda lame name (historic). Show the location that a location object points to.
 (define (show-location-location
-	 maybe-l
-	 #!key
-	 (errstr "*** ERROR IN (just showing location) ")
-	 (msg "")
-	 (args '())
-	 (display/maybe-port display/maybe-port)
-	 non-highlighting?
-	 normalize
-	 maybe-port)
+         maybe-l
+         #!key
+         (errstr "*** ERROR IN (just showing location) ")
+         (msg "")
+         (args '())
+         (display/maybe-port display/maybe-port)
+         non-highlighting?
+         normalize
+         maybe-port)
   (display/maybe-port
    (string-append
     errstr
     (if maybe-l
-	(location-string maybe-l
-			 non-highlighting?: non-highlighting?
-			 normalize: normalize)
-	"(no-location-information)")
+        (location-string maybe-l
+                         non-highlighting?: non-highlighting?
+                         normalize: normalize)
+        "(no-location-information)")
     " -- "
     msg
     (scm:objects->string args prepend: ": ")
@@ -415,29 +415,29 @@
    maybe-port))
 
 (define (show-source-location
-	 s
-	 #!key
-	 (errstr "*** ERROR IN (just showing location) ")
-	 (msg "")
-	 (args '())
-	 (display/maybe-port display/maybe-port)
-	 maybe-port)
+         s
+         #!key
+         (errstr "*** ERROR IN (just showing location) ")
+         (msg "")
+         (args '())
+         (display/maybe-port display/maybe-port)
+         maybe-port)
   (show-location-location (if (##source? s)
-			      (##source-locat s)
-			      #f)
-			  errstr: errstr
-			  msg: msg
-			  args: args
-			  display/maybe-port: display/maybe-port
-			  maybe-port: maybe-port))
+                              (##source-locat s)
+                              #f)
+                          errstr: errstr
+                          msg: msg
+                          args: args
+                          display/maybe-port: display/maybe-port
+                          maybe-port: maybe-port))
 
 
 ;; analog to source-error:
 (define (source-warn source message . args)
   (show-source-location source
-			errstr: "*** WARNING IN "
-			msg: message
-			args: args))
+                        errstr: "*** WARNING IN "
+                        msg: message
+                        args: args))
 
 ;; At runtime use variant for locations instead of source-warn, since
 ;; locations can be quoted easily, unlike source code:
@@ -446,21 +446,21 @@
   ;; way that emacs shows a window at that location (good for
   ;; e.g. showing *known* test failures).
   (let ((cont
-	 (lambda (normalize location message args)
-	   (show-location-location location
-				   errstr: (if non-highlighting?
-					       "*** Warning in "
-					       "*** WARNING IN ")
-				   msg: message
-				   args: args
-				   display/maybe-port: display/maybe-port
-				   non-highlighting?: non-highlighting?
-				   normalize: normalize))))
+         (lambda (normalize location message args)
+           (show-location-location location
+                                   errstr: (if non-highlighting?
+                                               "*** Warning in "
+                                               "*** WARNING IN ")
+                                   msg: message
+                                   args: args
+                                   display/maybe-port: display/maybe-port
+                                   non-highlighting?: non-highlighting?
+                                   normalize: normalize))))
     (if normalize?
-	(lambda (location normalize message . args)
-	  (cont normalize location message args))
-	(lambda (location message . args)
-	  (cont #f location message args)))))
+        (lambda (location normalize message . args)
+          (cont normalize location message args))
+        (lambda (location message . args)
+          (cont #f location message args)))))
 
 (define location-warn
   (_location-warn display-console #f #f))
@@ -479,25 +479,25 @@
 
 (define (show-source-error e #!optional maybe-port)
   (show-source-location (source-error-source e)
-			errstr: "*** ERROR IN syntax, "
-			msg: (source-error-message e)
-			args: (source-error-args e)
-			maybe-port: maybe-port))
+                        errstr: "*** ERROR IN syntax, "
+                        msg: (source-error-message e)
+                        args: (source-error-args e)
+                        maybe-port: maybe-port))
 
 (define (source-error->string e)
   (show-source-location (source-error-source e)
-			errstr: "*** ERROR IN syntax, "
-			msg: (source-error-message e)
-			args: (source-error-args e)
-			display/maybe-port: 1st-argument/2))
+                        errstr: "*** ERROR IN syntax, "
+                        msg: (source-error-message e)
+                        args: (source-error-args e)
+                        display/maybe-port: 1st-argument/2))
 
 (define (show-procedure-location p)
   (if (procedure? p)
       (show-location-location (##procedure-locat p)
-			      errstr: "*** DEFINED IN "
-			      msg: "as"
-			      args: (list p)
-			      display/maybe-port: display/maybe-port)
+                              errstr: "*** DEFINED IN "
+                              msg: "as"
+                              args: (list p)
+                              display/maybe-port: display/maybe-port)
       (error "not a procedure:" p)))
 
 
