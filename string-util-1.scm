def37cfcaf8206c3993e9498b24baccde71c5655
Christian Jaeger <ch@christianjaeger.ch>
2020-03-07 23:35:58 +0000

    string-util-1: add |string-split/location|, unfinished?
    Not sure how to handle location with retain-matches?==#t, also, should
    probably change that into a tri-state. retain-matches? is being used
    by |string-escape|.

--- string-util-1.scm
+++ string-util-1.scm
@@ -1,4 +1,4 @@
-;;; Copyright 2010, 2018 by Christian Jaeger <ch@christianjaeger.ch>
+;;; Copyright 2010, 2020 by Christian Jaeger <ch@christianjaeger.ch>
 
 ;;;    This file is free software; you can redistribute it and/or modify
 ;;;    it under the terms of the GNU General Public License (GPL) as published 
@@ -6,12 +6,14 @@
 ;;;    (at your option) any later version.
 
 
-(require test
+(require (cj-source source position position-line position-column-add)
+         test
 	 ;;(list-util-1 list-split)
 	 )
 
 (export string-contains-char?
-	string-split)
+	string-split
+        string-split/location)
 
 (include "cj-standarddeclares.scm")
 
@@ -75,8 +77,99 @@
  ("" "bar" "baz" "")
  > (string-split "||baz|" #\|)
  ("" "" "baz" "")
+ > (string-split "||baz|" (lambda (c) (case c ((#\|) #t) (else #f))) #f)
+ ("" "" "baz" "")
  > (string-split "||baz|" (lambda (c) (case c ((#\| #\a) #t) (else #f))))
  ("" "" "b" "z" "")
  > (string-split "||baz|" (lambda (c) (case c ((#\| #\a) #t) (else #f))) #t)
- ("" "|" "|b" "az" "|"))
+ ("" "|" "|b" "az" "|")
+ > (string-split "Hello\nWorld" #\newline #t)
+ ;; Is this really what it should be? XX surely not?
+ ("Hello" "\nWorld"))
+
+
+(define (string-split/location str/location
+                               char-or-pred
+                               #!optional retain-matches?)
+  "If str/location is source (with location), then return a list of
+source, too, with each element's location updated to reflect the
+position of the start of that element."
+  (if (source? str/location)
+      ;; adapted copy-paste; changed to process from the left.
+      (let* ((str (source-code str/location))
+             (loc (source-location str/location))
+             (pos (location-position loc))
+             (cnt (location-container loc))
+             (len (string-length str))
+             (pred (char-or-pred.pred char-or-pred)))
+        (let lp ((pos pos)
+                 (startpos pos)
+                 (i 0)
+                 (prev-i 0)
+                 (rstrs '()))
+          (if (< i len)
+              (let* ((c (string-ref str i))
+                     (pos* (case c
+                             ((#\newline)
+                              ;; Assume Unix behaviour.
+                              (position (inc (position-line pos))
+                                        1))
+                             ((#\return)
+                              ;; XX issue with MacOS (Classic only?)?
+                              (position (position-line pos)
+                                        1))
+                             (else
+                              ;; same for #\tab as well (since pos is
+                              ;; in characters, not the visual
+                              ;; column!)
+                              (position-column-add pos 1)))))
+                (if (pred c)
+                    (lp pos*
+                        pos*
+                        (inc i)
+                        (if retain-matches?
+                            i
+                            (inc i))
+                        (cons (source (substring str
+                                                 prev-i
+                                                 i)
+                                      (location cnt startpos))
+                              rstrs))
+                    (lp pos*
+                        startpos
+                        (inc i)
+                        prev-i
+                        rstrs)))
+              (reverse
+               (cons (source (substring str prev-i len)
+                             ;; XX use pos here correct???
+                             (location cnt startpos))
+                     rstrs)))))
+      (string-split str/location char-or-pred retain-matches?)))
+
+(TEST
+ > (define (t . args)
+     (let ((ss (apply string-split/location args)))
+       (list (map source-code ss)
+             (map (lambda (v) (location-string (source-location v))) ss))))
+ > (define s (source "Hello\nworld" (location '(foo) (position 10 13))))
+ > (t s #\newline)
+ (("Hello" "world") ("(foo)@10.13" "(foo)@11.1"))
+ > (t s #\o)
+ (("Hell" "\nw" "rld") ("(foo)@10.13" "(foo)@10.18" "(foo)@11.3"))
+ > (t s #\newline #t)
+ (("Hello" "\nworld") ("(foo)@10.13" "(foo)@11.1"))
+ > (string-split/location "||baz|" (lambda (c) (case c ((#\| #\a) #t) (else #f))) #t)
+ ("" "|" "|b" "az" "|")
+ > (define s2 (source "||baz|" (location '(f) (position 10 13))))
+ > (t s2 (lambda (c) (case c ((#\|) #t) (else #f))) #f)
+ (("" "" "baz" "")
+  ("(f)@10.13" "(f)@10.14" "(f)@10.15" "(f)@10.19"))
+ > (t s2 (lambda (c) (case c ((#\|) #t) (else #f))) #t)
+ (("" "|" "|baz" "|")
+  ("(f)@10.13" "(f)@10.14" "(f)@10.15" "(f)@10.19"))
+ ;; > (t s2 (lambda (c) (case c ((#\| #\a) #t) (else #f))) #t)
+ ;; (("" "|" "|b" "az" "|")
+ ;;  ("(f)@10.13" "(f)@10.13" "(f)@10.15" "(f)@10.16" "(f)@10.18"))
+ )
 
