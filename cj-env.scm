aec2159f6bea8caf17ee6caaf84aabb80654070f
Christian Jaeger <ch@christianjaeger.ch>
2020-04-03 07:16:47 +0100

    cj-env: add |no| alias. OK?

--- cj-env.scm
+++ cj-env.scm
@@ -19,6 +19,7 @@
 	(macro If)
         (macro if-not)
         (macro when)
+        no
 	natural?
 	natural0?
 	fxnatural?
@@ -148,6 +149,8 @@
  #!void)
 
 
+;; https://sep.yimg.com/ty/cdn/paulgraham/bel.bel?t=1570993483&
+(define no null?)
 
 
 (both-times
