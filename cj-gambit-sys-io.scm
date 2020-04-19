d51cc7ec7f9ec1e240e6a1ebd832f534c7126e3b
Christian Jaeger <ch@christianjaeger.ch>
2020-04-19 22:58:37 +0100

    Create cj-gambit-sys-io.scm

---
+++ cj-gambit-sys-io.scm
@@ -0,0 +1,30 @@
+;;; Copyright 2020 by Christian Jaeger <ch@christianjaeger.ch>
+
+;;;    This file is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License (GPL) as published 
+;;;    by the Free Software Foundation, either version 2 of the License, or
+;;;    (at your option) any later version.
+
+
+(require (string-util-2 string-downcase)
+         string-case
+         test)
+
+(export os-exception-codesymbol)
+
+(use-memcmp);; oh my, for string-case
+
+
+(define (os-exception-codesymbol e) ;; -> symbol? or exception
+  "A representation of errors as symbols, unlike the \"OS dependent\"
+codes, and unlike the strings which might change. Throws an exception
+on unknown errors."
+  ;; Sigh (why do I have to do this)
+  (let* ((code (os-exception-code e))
+         (msg (string-downcase (err-code->string code))))
+    (string-case
+     msg
+     (("can't convert from c utf-8-string") 'cannot-convert-from-utf-8)
+     (else
+      (error "unknown error message (unfinished) for code:" msg code)))))
+
