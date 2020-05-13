f234fd1cd0d07c8db171c8038157e611e7839322
Christian Jaeger <ch@christianjaeger.ch>
2020-05-13 15:55:55 +0100

    subtitles-utils: add string.subtitle-strip-newlines

--- subtitles-utils.scm
+++ subtitles-utils.scm
@@ -14,6 +14,8 @@
 (export (methods strings.save-to!)
         T-no-add
         i
+        (methods chars.subtitle-strip-newlines
+                 string.subtitle-strip-newlines)
         #!optional
         strings?)
 
@@ -57,3 +59,81 @@
  "<i>fun </i>\n"
  > (i "fun \nThere")
  "<i>fun </i>\n<i>There</i>")
+
+
+(def. (chars.subtitle-strip-newlines l [boolean? nbsp-after-minus?])
+  (reverse
+   (let lp ((l l)
+            (prev-space? #f)
+            (r '()))
+     (def (process- l r)
+          "process part after #\\- character"
+          (let (r* (cons #\- r))
+            (if nbsp-after-minus?
+                (let lp- ((l l)
+                          (prev-space? prev-space?)
+                          (r r*))
+                  (if-let-pair
+                   ((a l*) l)
+                   (if (eq? a #\space)
+                       (lp- l* #t (cons nbsp r))
+                       (lp l #f r))
+                   r))
+                (lp l prev-space? r*))))
+     (if-let-pair
+      ((a l*) l)
+      (cond ((eq? a #\-)
+             (process- l* r))
+            ((eq? a #\newline)
+             (if-let-pair
+              ((b l**) l*)
+              (if (eq? b #\-)
+                  ;; leave newline in and process #\-
+                  (process- l** (cons a r))
+                  ;; replace newline with a space, unless previous
+                  ;; character was a space already
+                  (lp l*
+                      ;; consider newline a space regardless of
+                      ;; whether it was replaced by a space? XX make a
+                      ;; whitespace flag, fully?
+                      #t
+                      (if prev-space? r (cons #\space r))))
+              (cons a r)))
+            (else
+             (lp l* (eq? a #\space) (cons a r))))
+      r))))
+
+(def. (string.subtitle-strip-newlines str [boolean? nbsp-after-minus?])
+     "Strip unnecessary newlines from a subtitle segment string; if
+the second argument is true, will replace spaces between a `-` and the
+next non-space character with non-breaking spaces."
+     ;; good that I don't keep them split up?
+     (=> str
+         .list
+         (.subtitle-strip-newlines nbsp-after-minus?)
+         char-list.string))
+;; BTW:
+;;  .list -> .char-list or .chars ?
+;;  char-list.string -> chars.string ?
+
+
+(TEST
+ > (def t (C .subtitle-strip-newlines _ #t))
+ > (t "a b")
+ "a b"
+ > (t "a\nb")
+ "a b"
+ > (t "a \nb")
+ "a b"
+ > (t "a \n b")
+ "a  b"
+ > (t "\na\n")
+ ;; should it remove the \n to the left?
+ " a\n"
+ > (t "\n\na\n\n")
+ " a \n"
+ > (t "\nder uns  sagen\nkonnte, \ndass die Früchte verdorben waren.\n")
+ " der uns  sagen konnte, dass die Früchte verdorben waren.\n"
+ > (t "Kriege ich den Schlüssel \nvon\nIhnen, Sir?\n- Ja,\nich werde ihn übergeben. -  Gut so!")
+ "Kriege ich den Schlüssel von Ihnen, Sir?\n- Ja, ich werde ihn übergeben. -  Gut so!")
+
